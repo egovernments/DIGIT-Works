a643f69b6aa81b03a99af696ab1424a63ded2297
Nipun Arora <aroranipun1@gmail.com>
2022-08-01 17:48:41 +0530

    CreateLoi static screen

---
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/works/src/pages/employee/LOI/Create.js
@@ -0,0 +1,268 @@
+import React, { useReducer } from 'react'
+import { Controller, useForm } from 'react-hook-form'
+import { Card, Header, CardSectionHeader, LabelFieldPair, CardLabel, CardText, CardSectionSubText, TextInput, Dropdown, UploadFile, MultiUploadWrapper, ActionBar, SubmitBar,DatePicker } from '@egovernments/digit-ui-react-components';
+import { useTranslation } from 'react-i18next';
+
+//import SubWork from '../../components/CreateEstimate/SubWork';
+const allowedFileTypes = /(.*?)(jpg|jpeg|png|image|pdf|msword|openxmlformats-officedocument)$/i;
+
+const CreateLOI = () => {
+    const { t } = useTranslation()
+    const {
+        register,
+        control,
+        watch,
+        setValue,
+        unregister,
+        handleSubmit,
+        formState: { errors, ...rest },
+        reset,
+        ...methods
+    } = useForm();
+
+    const dummyData = [
+        {
+            name: "Nipun"
+        },
+        {
+            name: "Vipul"
+        },
+        {
+            name: "Shaifali"
+        },
+        {
+            name: "Amit"
+        },
+        {
+            name: "Sumit"
+        },
+    ]
+
+    const onFormSubmit = (_data) => {
+        debugger
+        console.log(errors)
+        console.log(_data);
+    }
+
+    const getDate = () => {
+        const today = new Date();
+
+        const date = today.getDate() + '/' + (today.getMonth() + 1) + '/' + today.getFullYear();
+        return date
+    }
+
+    return (
+        <form onSubmit={handleSubmit(onFormSubmit)}>
+            <Header>{t("WORKS_CREATE_LOI")}</Header>
+            <Card >
+                <CardSectionHeader >{t(`WORKS_LOI_DETAILS`)}</CardSectionHeader>
+                <LabelFieldPair>
+                    <CardLabel style={{ "fontSize": "16px", "fontStyle": "bold", "fontWeight": "600" }} >{t(`WORKS_ESTIMATE_NO`)}</CardLabel>
+                    {/* <TextInput className={"field"} name="reasonDocumentNumber" inputRef={register()} value={getDate()} disabled /> */}
+                    <p>{"estimateIdPlaceholder"}</p>
+                </LabelFieldPair>
+                
+                <LabelFieldPair>
+                    <CardLabel style={{ "fontSize": "16px", "fontStyle": "bold", "fontWeight": "600" }} >{t(`WORKS_NAME_OF_WORK`)}</CardLabel>
+                    {/* <TextInput className={"field"} name="reasonDocumentNumber" inputRef={register()} value={getDate()} disabled /> */}
+                    <p>{"nameofworkplaceholder"}</p>
+                </LabelFieldPair>
+
+                <LabelFieldPair>
+                    <CardLabel style={{ "fontSize": "16px", "fontStyle": "bold", "fontWeight": "600" }} >{t(`WORKS_SUB_ESTIMATE_NO`)}</CardLabel>
+                    {/* <TextInput className={"field"} name="reasonDocumentNumber" inputRef={register()} value={getDate()} disabled /> */}
+                    <p>{"subestimatenoplaceholder"}</p>
+                </LabelFieldPair>
+
+                <LabelFieldPair>
+                    <CardLabel style={{ "fontSize": "16px", "fontStyle": "bold", "fontWeight": "600" }}>{`${t(`WORKS_ABSTRACT_ESTIMATE_NO`)}:`}</CardLabel>
+                    <TextInput className={"field"} name="lor" inputRef={register()} />
+                </LabelFieldPair>
+
+                <LabelFieldPair>
+                    <CardLabel style={{ "fontSize": "16px", "fontStyle": "bold", "fontWeight": "600" }}>{`${t(`WORKS_FILE_NO`)}:`}</CardLabel>
+                    <TextInput className={"field"} name="lor" inputRef={register()} />
+                </LabelFieldPair>
+
+                <LabelFieldPair>
+                    <CardLabel style={{ "fontSize": "16px", "fontStyle": "bold", "fontWeight": "600" }}>{`${t(`WORKS_FILE_DATE`)}:`}</CardLabel>
+                    <Controller
+                        render={(props) => <DatePicker style={{"width":"50%"}} date={props.value} onChange={props.onChange} />}
+                        name="fromDate"
+                        control={control}
+                    />
+                </LabelFieldPair>
+
+                <CardSectionHeader >{t(`WORKS_FINANCIAL_DETAILS`)}</CardSectionHeader>
+                
+                <LabelFieldPair>
+                    <CardLabel style={{ "fontSize": "16px", "fontStyle": "bold", "fontWeight": "600" }} >{t(`WORKS_SUB_ESTIMATE_NO`)}</CardLabel>
+                    {/* <TextInput className={"field"} name="reasonDocumentNumber" inputRef={register()} value={getDate()} disabled /> */}
+                    <p>{"ESTIMATE AMT"}</p>
+                </LabelFieldPair>
+
+                <LabelFieldPair>
+                    <CardLabel style={{ "fontSize": "16px", "fontStyle": "bold", "fontWeight": "600" }}>{`${t(`WORKS_FINALIZED_PER`)}:`}</CardLabel>
+                    <TextInput className={"field"} name="lor" inputRef={register()} />
+                </LabelFieldPair>
+                <LabelFieldPair>
+                    <CardLabel style={{ "fontSize": "16px", "fontStyle": "bold", "fontWeight": "600" }}>{`${t(`WORKS_AGREEMENT_AMT`)}:`}</CardLabel>
+                    <TextInput className={"field"} name="lor" inputRef={register()} />
+                </LabelFieldPair>
+
+                <CardSectionHeader >{t(`WORKS_AGGREEMENT_DETAILS`)}</CardSectionHeader>
+
+                <LabelFieldPair>
+                    <CardLabel style={{ "fontSize": "16px", "fontStyle": "bold", "fontWeight": "600" }}>{`${t(`WORKS_DATE_OF_AGG`)}:`}</CardLabel>
+                    <Controller
+                        render={(props) => <DatePicker style={{ "width": "50%" }} date={props.value} onChange={props.onChange} />}
+                        name="aggDate"
+                        control={control}
+                    />
+                </LabelFieldPair>
+
+                <LabelFieldPair>
+                    <CardLabel style={{ "fontSize": "16px", "fontStyle": "bold", "fontWeight": "600" }}>{`${t(`WORKS_AGENCY_NAME`)}:*`}</CardLabel>
+                    <Controller
+                        name="agencyname"
+                        control={control}
+                        //rules={{ required: true }}
+                        render={(props) => {
+                            return (
+                                <Dropdown
+                                    className={`field`}
+                                    option={dummyData}
+                                    //selected={props?.value}
+                                    optionKey={"name"}
+                                    t={t}
+                                    select={props?.onChange}
+                                />
+                            );
+                        }}
+                    />
+                </LabelFieldPair>
+
+                <LabelFieldPair>
+                    <CardLabel style={{ "fontSize": "16px", "fontStyle": "bold", "fontWeight": "600" }}>{`${t(`WORKS_CONT_ID`)}:`}</CardLabel>
+                    <TextInput className={"field"} name="lor" inputRef={register()} />
+                </LabelFieldPair>
+
+                <LabelFieldPair>
+                    <CardLabel style={{ "fontSize": "16px", "fontStyle": "bold", "fontWeight": "600" }}>{`${t(`WORKS_ADD_SECURITY_DP`)}:`}</CardLabel>
+                    <TextInput className={"field"} name="lor" inputRef={register()} />
+                </LabelFieldPair>
+
+                <LabelFieldPair>
+                    <CardLabel style={{ "fontSize": "16px", "fontStyle": "bold", "fontWeight": "600" }}>{`${t(`WORKS_BANK_G`)}:`}</CardLabel>
+                    <TextInput className={"field"} name="lor" inputRef={register()} />
+                </LabelFieldPair>
+
+                <LabelFieldPair>
+                    <CardLabel style={{ "fontSize": "16px", "fontStyle": "bold", "fontWeight": "600" }}>{`${t(`WORKS_EMD`)}:`}</CardLabel>
+                    <TextInput className={"field"} name="lor" inputRef={register()} />
+                </LabelFieldPair>
+
+                <LabelFieldPair>
+                    <CardLabel style={{ "fontSize": "16px", "fontStyle": "bold", "fontWeight": "600" }}>{`${t(`WORKS_CONT_PERIOD`)}:`}</CardLabel>
+                    <TextInput className={"field"} name="lor" inputRef={register()} />
+                </LabelFieldPair>
+
+                <LabelFieldPair>
+                    <CardLabel style={{ "fontSize": "16px", "fontStyle": "bold", "fontWeight": "600" }}>{`${t(`WORKS_DEFECT_LIA`)}:`}</CardLabel>
+                    <TextInput className={"field"} name="lor" inputRef={register()} />
+                </LabelFieldPair>
+
+                <LabelFieldPair>
+                    <CardLabel style={{ "fontSize": "16px", "fontStyle": "bold", "fontWeight": "600" }}>{`${t(`WORKS_OFFICER_INCHARGE_DES`)}:*`}</CardLabel>
+                    <Controller
+                        name="agencyname"
+                        control={control}
+                        //rules={{ required: true }}
+                        render={(props) => {
+                            return (
+                                <Dropdown
+                                    className={`field`}
+                                    option={dummyData}
+                                    //selected={props?.value}
+                                    optionKey={"name"}
+                                    t={t}
+                                    select={props?.onChange}
+                                />
+                            );
+                        }}
+                    />
+                </LabelFieldPair>
+
+                <LabelFieldPair>
+                    <CardLabel style={{ "fontSize": "16px", "fontStyle": "bold", "fontWeight": "600" }}>{`${t(`WORKS_OFFICER_INCHARGE_NAME`)}:*`}</CardLabel>
+                    <Controller
+                        name="agencyname"
+                        control={control}
+                        //rules={{ required: true }}
+                        render={(props) => {
+                            return (
+                                <Dropdown
+                                    className={`field`}
+                                    option={dummyData}
+                                    //selected={props?.value}
+                                    optionKey={"name"}
+                                    t={t}
+                                    select={props?.onChange}
+                                />
+                            );
+                        }}
+                    />
+                </LabelFieldPair>
+                {/* --------------------------------------- */}
+                
+                <CardSectionHeader >{t(`WORKS_RELEVANT_DOCS`)}</CardSectionHeader>
+                <LabelFieldPair>
+                    <CardLabel style={{ "fontSize": "16px", "fontStyle": "bold", "fontWeight": "600 " }}>{t(`WORKS_UPLOAD_FILES`)}</CardLabel>
+                    <div className='field'>
+                        <Controller
+                            name="uploads"
+                            control={control}
+                            // defaultValue={formData?.category ? data?.mseva?.EventCategories.filter(category => category.code === formData?.category)?.[0] : null}
+                            rules={{ required: false }}
+                            render={({ onChange, ref, value = [] }) => {
+                                function getFileStoreData(filesData) {
+                                    const numberOfFiles = filesData.length
+                                    let finalDocumentData = []
+                                    if (numberOfFiles > 0) {
+                                        filesData.forEach(value => {
+                                            finalDocumentData.push({
+                                                fileName: value?.[0],
+                                                fileStoreId: value?.[1]?.fileStoreId?.fileStoreId,
+                                                documentType: value?.[1]?.file?.type
+                                            })
+                                        })
+                                    }
+                                    onChange(finalDocumentData)
+                                }
+                                return <MultiUploadWrapper
+                                    t={t}
+                                    module="works"
+                                    tenantId={"pb"}
+                                    getFormState={getFileStoreData}
+                                    showHintBelow={true}
+                                    setuploadedstate={value}
+                                    allowedFileTypesRegex={allowedFileTypes}
+                                    allowedMaxSizeInMB={5}
+                                    hintText={t("DOCUMENTS_ATTACH_RESTRICTIONS_SIZE")}
+                                />
+                            }
+                            }
+                        />
+                    </div>
+                </LabelFieldPair>
+
+                <ActionBar>
+                    <SubmitBar submit={true} label={t("WORKS_CREATE_ESTIMATE")} />
+                </ActionBar>
+            </Card>
+        </form>
+
+    )
+}
+
+export default CreateLOI
--- frontend/micro-ui/web/micro-ui-internals/packages/modules/works/src/pages/employee/index.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/works/src/pages/employee/index.js
@@ -1,13 +1,14 @@
 import { PrivateRoute } from "@egovernments/digit-ui-react-components";
 import React from "react";
 import { Switch } from "react-router-dom";
-
+import CreateLOI from "./LOI/create";
 const App = ({ path }) => {
   return (
     <Switch>
       <React.Fragment>
         <div className="ground-container">
           <PrivateRoute path={`${path}/create-application`} component={() => <div>Hi</div>} />
+          <PrivateRoute path={`${path}/create-loi`} component={() => <CreateLOI />} />
         </div>
       </React.Fragment>
     </Switch>
