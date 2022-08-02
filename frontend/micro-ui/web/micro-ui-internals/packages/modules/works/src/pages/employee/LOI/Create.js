ebf25cfcb79828047fd8c378b3861a205518b7be
Nipun Arora <aroranipun1@gmail.com>
2022-08-02 19:41:49 +0530

    Added proper validations for create LOI screen

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/works/src/pages/employee/LOI/Create.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/works/src/pages/employee/LOI/Create.js
@@ -1,6 +1,6 @@
 import React, { useReducer } from 'react'
 import { Controller, useForm } from 'react-hook-form'
-import { Card, Header, CardSectionHeader, LabelFieldPair, CardLabel, CardText, CardSectionSubText, TextInput, Dropdown, UploadFile, MultiUploadWrapper, ActionBar, SubmitBar,DatePicker,Row,StatusTable } from '@egovernments/digit-ui-react-components';
+import { Card, Header, CardSectionHeader, LabelFieldPair, CardLabel, CardText, CardSectionSubText, TextInput, Dropdown, UploadFile, MultiUploadWrapper, ActionBar, SubmitBar, DatePicker, Row, StatusTable,CardLabelError } from '@egovernments/digit-ui-react-components';
 import { useTranslation } from 'react-i18next';
 
 //import SubWork from '../../components/CreateEstimate/SubWork';
@@ -40,10 +40,10 @@ const CreateLOI = () => {
 
     const onFormSubmit = (_data) => {
         debugger
-        console.log(errors)
+        
         console.log(_data);
     }
-
+    console.log(errors)
     const getDate = () => {
         const today = new Date();
 
@@ -51,21 +51,23 @@ const CreateLOI = () => {
         return date
     }
 
+    let validation = {}
+
     return (
         <form onSubmit={handleSubmit(onFormSubmit)}>
-            <Header style={{"marginLeft":"14px"}}>{t("WORKS_CREATE_LOI")}</Header>
+            <Header style={{ "marginLeft": "14px" }}>{t("WORKS_CREATE_LOI")}</Header>
             <Card >
                 <CardSectionHeader >{t(`WORKS_LOI_DETAILS`)}</CardSectionHeader>
                 <StatusTable>
-                    <Row label={t("WORKS_ESTIMATE_NO")} text={"NA"} textStyle={{ whiteSpace: "pre" }} />
+                    <Row label={`${t("WORKS_ESTIMATE_NO")}:`} text={"NA"} textStyle={{ whiteSpace: "pre" }} />
                     <Row
-                        label={t("WORKS_NAME_OF_WORK")}
+                        label={`${t("WORKS_NAME_OF_WORK")}:`}
                         text={"NA"}
                         textStyle={{ whiteSpace: "pre" }}
                     />
-                    <Row label={t("WORKS_SUB_ESTIMATE_NO")} text={ "NA"} />
-                    </StatusTable>
-                
+                    <Row label={`${t("WORKS_SUB_ESTIMATE_NO")}:`} text={"NA"} />
+                </StatusTable>
+
                 {/* <LabelFieldPair>
                     <CardLabel style={{ "fontSize": "16px", "fontStyle": "bold", "fontWeight": "600" }} >{t(`WORKS_ESTIMATE_NO`)}</CardLabel>
                     <p>{"estimateIdPlaceholder"}</p>
@@ -83,26 +85,46 @@ const CreateLOI = () => {
 
                 <LabelFieldPair>
                     <CardLabel style={{ "fontSize": "16px", "fontStyle": "bold", "fontWeight": "600" }}>{`${t(`WORKS_ABSTRACT_ESTIMATE_NO`)}:`}</CardLabel>
-                    <TextInput className={"field"} name="lor" inputRef={register()} />
+                    <TextInput className={"field"} name="lor" inputRef={register()}
+                        {...(validation = {
+                            isRequired: false,
+                            pattern: "^[a-zA-Z0-9_.$@#\/]*$",
+                            type: "text",
+                            title: t("WORKS_INVALID_INPUT"),
+                        })} />
                 </LabelFieldPair>
 
                 <LabelFieldPair>
-                    <CardLabel style={{ "fontSize": "16px", "fontStyle": "bold", "fontWeight": "600" }}>{`${t(`WORKS_FILE_NO`)}:`}</CardLabel>
-                    <TextInput className={"field"} name="lor" inputRef={register()} />
+                    <CardLabel style={{ "fontSize": "16px", "fontStyle": "bold", "fontWeight": "600" }}>{`${t(`WORKS_FILE_NO`)}:*`}</CardLabel>
+                    <div className='field'>
+                    <TextInput  name="fileno" inputRef={register({required:true})} {...(validation = {
+                        isRequired: false,
+                        pattern: "^[a-zA-Z0-9_.$@#\/]*$",
+                        type: "text",
+                        title: t("WORKS_INVALID_INPUT"),
+                    })} />
+                        {errors && errors?.fileno?.type === "required" && (
+                            <CardLabelError>{t(`WORKS_REQUIRED_ERR`)}</CardLabelError>)}
+                    </div>
                 </LabelFieldPair>
 
                 <LabelFieldPair>
-                    <CardLabel style={{ "fontSize": "16px", "fontStyle": "bold", "fontWeight": "600" }}>{`${t(`WORKS_FILE_DATE`)}:`}</CardLabel>
+                    <CardLabel style={{ "fontSize": "16px", "fontStyle": "bold", "fontWeight": "600" }}>{`${t(`WORKS_FILE_DATE`)}:*`}</CardLabel>
+                    <div className='field'>
                     <Controller
-                        render={(props) => <DatePicker style={{"width":"50%"}} date={props.value} onChange={props.onChange} />}
+                        render={(props) => <DatePicker style={{ "width": "100%" }} date={props.value} onChange={props.onChange} />}
                         name="fromDate"
                         control={control}
+                        rules={{required:true}}
                     />
+                    {errors && errors?.fromDate?.type === "required" && (
+                        <CardLabelError>{t(`WORKS_REQUIRED_ERR`)}</CardLabelError>)}
+                </div>
                 </LabelFieldPair>
 
                 <CardSectionHeader >{t(`WORKS_FINANCIAL_DETAILS`)}</CardSectionHeader>
                 <StatusTable>
-                <Row label={t("WORKS_ESTIMATED_AMT")} text={"NA"} />
+                    <Row label={`${t("WORKS_ESTIMATED_AMT")}:`} text={"NA"} />
                 </StatusTable>
                 {/* <LabelFieldPair>
                     <CardLabel style={{ "fontSize": "16px", "fontStyle": "bold", "fontWeight": "600" }} >{t(`WORKS_ESTIMATED_AMT`)}</CardLabel>
@@ -121,24 +143,29 @@ const CreateLOI = () => {
                 <CardSectionHeader >{t(`WORKS_AGGREEMENT_DETAILS`)}</CardSectionHeader>
 
                 <LabelFieldPair>
-                    <CardLabel style={{ "fontSize": "16px", "fontStyle": "bold", "fontWeight": "600" }}>{`${t(`WORKS_DATE_OF_AGG`)}:`}</CardLabel>
+                    <CardLabel style={{ "fontSize": "16px", "fontStyle": "bold", "fontWeight": "600" }}>{`${t(`WORKS_DATE_OF_AGG`)}:*`}</CardLabel>
+                    <div className='field'>
                     <Controller
-                        render={(props) => <DatePicker style={{ "width": "50%" }} date={props.value} onChange={props.onChange} />}
+                        render={(props) => <DatePicker style={{ "width": "100%" }} date={props.value} onChange={props.onChange} />}
                         name="aggDate"
                         control={control}
+                        rules={{required:true}}
                     />
+                        {errors && errors?.aggDate?.type === "required" && (
+                            <CardLabelError>{t(`WORKS_REQUIRED_ERR`)}</CardLabelError>)}
+                    </div>
                 </LabelFieldPair>
 
                 <LabelFieldPair>
                     <CardLabel style={{ "fontSize": "16px", "fontStyle": "bold", "fontWeight": "600" }}>{`${t(`WORKS_AGENCY_NAME`)}:*`}</CardLabel>
+                    <div className='field'>
                     <Controller
                         name="agencyname"
                         control={control}
-                        //rules={{ required: true }}
+                        rules={{ required: true }}
                         render={(props) => {
                             return (
                                 <Dropdown
-                                    className={`field`}
                                     option={dummyData}
                                     //selected={props?.value}
                                     optionKey={"name"}
@@ -148,6 +175,9 @@ const CreateLOI = () => {
                             );
                         }}
                     />
+                        {errors && errors?.agencyname?.type === "required" && (
+                        <CardLabelError>{t(`WORKS_REQUIRED_ERR`)}</CardLabelError>)}
+                        </div>
                 </LabelFieldPair>
 
                 <LabelFieldPair>
@@ -157,39 +187,73 @@ const CreateLOI = () => {
 
                 <LabelFieldPair>
                     <CardLabel style={{ "fontSize": "16px", "fontStyle": "bold", "fontWeight": "600" }}>{`${t(`WORKS_ADD_SECURITY_DP`)}:`}</CardLabel>
-                    <TextInput className={"field"} name="lor" inputRef={register()} />
+                    <TextInput className={"field"} name="lor" inputRef={register()} 
+                        {...(validation = {
+                            isRequired: false,
+                            pattern: "^[0-9]*$",
+                            type: "number",
+                            title: t("WORKS_INVALID_INPUT"),
+                        })} />
                 </LabelFieldPair>
 
                 <LabelFieldPair>
                     <CardLabel style={{ "fontSize": "16px", "fontStyle": "bold", "fontWeight": "600" }}>{`${t(`WORKS_BANK_G`)}:`}</CardLabel>
-                    <TextInput className={"field"} name="lor" inputRef={register()} />
+                    <TextInput className={"field"} name="lor" inputRef={register()} {...(validation = {
+                        isRequired: false,
+                        pattern: "^[a-zA-Z0-9_.$@#\/]*$",
+                        type: "text",
+                        title: t("WORKS_INVALID_INPUT"),
+                    })} />
                 </LabelFieldPair>
 
                 <LabelFieldPair>
-                    <CardLabel style={{ "fontSize": "16px", "fontStyle": "bold", "fontWeight": "600" }}>{`${t(`WORKS_EMD`)}:`}</CardLabel>
-                    <TextInput className={"field"} name="lor" inputRef={register()} />
+                    <CardLabel style={{ "fontSize": "16px", "fontStyle": "bold", "fontWeight": "600" }}>{`${t(`WORKS_EMD`)}:*`}</CardLabel>
+                    <div className='field'>
+                    <TextInput  name="lor" inputRef={register({required:true})}  {...(validation = {
+                        isRequired: false,
+                        pattern: "^[0-9]*$",
+                        type: "number",
+                        title: t("WORKS_INVALID_INPUT"),
+                    })} />
+                    {errors && errors?.fileno?.type === "required" && (
+                        <CardLabelError>{t(`WORKS_REQUIRED_ERR`)}</CardLabelError>)}
+                    </div>
                 </LabelFieldPair>
 
                 <LabelFieldPair>
                     <CardLabel style={{ "fontSize": "16px", "fontStyle": "bold", "fontWeight": "600" }}>{`${t(`WORKS_CONT_PERIOD`)}:`}</CardLabel>
-                    <TextInput className={"field"} name="lor" inputRef={register()} />
+                    <TextInput className={"field"} name="lor" inputRef={register()} {...(validation = {
+                        isRequired: false,
+                        pattern: "^[0-9]*$",
+                        type: "number",
+                        title: t("WORKS_INVALID_INPUT"),
+                    })} />
                 </LabelFieldPair>
 
                 <LabelFieldPair>
-                    <CardLabel style={{ "fontSize": "16px", "fontStyle": "bold", "fontWeight": "600" }}>{`${t(`WORKS_DEFECT_LIA`)}:`}</CardLabel>
-                    <TextInput className={"field"} name="lor" inputRef={register()} />
+                    <CardLabel style={{ "fontSize": "16px", "fontStyle": "bold", "fontWeight": "600" }}>{`${t(`WORKS_DEFECT_LIA`)}:*`}</CardLabel>
+                    <div className='field'>
+                    <TextInput name="dlperiod" inputRef={register({required:true})} {...(validation = {
+                        isRequired: false,
+                        pattern: "^[0-9]*$",
+                        type: "number",
+                        title: t("WORKS_INVALID_INPUT"),
+                    })} />
+                        {errors && errors?.dlperiod?.type === "required" && (
+                            <CardLabelError>{t(`WORKS_REQUIRED_ERR`)}</CardLabelError>)}
+                    </div>
                 </LabelFieldPair>
 
                 <LabelFieldPair>
                     <CardLabel style={{ "fontSize": "16px", "fontStyle": "bold", "fontWeight": "600" }}>{`${t(`WORKS_OFFICER_INCHARGE_DES`)}:*`}</CardLabel>
+                    <div className='field'>
                     <Controller
-                        name="agencyname"
+                        name="officerInChargedesig"
                         control={control}
-                        //rules={{ required: true }}
+                        rules={{ required: true }}
                         render={(props) => {
                             return (
                                 <Dropdown
-                                    className={`field`}
                                     option={dummyData}
                                     //selected={props?.value}
                                     optionKey={"name"}
@@ -199,18 +263,22 @@ const CreateLOI = () => {
                             );
                         }}
                     />
+                        {errors && errors?.officerInChargedesig?.type === "required" && (
+                            <CardLabelError>{t(`WORKS_REQUIRED_ERR`)}</CardLabelError>)}
+                    </div>
                 </LabelFieldPair>
 
                 <LabelFieldPair>
                     <CardLabel style={{ "fontSize": "16px", "fontStyle": "bold", "fontWeight": "600" }}>{`${t(`WORKS_OFFICER_INCHARGE_NAME`)}:*`}</CardLabel>
+                    <div className='field'>
                     <Controller
-                        name="agencyname"
+                        name="officerIncharge"
                         control={control}
-                        //rules={{ required: true }}
+                        rules={{ required: true }}
                         render={(props) => {
                             return (
                                 <Dropdown
-                                    className={`field`}
+                                   
                                     option={dummyData}
                                     //selected={props?.value}
                                     optionKey={"name"}
@@ -220,9 +288,12 @@ const CreateLOI = () => {
                             );
                         }}
                     />
+                        {errors && errors?.officerIncharge?.type === "required" && (
+                            <CardLabelError>{t(`WORKS_REQUIRED_ERR`)}</CardLabelError>)}
+                    </div>
                 </LabelFieldPair>
                 {/* --------------------------------------- */}
-                
+
                 <CardSectionHeader >{t(`WORKS_RELEVANT_DOCS`)}</CardSectionHeader>
                 <LabelFieldPair>
                     <CardLabel style={{ "fontSize": "16px", "fontStyle": "bold", "fontWeight": "600 " }}>{t(`WORKS_UPLOAD_FILES`)}</CardLabel>
