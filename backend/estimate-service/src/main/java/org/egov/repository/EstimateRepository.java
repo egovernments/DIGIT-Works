449b5de4c86bc250645e008d6852db07258942b0
Pintu-eGov <pintu.kumar@egovernments.org>
2022-08-23 13:24:34 +0530

    Create estimate API : Resolved - review comments

--- backend/estimate-service/src/main/java/org/egov/repository/EstimateRepository.java
+++
@@ -1,4 +0,0 @@
-package org.egov.repository;
-
-public class EstimateRepository {
-}
--- backend/estimate-service/src/main/java/org/egov/service/EnrichmentService.java
+++
@@ -1,7 +0,0 @@
-package org.egov.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class EnrichmentService {
-}
