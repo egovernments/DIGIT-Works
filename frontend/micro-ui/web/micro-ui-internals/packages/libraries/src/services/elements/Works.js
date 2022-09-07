607e21fe8d37670bff80d45f301991f0c221c5b2
Nipun Arora <aroranipun1@gmail.com>
2022-09-07 16:46:48 +0530

    Added WorksService

---
+++ frontend/micro-ui/web/micro-ui-internals/packages/libraries/src/services/elements/Works.js
@@ -0,0 +1,33 @@
+import { Request } from "../atoms/Utils/Request";
+
+export const WorksService = {
+    createLOI: (details) =>
+        Request({
+            url: Urls.works.create,
+            data: details,
+            useCache: false,
+            setTimeParam: false,
+            userService: true,
+            method: "POST",
+            params: {},
+            auth: true,
+        }),
+    estimateSearch: ({ tenantId, filters }) =>
+        Request({
+            url: Urls.works.estimateSearch,
+            useCache: false,
+            method: "POST",
+            auth: true,
+            userService: false,
+            params: { tenantId, ...filters },
+        }),
+    loiSearch: ({ tenantId, filters }) =>
+        Request({
+            url: Urls.works.loiSearch,
+            useCache: false,
+            method: "POST",
+            auth: true,
+            userService: false,
+            params: { tenantId, ...filters },
+        })
+}
