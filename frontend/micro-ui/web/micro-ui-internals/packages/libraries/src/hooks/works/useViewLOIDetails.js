09af49fb5745706088831db38cb6cbb69ab5485a
Nipun Arora <aroranipun1@gmail.com>
2022-09-30 09:58:30 +0530

    View Loi api integration , fixed some minor issues

--- frontend/micro-ui/web/micro-ui-internals/packages/libraries/src/hooks/works/useViewLOIDetails.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/libraries/src/hooks/works/useViewLOIDetails.js
@@ -1,10 +1,11 @@
 import { WorksSearch } from "../../services/molecules/Works/Search";
 import { useQuery } from "react-query";
 
-const useViewLOIDetails = (t, tenantId, LOINumber,subEstiamteNumber, config = {}) => {
+const useViewLOIDetails = (t, tenantId, loiNumber,subEstiamteNumber, config = {}) => {
+    
     return useQuery(
-        ["LOI_WORKS_SEARCH", "LOI_SEARCH", tenantId, LOINumber, subEstiamteNumber],
-        () => WorksSearch.viewLOIScreen(t, tenantId, LOINumber, subEstiamteNumber),
+        ["LOI_WORKS_SEARCH", "LOI_SEARCH", tenantId, loiNumber, subEstiamteNumber],
+        () => WorksSearch.viewLOIScreen(t, tenantId, loiNumber, subEstiamteNumber),
         config
     );
 }
