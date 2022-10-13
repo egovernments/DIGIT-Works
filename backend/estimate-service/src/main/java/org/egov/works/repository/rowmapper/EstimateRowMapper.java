6f247b8f729db233afed1046013ef9340d30ee7c
pintu-eGov <pintu.kumar@egovernments.org>
2022-10-13 19:45:59 +0530

    estimate-service : search :: bug fixed

--- backend/estimate-service/src/main/java/org/egov/works/repository/rowmapper/EstimateRowMapper.java
+++ backend/estimate-service/src/main/java/org/egov/works/repository/rowmapper/EstimateRowMapper.java
@@ -74,16 +74,16 @@ public class EstimateRowMapper implements ResultSetExtractor<List<Estimate>> {
                     .workCategory(workCategory).auditDetails(auditDetails).build();
 
 
-            addEstimateDetails(rs, estimate, estimateMap);
+            addEstimateDetails(rs, estimate);
             estimateMap.put(id, estimate);
         }
         return new ArrayList<>(estimateMap.values());
     }
 
-    private void addEstimateDetails(ResultSet rs, Estimate estimate, Map<String, Estimate> estimateMap) throws SQLException {
+    private void addEstimateDetails(ResultSet rs, Estimate estimate) throws SQLException {
         String estDetailsId = rs.getString("estDetailId");
-        if (StringUtils.isNotBlank(estDetailsId)) {
-
+        String estimateId = rs.getString("estDetail.estimate_id");
+        if (StringUtils.isNotBlank(estimateId) && estimateId.equalsIgnoreCase(estimate.getId().toString())) {
             EstimateDetail estimateDetail = EstimateDetail.builder()
                     .id(UUID.fromString(estDetailsId))
                     .estimateDetailNumber(rs.getString("estimate_detail_number"))
@@ -94,14 +94,14 @@ public class EstimateRowMapper implements ResultSetExtractor<List<Estimate>> {
             JsonNode additionalDetails = getAdditionalDetail("estDetailAdditional", rs);
             estimateDetail.setAdditionalDetails(additionalDetails);
 
-            if (estimateMap.containsKey(estDetailsId)) {
-                estimateMap.get(estDetailsId).getEstimateDetails().add(estimateDetail);
-            } else {
+            if (estimate.getEstimateDetails() == null || estimate.getEstimateDetails().isEmpty()) {
                 List<EstimateDetail> estimateDetailList = new LinkedList<>();
                 estimateDetailList.add(estimateDetail);
-
                 estimate.setEstimateDetails(estimateDetailList);
+            } else {
+                estimate.getEstimateDetails().add(estimateDetail);
             }
+
         }
     }
 
