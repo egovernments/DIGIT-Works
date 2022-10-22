da27cfcd81d9c3abfeb0088c51bc993e6379be4d
pintu-eGov <pintu.kumar@egovernments.org>
2022-10-22 15:44:21 +0530

    PFM-1192: works inbox service : temp fix for estimate service url

--- backend/works-inbox-service/src/main/java/org/egov/inbox/service/InboxService.java
+++ backend/works-inbox-service/src/main/java/org/egov/inbox/service/InboxService.java
@@ -331,11 +331,7 @@ public class InboxService {
             throw new CustomException(ErrorConstants.INVALID_MODULE_SEARCH_PATH,
                     "search path not configured for the businessService : " + businessServiceName);
         }
-        //TODO-start : check how is configured in other inbox service
-        String searchModuleUrl = srvMap.get("searchPath");
-        searchModuleUrl = searchModuleUrl.replace("http://localhost:8080",config.getWorkflowHost());
-        //TODO-end
-        StringBuilder url = new StringBuilder(searchModuleUrl);
+        StringBuilder url = new StringBuilder(srvMap.get("searchPath"));
         url.append("?tenantId=").append(tenantId);
 
         Set<String> searchParams = moduleSearchCriteria.keySet();
