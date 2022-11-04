40f319863c16bae9f570384164c9d48818f53a61
Nipun Arora <aroranipun1@gmail.com>
2022-11-04 11:45:09 +0530

    Attendence mgmt initial setup done

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/AttendenceMgmt/src/components/AttendenceMgmtCard.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/AttendenceMgmt/src/components/AttendenceMgmtCard.js
@@ -1,4 +1,4 @@
-import { EmployeeModuleCard, ArrowRightInbox, WSICon } from "@egovernments/digit-ui-react-components";
+import { EmployeeModuleCard, ArrowRightInbox, WSICon,ComplaintIcon } from "@egovernments/digit-ui-react-components";
 import React from "react";
 import { useTranslation } from "react-i18next";
 
@@ -19,7 +19,7 @@ const AttendenceMgmtCard = () => {
     // links = links.filter((link) => (link.roles ? checkForEmployee(link.roles) : true));
 
     const propsForModuleCard = {
-        Icon: <WSICon />,
+        Icon: <ComplaintIcon fill="white"/>,
         moduleName: t("AttendenceMgmt"),
         kpis: [
             {
@@ -31,45 +31,25 @@ const AttendenceMgmtCard = () => {
         ],
         links: [
             {
-                label: t("WORKS_ESTIMATES"),
-                link: `/${window?.contextPath}/employee/works/inbox`,
+                label: t("ORG_MGMT"),
+                link: `/${window?.contextPath}/employee/attendencemgmt/inbox`,
                 roles: ["EST_CREATOR", "EST_CHECKER", "EST_TECH_SANC", "EST_FIN_SANC", "EMPLOYEE"],
             },
             {
-                label: t("WORKS_LOI"),
-                link: `/${window?.contextPath}/employee/works/loiinbox`,
-                roles: ["LOI CHECKER", "LOI APPROVER", "LOI CREATOR", "EMPLOYEE"],
+                label: t("IND_MGMT"),
+                link: `/${window?.contextPath}/employee/attendencemgmt/inbox`,
+                roles: ["EST_CREATOR", "EST_CHECKER", "EST_TECH_SANC", "EST_FIN_SANC", "EMPLOYEE"],
             },
             {
-                label: t("WORKS_MILESSTONES"),
-                link: `/${window?.contextPath}/employee`,
-                roles: ["WS_CEMP", "WS_APPROVER", "WS_FIELD_INSPECTOR", "WS_DOC_VERIFIER", "WS_CLERK"],
+                label: t("ATTENDENCE_APPROVAL"),
+                link: `/${window?.contextPath}/employee/attendencemgmt/inbox`,
+                roles: ["EST_CREATOR", "EST_CHECKER", "EST_TECH_SANC", "EST_FIN_SANC", "EMPLOYEE"],
             },
             {
-                label: t("WORKS_PAY_CALENDAR"),
-                link: `/${window?.contextPath}/employee`,
-                roles: ["WS_CEMP", "WS_APPROVER", "WS_FIELD_INSPECTOR", "WS_DOC_VERIFIER", "WS_CLERK"],
-            },
-            {
-                label: t("WORKS_CONTRACTOR_BILL"),
-                link: `/${window?.contextPath}/employee/works/create-contractor`,
-                roles: ["WS_CEMP", "WS_APPROVER", "WS_FIELD_INSPECTOR", "WS_DOC_VERIFIER", "WS_CLERK"],
-            },
-            {
-                label: t("WORKS_REPORTS"),
-                link: `/${window?.contextPath}/employee/`,
-                roles: ["WS_CEMP", "WS_APPROVER", "WS_FIELD_INSPECTOR", "WS_DOC_VERIFIER", "WS_CLERK"],
-            },
-            {
-                label: t("WORKS_MASTERS"),
-                link: `/${window?.contextPath}/employee/`,
-                roles: ["WS_CEMP", "WS_APPROVER", "WS_FIELD_INSPECTOR", "WS_DOC_VERIFIER", "WS_CLERK"],
-            },
-            {
-                label: t("WORKS_DASHBOARD"),
-                link: `/${window?.contextPath}/employee/`,
-                roles: ["WS_CEMP", "WS_APPROVER", "WS_FIELD_INSPECTOR", "WS_DOC_VERIFIER", "WS_CLERK"],
-            },
+                label: t("ATMGMT_DASHBOADR"),
+                link: `/${window?.contextPath}/employee/attendencemgmt/inbox`,
+                roles: ["EST_CREATOR", "EST_CHECKER", "EST_TECH_SANC", "EST_FIN_SANC", "EMPLOYEE"],
+            }    
         ],
     };
     return <EmployeeModuleCard {...propsForModuleCard} />;
