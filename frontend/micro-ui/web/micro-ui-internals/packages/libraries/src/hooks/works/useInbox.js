adf45d7139f2c28e3a7678b2f33c97d8759f60e6
burhan j <burhan.j@protonshub.com>
2022-11-11 15:46:35 +0530

    Inbox Search Params Added

--- frontend/micro-ui/web/micro-ui-internals/packages/libraries/src/hooks/works/useInbox.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/libraries/src/hooks/works/useInbox.js
@@ -2,32 +2,39 @@ import useInbox from "../useInbox";
 
 
 const useWorksInbox = ({ tenantId, _filters, config }) => {
-
-    //you can take all the parameters from here
-    const { estimateNumber,limit,offset,...rest } = _filters
+    const { estimateNumber, 
+        department, 
+        typeOfWork,
+        fund,
+        budgetHead,
+        fromProposalDate,
+        toProposalDate,
+        sortOrder,
+        limit,
+        offset,
+        ...rest } = _filters
     const USER_UUID = Digit.UserService.getUser()?.info?.uuid;
 
     const filters = {
         tenantId,
         processSearchCriteria: {
-            moduleName: "estimate-service",
             tenantId,
-            businessIds:[
-                estimateNumber
-            ],
             businessService: [
                 "estimate-approval-2"
             ],
-            // uuid:USER_UUID,
+            moduleName: "estimate-service",
+            assignee:USER_UUID,
         },
         moduleSearchCriteria: {
-            // ...(mobileNumber ? { mobileNumber } : {}),
-            // ...(applicationNumber ? { applicationNumber } : {}),
-            // ...(sortBy ? { sortBy } : {}),
-            // ...(sortOrder ? { sortOrder } : {}),
-            // ...(locality?.length > 0 ? { locality: locality.map((item) => item.code.split("_").pop()).join(",") } : {}),
-
-            // @Burhan Add all the search params here. I have not added because pintu has not configured all of them currently
+            department,
+            typeOfWork,
+            fund,
+            function: rest.function,
+            budgetHead,
+            estimateNumber,
+            fromProposalDate,
+            toProposalDate,
+            sortOrder
         },
         limit,
         offset
