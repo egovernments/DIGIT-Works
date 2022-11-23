9ce06cca427bb5ca77af56a2f7c218d3273d355b
Nipun Arora <aroranipun1@gmail.com>
2022-11-23 13:34:32 +0530

    Added view project screens (#123)

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/AttendenceMgmt/src/pages/citizen/manageWageSeekers/ViewProject.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/AttendenceMgmt/src/pages/citizen/manageWageSeekers/ViewProject.js
@@ -1,15 +1,17 @@
-import { Card, StatusTable, Row, DetailsCard, SearchIcon, Dropdown, SearchableDropdown,Table } from '@egovernments/digit-ui-react-components'
+import { Card, StatusTable, Row, DetailsCard, SearchIcon, Dropdown, SearchableDropdown,Table, DeleteIcon,SubmitBar,Toast} from '@egovernments/digit-ui-react-components'
 import React,{useMemo,useState} from 'react'
 import { useTranslation } from 'react-i18next'
+import { useQuery } from 'react-query'
 import { Link, useLocation } from "react-router-dom"
+import useDebounce from '../../../pageComponents/useDebounce'
 
 const sampleTableData = {
-  rows:[
+  rows: [
     {
-      "name":"Name A",
-      "aadhar":"1223-1223-1221",
-      "acno":"1213-1241-2121",
-      "ifsc":"3242-3423-4322"
+      "name": "Name A",
+      "aadhar": "1223-1223-1220",
+      "acno": "1213-1241-2121",
+      "ifsc": "3242-3423-4322"
     },
     {
       "name": "Name B",
@@ -19,51 +21,94 @@ const sampleTableData = {
     },
     {
       "name": "Name C",
-      "aadhar": "1223-1223-1221",
+      "aadhar": "1223-1223-1222",
       "acno": "1223-1223-1221",
       "ifsc": "1223-1223-1221"
     },
     {
       "name": "Name D",
-      "aadhar": "1223-1223-1221",
+      "aadhar": "1223-1223-1223",
       "acno": "1223-1223-1221",
       "ifsc": "1223-1223-1221"
     },
     {
       "name": "Name E",
-      "aadhar": "1223-1223-1221",
+      "aadhar": "1223-1223-1224",
       "acno": "1223-1223-1221",
       "ifsc": "1223-1223-1221"
     },
     {
       "name": "Name F",
-      "aadhar": "1223-1223-1221",
+      "aadhar": "1223-1223-1225",
       "acno": "1223-1223-1221",
       "ifsc": "1223-1223-1221"
     },
     {
       "name": "Name G",
-      "aadhar": "1223-1223-1221",
+      "aadhar": "1223-1223-1226",
       "acno": "1223-1223-1221",
       "ifsc": "1223-1223-1221"
     },
     {
       "name": "Name H",
-      "aadhar": "1223-1223-1221",
+      "aadhar": "1223-1223-1227",
       "acno": "1223-1223-1221",
       "ifsc": "1223-1223-1221"
     }
   ]
 }
 
+function useTodos(page, search) {
+  let url = `https://jsonplaceholder.typicode.com/todos?_page=${page}`;
+  if (!!search) {
+    url += `&q=${search}`;
+  }
+
+  // see https://react-query.tanstack.com/guides/important-defaults
+  // see https://react-query.tanstack.com/guides/paginated-queries
+  return useQuery(
+    ["todos", { page, search }],
+    () => fetch(url).then((res) => res.json())
+    // the following can be used to avoid refetches on already fetched data (see paginated queries docs)
+    // { keepPreviousData: true, staleTime: 5 * 60 * 1000 }
+  );
+}
+
 const ViewProject = ({isLoading=false,resultOk=true,data={}}) => {
+
+  const [tableRow, setTableRow] = useState(sampleTableData.rows)
   const state  = useLocation()
   const { t } = useTranslation()
-  const [service, setService] = useState([]);
+  const [service, setService] = useState(null);
+  console.log(service);
   const [showExpander,setShowExpander] = useState(false)
   const [isExpanded, setIsExpanded] = useState(true)
   const [wageSeekers,setWageSeekers] = useState([])
+  const [showToast,setShowToast] = useState(null)
+  const [search, setSearch] = useState("");
+
+  const debouncedSearch = useDebounce(search, 500);
+
+  const { isFetching, isError, data:responseData, error } = useTodos(0, debouncedSearch);
+  
   data = [[]]
+
+  const handleDeleteRow = ({ value, column, row }) => {
+    setShowToast({label:"ATM_WAGE_SEEKERS_REMOVED"});
+    setTableRow((prevState)=>prevState.filter(tableRow => tableRow.aadhar !== row.original.aadhar))
+  }
+
+  const handleSubmit = () => {
+    setShowToast({ label: "ATM_WAGE_SEEKERS_ADDED" });
+    console.log("project submitted, wage seekers added");
+  }
+
+  const closeToast = () => {
+    setShowToast(null);
+  };
+  setTimeout(() => {
+    closeToast();
+  }, 10000); 
 
   const tableColumns = useMemo(()=>{
     return [
@@ -93,6 +138,13 @@ const ViewProject = ({isLoading=false,resultOk=true,data={}}) => {
         accessor: "ifsc",
         Cell: ({ value, column, row }) => {
           return String(t(value));
+        }
+      },
+      {
+        Header: t(""),
+        accessor: "delete",
+        Cell: ({ value, column, row }) => {
+          return <span onClick={()=>handleDeleteRow({value,column,row})}> <DeleteIcon fill={"#B1B4B6"} style={{ "margin": "auto" }} /></span>
         }
       },
     ]
@@ -148,7 +200,7 @@ const ViewProject = ({isLoading=false,resultOk=true,data={}}) => {
       </Card>
 
       <div className='card' style={{"padding":"0px"}}>
-        <SearchableDropdown showSearchIcon={true} noBorder={true} placeholder={t("ATM_SEARCH_WAGE_SEEKERS_PLACEHOLDER")} option={sampleOptions} optionKey={"name"} selected={service} select={setService}  />
+        <SearchableDropdown showSearchIcon={true} noBorder={true} placeholder={t("ATM_SEARCH_WAGE_SEEKERS_PLACEHOLDER")} option={responseData} optionKey={"title"} selected={service} select={setService} value={search} setValue={setSearch} tableRow={tableRow} setTableRow={setTableRow} />
       </div>
 
       {/* Render the table here */}
@@ -163,8 +215,8 @@ const ViewProject = ({isLoading=false,resultOk=true,data={}}) => {
           // globalSearch={filterValue}
           initSortId="S N "
           // onSearch={onSearch}
-          data={sampleTableData.rows}
-          totalRecords={sampleTableData.rows.length}
+          data={tableRow}
+          totalRecords={tableRow.length}
           columns={tableColumns}
           isPaginationRequired={false}
           getCellProps={(cellInfo) => {
@@ -174,6 +226,11 @@ const ViewProject = ({isLoading=false,resultOk=true,data={}}) => {
           }}
         />
       </div>
+      <div style={{margin:"1rem 8px"}}>
+      <SubmitBar onSubmit={handleSubmit} label={t("ATM_SUBMIT")} />
+      </div>
+
+      {showToast && <Toast label={t(showToast.label)} onClose={closeToast} />}
     </React.Fragment>
   )
 }
