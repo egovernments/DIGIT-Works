5f145c9b47a4ccfd8f9b9737bc09fb7c40e11215
prachirane-eGov <prachi.rane@egovernments.org>
2022-12-05 12:32:52 +0530

    [PFM-1303] | [Prachi] | Review comments: passing only req props to Card inside Formcomposer

--- frontend/micro-ui/web/micro-ui-internals/packages/react-components/src/hoc/FormComposer.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/react-components/src/hoc/FormComposer.js
@@ -281,7 +281,7 @@ export const FormComposer = (props) => {
   };
   return (
     <form onSubmit={handleSubmit(onSubmit)} onKeyDown={(e) => checkKeyDown(e)} id={props.formId} className={props.className}>
-      <Card style={getCardStyles()} className={props?.className} {...props}>
+      <Card style={getCardStyles()} className={props?.className} noCardStyle={props.noCardStyle}>
         {!props.childrenAtTheBottom && props.children}
         {props.heading && <CardSubHeader style={{ ...props.headingStyle }}> {props.heading} </CardSubHeader>}
         {props.description && <CardLabelDesc> {props.description} </CardLabelDesc>}
