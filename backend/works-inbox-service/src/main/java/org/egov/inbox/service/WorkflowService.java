1c942ff3c8af906e20b133c517a788a1c008e467
Rohit Shende <rohitshende@Rohits-MacBook-Air.local>
2022-12-07 08:39:27 +0530

    PFM-1700: Added v2 controller and its services

--- backend/works-inbox-service/src/main/java/org/egov/inbox/service/WorkflowService.java
+++ backend/works-inbox-service/src/main/java/org/egov/inbox/service/WorkflowService.java
@@ -15,6 +15,7 @@ import org.egov.common.contract.request.Role;
 import org.egov.inbox.config.InboxConfiguration;
 import org.egov.inbox.repository.ServiceRequestRepository;
 import org.egov.inbox.util.ErrorConstants;
+import org.egov.inbox.web.model.InboxRequest;
 import org.egov.inbox.web.model.RequestInfoWrapper;
 import org.egov.inbox.web.model.workflow.BusinessService;
 import org.egov.inbox.web.model.workflow.BusinessServiceResponse;
@@ -23,6 +24,7 @@ import org.egov.inbox.web.model.workflow.ProcessInstanceSearchCriteria;
 import org.egov.tracer.model.CustomException;
 import org.egov.inbox.web.model.workflow.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 import org.springframework.util.ObjectUtils;
@@ -305,6 +307,53 @@ public class WorkflowService {
 		});
 		return stateToRolesMap;
 	}
+
+	@Cacheable(value="businessServices")
+	public List<BusinessService> getBusinessServices(InboxRequest request) {
+		String tenantId = request.getInbox().getTenantId();
+		RequestInfo requestInfo = request.getRequestInfo() ;
+		List<String> businessServicesCodes = request.getInbox().getProcessSearchCriteria().getBusinessService();
+		String businessServiceList = String.join(",",businessServicesCodes);
+		StringBuilder url = getSearchURLWithParams(tenantId, businessServiceList);
+		RequestInfoWrapper requestInfoWrapper = RequestInfoWrapper.builder().requestInfo(requestInfo).build();
+		Object result = serviceRequestRepository.fetchResult(url, requestInfoWrapper);
+		BusinessServiceResponse response = null;
+		try {
+			response = mapper.convertValue(result, BusinessServiceResponse.class);
+		} catch (IllegalArgumentException e) {
+			throw new CustomException(ErrorConstants.PARSING_ERROR, "Failed to parse response of Workflow");
+		}
+		return response.getBusinessServices();
+	}
+
+	public Map<String,String> getStatusIdToBusinessServiceMap(List<BusinessService> businessServices){
+
+		Map<String,String> statusIdToBusinessServiceMap = new HashMap<>();
+
+		businessServices.forEach(businessService -> {
+			businessService.getStates().forEach(state -> {
+						statusIdToBusinessServiceMap.put(state.getUuid(), businessService.getBusinessService());
+					}
+			);
+		});
+
+		return statusIdToBusinessServiceMap;
+	}
+
+	public Map<String,String> getApplicationStatusIdToStatusMap(List<BusinessService> businessServices){
+
+		Map<String,String> statusIdToApplicationStatusMap = new HashMap<>();
+
+		businessServices.forEach(businessService -> {
+			businessService.getStates().forEach(state -> {
+						statusIdToApplicationStatusMap.put(state.getUuid(), state.getApplicationStatus());
+					}
+			);
+		});
+
+		return statusIdToApplicationStatusMap;
+	}
+
 
 
 }
