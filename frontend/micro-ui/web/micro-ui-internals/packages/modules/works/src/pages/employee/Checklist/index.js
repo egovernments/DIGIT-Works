1a7a512ae4612b4a6be847365d8b9102e11d0bb3
Nipun Arora <aroranipun1@gmail.com>
2022-12-08 15:59:17 +0530

    pfm-1231 pfm-1207 (#162)
    * Added Inbox link
    
    * Checklist Static Screen pfm-1207
    
    * Css changes
    
    * added closure checklist
    
    * form composer enhancements for showing multiple cards
    
    * Update index.js

---
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/works/src/pages/employee/Checklist/index.js
@@ -0,0 +1,40 @@
+import { FormComposer,Header } from '@egovernments/digit-ui-react-components'
+import React, { useState } from 'react'
+
+import { useTranslation } from 'react-i18next'
+import { configChecklist } from './config'
+const Checklist = (props) => {
+    const { t } = useTranslation()
+    const onSubmit = (data) => {
+        console.log("form submitted",data);
+    }
+
+    const [showForm, setShowForm] = useState(false)
+
+  return (
+    <React.Fragment>
+        <Header styles={{ marginLeft: "15px" }}>{t("WORKS_CHECKLIST")}</Header>
+        <FormComposer
+            //heading={t("WORKS_KICKOFF_CHECKLIST")}
+            label={t("CS_ACTION_DISPOSE")}
+            //description={"Sample Description"}
+            //text={"Sample Text"}
+            config={configChecklist(t,setShowForm,showForm).form.map((config) => {
+                return {
+                    ...config,
+                    body: config?.body?.filter((a) => !a.hideInEmployee),
+                };
+            })}
+            defaultValues={{}}
+            onSubmit={onSubmit}
+            fieldStyle={{ marginLeft: "0.5rem" }}
+            showWrapperContainers={true}
+            isDescriptionBold={true}
+            noBreakLine={true}
+            showMultipleCards={true}
+        />
+    </React.Fragment>
+  )
+}
+
+export default Checklist
