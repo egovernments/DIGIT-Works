8f1b15d14220307cf63627d23e87c185c15ebeba
Ramkrishna-egov <85437265+Ramkrishna-egov@users.noreply.github.com>
2022-12-08 11:18:05 +0530

    Works-SHG Initial setup (#147)
    * Update CODEOWNERS
    
    * PFM-1340 Added entry for attendance service in builds config
    
    * Update CODEOWNERS
    
    * PFM-1341: Added entry for muster roll service in build config file
    
    * Works-SHG Initial setup
    
    * Delete .idea directory
    
    * Update build-config.yml
    
    * Update build-config.yml
    
    Co-authored-by: Gowtham-egov <112694085+Gowtham-egov@users.noreply.github.com>
    Co-authored-by: Rohit Shende <rohitshende@Rohits-MacBook-Air.local>
    Co-authored-by: nikesh-eGov <48427967+nikesh-eGov@users.noreply.github.com>
    Co-authored-by: subhashini-egov <107110442+subhashini-egov@users.noreply.github.com>
    Co-authored-by: Jagankumar <53823168+jagankumar-egov@users.noreply.github.com>

---
+++ frontend/works_shg_app/android/app/src/main/kotlin/com/works/shg/works_shg_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.works.shg.works_shg_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ frontend/works_shg_app/packages/digit_components/lib/widgets/widgets.dart
@@ -0,0 +1,8 @@
+export './digit_card.dart';
+export './digit_checkbox_tile.dart';
+export './digit_elevated_button.dart';
+export './digit_text_field.dart';
+export './scrollable_content.dart';
+export './powered_by_digit.dart';
+export './labeled_field.dart';
+export './digit_dialog.dart';
