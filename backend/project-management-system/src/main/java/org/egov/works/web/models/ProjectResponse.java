4be1ea10572cc3b3ac768483d11d788a2fb69fa5
chetankolte <chetan.kolte@egovernments.org>
2022-12-13 14:14:42 +0530

    Change import to common contract

--- backend/project-management-system/src/main/java/org/egov/works/web/models/ProjectResponse.java
+++ backend/project-management-system/src/main/java/org/egov/works/web/models/ProjectResponse.java
@@ -2,6 +2,7 @@ package org.egov.works.web.models;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
+import org.egov.common.contract.request.RequestInfo;
 import org.springframework.validation.annotation.Validated;
 
 import javax.validation.Valid;
