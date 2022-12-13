88860fef742f43498a43cd3a387b58ddbefea930
Jagankumar <53823168+jagankumar-egov@users.noreply.github.com>
2022-12-13 17:49:57 +0530

    Update webpack.config.js

--- frontend/micro-ui/web/webpack.config.js
+++ frontend/micro-ui/web/webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
   output: {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "build"),
-    publicPath: "/digit-ui/",
+    publicPath: "/core-ui/",
   },
   optimization: {
     splitChunks: {
