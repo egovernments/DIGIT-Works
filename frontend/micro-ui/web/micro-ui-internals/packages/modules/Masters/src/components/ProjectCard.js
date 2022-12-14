9b9615dbb6ba4496a815a9927dcdc874b322b3b1
Jagankumar <53823168+jagankumar-egov@users.noreply.github.com>
2022-12-14 09:30:09 +0530

    PFM-1707 :: UI Formatted the works code, created new module Estimate, and updated the links in home screen  (#185)
    * PFM-1707 :: Created a seperate module for estimate
    
    * PFM-1707 :: Estimate contracts and project seperate cards created to home screen
    
    * formatted
    
    * Update package.json
    
    * PFM-1707 :: created expenditure module
    
    * PFM-1707 :: Updated the icons and links in home screen

---
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/Masters/src/components/ProjectCard.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { useTranslation } from "react-i18next";
+import { EmployeeModuleCard, OBPSIconSolidBg } from "@egovernments/digit-ui-react-components";
+
+const ProjectCard = () => {
+  const { t } = useTranslation();
+
+  const propsForModuleCard = {
+    Icon: <OBPSIconSolidBg />,
+    moduleName: t("WORKS_CHECKLIST"),
+    kpis: [],
+    links: [
+      {
+        label: t("WORKS_KICKOFF_CHECKLIST"),
+        link: `/${window?.contextPath}/employee/works/checklist`,
+        roles: ["WS_CEMP", "WS_APPROVER", "WS_FIELD_INSPECTOR", "WS_DOC_VERIFIER", "WS_CLERK"],
+        count: 21,
+      },
+      {
+        label: t("ACTION_TEST_VIEW_WORK_ORDER"),
+        link: `/${window?.contextPath}/employee/works/checklist`,
+        roles: ["EST_CREATOR", "EST_CHECKER", "EST_TECH_SANC", "EST_FIN_SANC", "EMPLOYEE"],
+      },
+    ],
+  };
+  return <EmployeeModuleCard {...propsForModuleCard} />;
+};
+
+export default ProjectCard;
