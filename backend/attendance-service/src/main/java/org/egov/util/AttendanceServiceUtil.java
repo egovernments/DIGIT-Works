8f25fb69d79af3dc8710539a315f44106674b8fc
chetankolte <chetan.kolte@egovernments.org>
2022-12-15 08:29:35 +0530

    Remove loop db calls, Refactor code

--- backend/attendance-service/src/main/java/org/egov/util/AttendanceServiceUtil.java
+++ backend/attendance-service/src/main/java/org/egov/util/AttendanceServiceUtil.java
@@ -6,12 +6,12 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class AttendanceServiceUtil {
-    public AuditDetails getAuditDetails(String by, AttendanceRegister attendanceRegister, Boolean isCreate) {
+    public AuditDetails getAuditDetails(String by, AuditDetails auditDetails, Boolean isCreate) {
         Long time = System.currentTimeMillis();
         if (isCreate)
             return AuditDetails.builder().createdBy(by).lastModifiedBy(by).createdTime(time).lastModifiedTime(time).build();
         else
-            return AuditDetails.builder().createdBy(attendanceRegister.getAuditDetails().getCreatedBy()).lastModifiedBy(by)
-                    .createdTime(attendanceRegister.getAuditDetails().getCreatedTime()).lastModifiedTime(time).build();
+            return AuditDetails.builder().createdBy(auditDetails.getCreatedBy()).lastModifiedBy(by)
+                    .createdTime(auditDetails.getCreatedTime()).lastModifiedTime(time).build();
     }
 }
