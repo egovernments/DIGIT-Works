22dbbc1fea0c171c662929e4e13499719001f16a
Nipun Arora <aroranipun1@gmail.com>
2022-12-16 15:36:31 +0530

    Added dummy data (#197)

--- frontend/micro-ui/web/micro-ui-internals/packages/libraries/src/hooks/works/useKickoffInbox.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/libraries/src/hooks/works/useKickoffInbox.js
@@ -19,7 +19,7 @@ const useKickoffInbox = ({ tenantId, _filters, config }) => {
         processSearchCriteria: {
             tenantId,
             businessService: [
-                "estimate-approval-2"
+                "estimate-approval"
             ],
             moduleName: "estimate-service",
         },
@@ -43,23 +43,12 @@ const useKickoffInbox = ({ tenantId, _filters, config }) => {
                 statuses: data.statusMap,
                 table: data?.items.map(application => {
                     const obj = {
-                        // estimateNumber: application?.ProcessInstance?.businessId,
-                        // department: application?.businessObject?.additionalDetails?.formData?.department?.code,
-                        // fund: application?.businessObject?.additionalDetails?.formData?.fund?.code,
-                        // function: application?.businessObject?.additionalDetails?.formData?.function?.code,
-                        // budgetHead: application?.businessObject?.additionalDetails?.formData?.budgetHead?.code,
-                        // createdBy: application?.businessObject?.additionalDetails?.createdBy,
-                        // status: application?.ProcessInstance?.state?.applicationStatus,
-                        // owner: application?.businessObject?.additionalDetails?.owner,
-                        // totalAmount: 999,
-                        //total amount is static for now(not getting response from inbox api so all the fields are currently showing from user input form only)
                         workOrderId:"1136/TO/DB/FLOOD/10-11",
                         woIssuedDate:"08/09/2010",
                         woAcceptedDate:"08/09/2010",
                         nameOfWork:"Providing CC Drain in Birla Gaddah(akshaynagar colony) in 27th ward",
                         agencyName:"S.K. Om Birla",
                         status:"complete"
-
                         //sending dummy data(here for now I'm calling estimates inbox just to test the inbox table, for every item in estimate inbox respnse I'm showing the same dummy data)
                     }
 
