858d5edfc9b9dee4a15111baf1ba476e150e1a21
pintu-eGov <pintu.kumar@egovernments.org>
2022-12-26 14:16:13 +0530

    PFM-1748 : Fixed the update estimate issue

--- backend/estimate-service/src/main/java/org/egov/works/repository/EstimateRepository.java
+++ backend/estimate-service/src/main/java/org/egov/works/repository/EstimateRepository.java
@@ -1,7 +1,6 @@
 package org.egov.works.repository;
 
 
-import org.egov.common.contract.request.RequestInfo;
 import org.egov.works.repository.rowmapper.EstimateQueryBuilder;
 import org.egov.works.repository.rowmapper.EstimateRowMapper;
 import org.egov.works.web.models.Estimate;
@@ -32,6 +31,9 @@ public class EstimateRepository {
      */
     public List<Estimate> getEstimate(EstimateSearchCriteria searchCriteria) {
         List<Object> preparedStmtList = new ArrayList<>();
+        if (searchCriteria.getIsCountCall() == null) {
+            searchCriteria.setIsCountCall(Boolean.FALSE);
+        }
         String query = queryBuilder.getEstimateQuery(searchCriteria, preparedStmtList);
         List<Estimate> estimateList = jdbcTemplate.query(query, rowMapper, preparedStmtList.toArray());
         return estimateList;
