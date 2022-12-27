e073013061a39242b0275a5b91d2ba7173b2e38d
chetankolte <chetan.kolte@egovernments.org>
2022-12-27 09:19:51 +0530

    Add project api enrichment
    - Added project enrichment and Project service util

---
+++ backend/project-management-system/src/main/java/org/egov/works/enrichment/ProjectEnrichment.java
@@ -0,0 +1,51 @@
+package org.egov.works.enrichment;
+
+import digit.models.coremodels.AuditDetails;
+import org.egov.common.contract.request.RequestInfo;
+import org.egov.works.util.ProjectServiceUtil;
+import org.egov.works.web.models.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class ProjectEnrichment {
+
+    @Autowired
+    private ProjectServiceUtil projectServiceUtil;
+
+    public void enrichCreateProject(ProjectRequest request) {
+        RequestInfo requestInfo = request.getRequestInfo();
+        List<Project> projects = request.getProjects();
+
+        for (Project project: projects) {
+
+            AuditDetails auditDetails = projectServiceUtil.getAuditDetails(requestInfo.getUserInfo().getUuid(), null, true);
+            project.setAuditDetails(auditDetails);
+            project.setId(UUID.randomUUID().toString());
+
+            for (Target target: project.getTargets()) {
+                target.setId(UUID.randomUUID().toString());
+                target.setAuditDetails(auditDetails);
+            }
+
+            for (Document document: project.getDocuments()) {
+                document.setId(UUID.randomUUID().toString());
+                document.setAuditDetails(auditDetails);
+            }
+
+            project.getAddress().setAuditDetails(auditDetails);
+            project.getAddress().setId(UUID.randomUUID().toString());
+
+            project.getAddress().getLocality().setId(UUID.randomUUID().toString());
+
+            List<Boundary> children = project.getAddress().getLocality().getChildren();
+            for (Boundary child: children) {
+                child.setId(UUID.randomUUID().toString());
+            }
+        }
+
+    }
+}
