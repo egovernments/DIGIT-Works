15faae92548c5f9c92eb46ab6281146c05aa1c03
Hariom Sinha <hariom.sinha@egovernments.org>
2023-01-03 13:11:22 +0530

    added no results found label

--- frontend/micro-ui/web/micro-ui-internals/packages/react-components/src/atoms/NoResultsFound.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/react-components/src/atoms/NoResultsFound.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { NoResultsFoundIcon } from "./svgindex";
+import { useTranslation } from "react-i18next";
 
 const NoResultsFound = () => {
+    const {t} = useTranslation();
     return (
         <div className="no-data-found">
               <NoResultsFoundIcon />
-              <span className="error-msg">No Result Found</span>
+              <span className="error-msg">{t("COMMON_NO_RESULTS_FOUND")}</span>
         </div>
     )
 }
