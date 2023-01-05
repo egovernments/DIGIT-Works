cb27e147144038ec0993667e6a0c75008e8f07e3
Hariom Sinha <hariom.sinha@egovernments.org>
2023-01-05 13:31:39 +0530

    updated project card and config for checklist

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/Project/src/components/ProjectCard.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/Project/src/components/ProjectCard.js
@@ -15,6 +15,27 @@ const ProjectCard = () => {
         link: `/${window?.contextPath}/employee/project/create-project`,
         roles: [],
       },
+      {
+        label: t("WORKS_KICKOFF_CHECKLIST"),
+        link: `/${window?.contextPath}/employee/works/checklist`,
+        roles: ["WS_CEMP", "WS_APPROVER", "WS_FIELD_INSPECTOR", "WS_DOC_VERIFIER", "WS_CLERK"],
+        count: 21,
+      },
+      {
+        label: t("ACTION_TEST_VIEW_WORK_ORDER"),
+        link: `/${window?.contextPath}/employee/works/checklist`,
+        roles: ["EST_CREATOR", "EST_CHECKER", "EST_TECH_SANC", "EST_FIN_SANC", "EMPLOYEE"],
+      },
+      {
+        label: t("WORKS_CHECKLIST_INBOX"),
+        link: `/${window?.contextPath}/employee/works/checklistinbox`,
+        roles: ["WS_CEMP", "WS_APPROVER", "WS_FIELD_INSPECTOR", "WS_DOC_VERIFIER", "WS_CLERK"],
+      },
+      {
+        label: t("WORKS_PROJECT_CLOSURE"),
+        link: `/${window?.contextPath}/employee/works/projectclosure`,
+        roles: [],
+      },
     ],
   };
   return <EmployeeModuleCard {...propsForModuleCard} />;
