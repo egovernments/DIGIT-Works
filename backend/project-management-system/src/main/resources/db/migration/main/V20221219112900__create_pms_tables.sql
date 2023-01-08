2142bac11c682f0c233ed69727c405024f29ec8d
chetankolte <chetan.kolte@egovernments.org>
2023-01-09 00:02:12 +0530

    Update column names, add columns
    - Update column names to readable columns. Seperated by "_"
    - Add project_number
    - Add project name colum
    - Remove locality table
    - Add locality column in address table
    - Update indices columns according to change in column names

--- backend/project-management-system/src/main/resources/db/migration/main/V20221219112900__create_pms_tables.sql
+++ backend/project-management-system/src/main/resources/db/migration/main/V20221219112900__create_pms_tables.sql
@@ -1,46 +1,49 @@
 CREATE TABLE eg_pms_project(
 id                           character varying(64),
-tenantid                     character varying(64) NOT NULL,
-projecttype                  character varying(64),
-projectsubtype               character varying(64),
+tenant_id                    character varying(64) NOT NULL,
+project_number               character varying(128) NOT NULL,
+name                         character varying(128) NOT NULL,
+project_type                 character varying(64) NOT NULL,
+project_subtype              character varying(64),
 department                   character varying(64),
 description                  character varying(256),
 reference_id                 character varying(100),
-startdate                    bigint NOT NULL,
-enddate                      bigint,
-istaskenabled                boolean,
+start_date                   bigint NOT NULL,
+end_date                     bigint,
+is_task_enabled              boolean,
 parent                       character varying(64),
-additionaldetails            JSONB,
-isdeleted                    boolean,
-rowversion                   numeric,
-createdby                    character varying(64)  NOT NULL,
-lastmodifiedby               character varying(64),
-createdtime                  bigint,
-lastmodifiedtime             bigint,
+additional_details           JSONB,
+is_deleted                   boolean,
+row_version                  numeric,
+created_by                   character varying(64)  NOT NULL,
+last_modified_by             character varying(64),
+created_time                 bigint,
+last_modified_time           bigint,
 
 CONSTRAINT pk_eg_pms_project PRIMARY KEY (id)
 );
 
 CREATE TABLE eg_pms_address(
 id                           character varying(64),
-tenantid                     character varying(64) NOT NULL,
+tenant_id                    character varying(64) NOT NULL,
 project_id                   character varying(64) NOT NULL,
-doorno                       character varying(64),
+door_no                      character varying(64),
 latitude                     bigint,
 longitude                    bigint,
-locationaccuracy             bigint,
+location_accuracy            bigint,
 type                         character varying(64),
-addressline1                 character varying(256),
-addressline2                 character varying(256),
+address_line1                character varying(256),
+address_line2                character varying(256),
 landmark                     character varying(256),
 city                         character varying(256),
-pincode                      character varying(64),
-buildingname                 character varying(256),
+pin_code                     character varying(64),
+building_name                character varying(256),
 street                       character varying(256),
-createdby                    character varying(64)  NOT NULL,
-lastmodifiedby               character varying(64),
-createdtime                  bigint,
-lastmodifiedtime             bigint,
+locality                     character varying(128),
+created_by                   character varying(64)  NOT NULL,
+last_modified_by             character varying(64),
+created_time                 bigint,
+last_modified_time           bigint,
 
 CONSTRAINT pk_eg_pms_address PRIMARY KEY (id),
 CONSTRAINT fk_eg_pms_address FOREIGN KEY (project_id) REFERENCES eg_pms_project (id)
@@ -49,14 +52,15 @@ CONSTRAINT fk_eg_pms_address FOREIGN KEY (project_id) REFERENCES eg_pms_project 
 CREATE TABLE eg_pms_target(
 id                           character varying(64),
 project_id                   character varying(64) NOT NULL,
-beneficiarytype              character varying(64),
-totalno                      numeric,
-targetno                     numeric,
-isdeleted                    boolean,
-createdby                    character varying(64)  NOT NULL,
-lastmodifiedby               character varying(64),
-createdtime                  bigint,
-lastmodifiedtime             bigint,
+beneficiary_type             character varying(64),
+total_no                     numeric,
+target_no                    numeric,
+is_deleted                   boolean,
+created_by                   character varying(64)  NOT NULL,
+last_modified_by             character varying(64),
+created_time                 bigint,
+last_modified_time           bigint,
+
 CONSTRAINT pk_eg_pms_target PRIMARY KEY (id),
 CONSTRAINT fk_eg_pms_target FOREIGN KEY (project_id) REFERENCES eg_pms_project (id)
 );
@@ -64,49 +68,36 @@ CONSTRAINT fk_eg_pms_target FOREIGN KEY (project_id) REFERENCES eg_pms_project (
 CREATE TABLE eg_pms_document(
 id                           character varying(64),
 project_id                   character varying(64) NOT NULL,
-documenttype                 character varying(256),
+document_type                character varying(256),
 filestore_id                 character varying(256) NOT NULL,
-documentuid                  character varying(64),
-additionaldetails            JSONB,
+document_uid                 character varying(64),
+additional_details           JSONB,
 status                       character varying(64),
-createdby                    character varying(64)  NOT NULL,
-lastmodifiedby               character varying(64),
-createdtime                  bigint,
-lastmodifiedtime             bigint,
+created_by                   character varying(64)  NOT NULL,
+last_modified_by             character varying(64),
+created_time                 bigint,
+last_modified_time           bigint,
+
 CONSTRAINT pk_eg_pms_document PRIMARY KEY (id),
 CONSTRAINT fk_eg_pms_document FOREIGN KEY (project_id) REFERENCES eg_pms_project (id)
 );
 
-CREATE TABLE eg_pms_locality(
-id                           character varying(64),
-code                         character varying(64),
-address_id                   character varying(64),
-parent_id                    character varying(64),
-name                         character varying(64),
-latitude                     character varying(64),
-longitude                    character varying(64),
-materializedPath             character varying(256),
-
-CONSTRAINT pk_eg_pms_locality PRIMARY KEY (id),
-CONSTRAINT fk_eg_pms_locality FOREIGN KEY (address_id) REFERENCES eg_pms_address (id)
-);
 
 
-
-CREATE INDEX IF NOT EXISTS index_eg_pms_project_tenantId ON eg_pms_project (tenantId);
-CREATE INDEX IF NOT EXISTS index_eg_pms_project_projecttype ON eg_pms_project (projecttype);
-CREATE INDEX IF NOT EXISTS index_eg_pms_project_projectsubtype ON eg_pms_project (projectsubtype);
+CREATE INDEX IF NOT EXISTS index_eg_pms_project_tenant_id ON eg_pms_project (tenant_id);
+CREATE INDEX IF NOT EXISTS index_eg_pms_project_project_type ON eg_pms_project (project_type);
+CREATE INDEX IF NOT EXISTS index_eg_pms_project_project_subtype ON eg_pms_project (project_subtype);
 CREATE INDEX IF NOT EXISTS index_eg_pms_project_department ON eg_pms_project (department);
 CREATE INDEX IF NOT EXISTS index_eg_pms_project_reference_id ON eg_pms_project (reference_id);
 CREATE INDEX IF NOT EXISTS index_eg_pms_project_parent ON eg_pms_project (parent);
-CREATE INDEX IF NOT EXISTS index_eg_pms_project_isdeleted ON eg_pms_project (isdeleted);
-CREATE INDEX IF NOT EXISTS index_eg_pms_project_istaskenabled ON eg_pms_project (istaskenabled);
-CREATE INDEX IF NOT EXISTS index_eg_pms_project_startdate ON eg_pms_project (startdate);
-CREATE INDEX IF NOT EXISTS index_eg_pms_project_enddate ON eg_pms_project (enddate);
-CREATE INDEX IF NOT EXISTS index_eg_pms_project_createdtime ON eg_pms_project (createdtime);
+CREATE INDEX IF NOT EXISTS index_eg_pms_project_is_deleted ON eg_pms_project (is_deleted);
+CREATE INDEX IF NOT EXISTS index_eg_pms_project_is_task_enabled ON eg_pms_project (is_task_enabled);
+CREATE INDEX IF NOT EXISTS index_eg_pms_project_start_date ON eg_pms_project (start_date);
+CREATE INDEX IF NOT EXISTS index_eg_pms_project_end_date ON eg_pms_project (end_date);
+CREATE INDEX IF NOT EXISTS index_eg_pms_project_created_time ON eg_pms_project (created_time);
 
-CREATE INDEX IF NOT EXISTS index_eg_pms_target_beneficiarytype ON eg_pms_target (beneficiarytype);
-CREATE INDEX IF NOT EXISTS index_eg_pms_target_totalno ON eg_pms_target (totalno);
-CREATE INDEX IF NOT EXISTS index_eg_pms_target_targetno ON eg_pms_target (targetno);
-CREATE INDEX IF NOT EXISTS index_eg_pms_target_isdeleted ON eg_pms_target (isdeleted);
-CREATE INDEX IF NOT EXISTS index_eg_pms_target_createdtime ON eg_pms_target (createdtime);
+CREATE INDEX IF NOT EXISTS index_eg_pms_target_beneficiary_type ON eg_pms_target (beneficiary_type);
+CREATE INDEX IF NOT EXISTS index_eg_pms_target_total_no ON eg_pms_target (total_no);
+CREATE INDEX IF NOT EXISTS index_eg_pms_target_target_no ON eg_pms_target (target_no);
+CREATE INDEX IF NOT EXISTS index_eg_pms_target_is_deleted ON eg_pms_target (is_deleted);
+CREATE INDEX IF NOT EXISTS index_eg_pms_target_created_time ON eg_pms_target (created_time);
