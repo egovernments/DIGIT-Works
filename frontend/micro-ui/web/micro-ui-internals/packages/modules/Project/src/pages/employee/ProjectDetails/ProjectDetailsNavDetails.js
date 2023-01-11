ccd4c309f1f18cff40d7d5f331b54155e9510831
Hariom Sinha <hariom.sinha@egovernments.org>
2023-01-11 13:31:40 +0530

    removed unused code and added useeffect for config updates

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/Project/src/pages/employee/ProjectDetails/ProjectDetailsNavDetails.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/Project/src/pages/employee/ProjectDetails/ProjectDetailsNavDetails.js
@@ -2,10 +2,6 @@ import React, { Fragment } from "react";
 import ApplicationDetails from "../../../../../templates/ApplicationDetails";
 
 const ProjectDetailsNavDetails = ({activeLink}) => {
-
-    // integrate once api is ready
-    const { applicationDetails_ProjectDetails } = Digit.Hooks.project.useViewProjectDetails({}); 
-    const { applicationDetails_FinancialDetails } = Digit.Hooks.project.useViewFinancialDetails({}); 
     const ViewProjectComponent = Digit?.ComponentRegistryService?.getComponent("ViewProject"); 
     const ViewFinancialDetailsComponent = Digit?.ComponentRegistryService?.getComponent("ViewFinancialDetails"); 
 
