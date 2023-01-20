6937982b4f0dc30070c08b9d40f4030849f57960
Hariom Sinha <hariom.sinha@egovernments.org>
2023-01-20 21:15:41 +0530

    added sample payload generator

---
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/Project/src/utils/createProjectUtils.js
@@ -0,0 +1,68 @@
+function createProjectList(data, parentProjectID) {
+    console.log(data, parentProjectID);
+    let project_payload = [];
+    if(!parentProjectID) {
+        //Project that has no sub-projects
+        let parent_project_with_no_subprojects =   {
+            "tenantId": "pb.amritsar",
+            "name": "project101",
+            "projectType": "MP-CWS",
+            "projectSubType": "",
+            "department": "DEPT_11",
+            "description": "DEPT_11 description",
+            "referenceID": "d9d8ffee-68a2-485d-bc50-14160477ea4c",
+            "documents": [
+                {
+                  "id": "",
+                  "documentType": "Document1",
+                  "fileStore": "99dcde84-aae2-4836-9ff9-f711fb82bb8b",
+                  "documentUid": "QWERTY123"
+                }
+            ],
+            "address": {
+              "tenantId": "pb.amritsar",
+              "doorNo": "1",
+              "latitude": 90,
+              "longitude": 180,
+              "locationAccuracy": 10000,
+              "type": "Home",
+              "addressLine1": "Address Line 1",
+              "addressLine2": "Address Line 2",
+              "landmark": "Area1",
+              "city": "City1",
+              "pincode": "999999",
+              "buildingName": "Test_Building",
+              "street": "Test_Street",
+              "locality": "SUN04"
+            },
+            "startDate": 0,
+            "endDate": 0, 
+            "isTaskEnabled": false,
+            "parent": "",
+            "targets": [
+              {
+                "beneficiaryType": "Slum",
+                "totalNo": 0,
+                "targetNo": 0
+              }
+            ],
+            "additionalDetails": {},
+            "rowVersion": 0
+        }
+        project_payload.push(parent_project_with_no_subprojects);
+    }
+    return project_payload;
+}
+
+const CreateProjectUtils = {
+    payload : {
+        create : (data, parentProjectID) => {
+            return {
+                Projects : createProjectList(data, parentProjectID), //if there is a Parent Project, then create list of sub-projects array, or only create one object for Parent Project.
+                apiOperation : "CREATE"
+            }
+        }
+    }
+}
+
+export default CreateProjectUtils;
