99a9255626c8e646416b6039d5855cab31defb15
Nipun Arora <aroranipun1@gmail.com>
2023-02-06 16:05:17 +0530

    Search Estimate Static Screen and API integration (#320)

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/Estimate/src/pages/employee/EstimateSearch.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/Estimate/src/pages/employee/EstimateSearch.js
@@ -1,16 +1,30 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
-import { Header, InboxSearchComposer } from "@egovernments/digit-ui-react-components";
+import { Header, InboxSearchComposer,Loader } from "@egovernments/digit-ui-react-components";
 import searchConfig from "../../configs/searchConfig";
 
 const EstimateSearch = () => {
     const { t } = useTranslation();
 
-    const configs = searchConfig();
+    // const configs = searchConfig();
 
+    const tenant = Digit.ULBService.getStateId();
+    const { isLoading, data } = Digit.Hooks.useCustomMDMS(
+        tenant,
+        "commonUiConfig",
+        [
+            {
+                "name": "SearchEstimateConfig"
+            }
+        ]
+    );
+
+    const configs = data?.commonUiConfig?.SearchEstimateConfig?.[0]
+
+    if (isLoading) return <Loader />
     return (
         <React.Fragment>
-            <Header styles={{ fontSize: "32px" }}>{configs?.label}</Header>
+            <Header styles={{ fontSize: "32px" }}>{t(configs?.label)}</Header>
             <div className="inbox-search-wrapper">
                 <InboxSearchComposer configs={configs}></InboxSearchComposer>
             </div>
