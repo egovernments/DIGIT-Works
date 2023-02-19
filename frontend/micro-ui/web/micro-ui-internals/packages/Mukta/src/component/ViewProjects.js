c16a25d10aa60f06bfc1d37f850485a0388a4550
Hariom Sinha <hariom.sinha@egovernments.org>
2023-02-20 02:25:38 +0530

    integrated hook response to view screens

--- frontend/micro-ui/web/micro-ui-internals/packages/Mukta/src/component/ViewProjects.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/Mukta/src/component/ViewProjects.js
@@ -17,7 +17,7 @@ const ProjectDetails = () => {
         Projects : [
             {
                 tenantId : queryStrings?.tenantId || tenantId,
-                projectNumber : queryStrings?.projectNumber || "PR/2022-23/02/000985" //will change this after integration with Search
+                projectNumber : queryStrings?.projectNumber || "PR/2022-23/02/000988" //will change this after integration with Search
             }
         ]
     } 
@@ -33,17 +33,48 @@ const ProjectDetails = () => {
 
     const preProcessApplicationData = (project) => {
         const basicDetails = {
-            title: "",
+            title: " ",
             asSectionHeader: true,
             values: [
                 { title: "WORKS_PROJECT_ID", value: project?.basicDetails?.projectID || "NA"},
+                { title: "WORKS_PROJECT_DATE_OF_PROPOSAL", value: project?.basicDetails?.projectProposalDate || "NA"},
+                { title: "WORKS_PROJECT_PROJECT_NAME", value: project?.basicDetails?.projectName || "NA"},
+                { title: "WORKS_PROJECT_PROJECT_DESC", value: project?.basicDetails?.projectDesc || "NA"},
             ]
         };
 
-        const projectDetails = {applicationDetails : [basicDetails]}
+        const projectDetails = {
+            title: "WORKS_PROJECT_DETAILS",
+            asSectionHeader: true,
+            values: [
+                { title: "WORKS_PROJECT_LOR", value: project?.details?.projectDetails?.applicationDetails?.[0]?.values?.[2]?.value || "NA"},
+                { title: "WORKS_PROJECT_TYPE", value: project?.details?.projectDetails?.applicationDetails?.[1]?.values?.[0]?.value || "NA"},
+                { title: "WORKS_PROJECT_TARGET_DEMOGRAPHY", value: project?.details?.projectDetails?.applicationDetails?.[0]?.values?.[1]?.value || "NA"},
+                { title: "WORKS_PROJECT_ESTIMATED_COST", value: project?.details?.projectDetails?.applicationDetails?.[0]?.values?.[3]?.value || "NA"},
+            ]
+        };
+
+        const locationDetails = {
+            title: "WORKS_LOCATION_DETAILS",
+            asSectionHeader: true,
+            values: [
+                { title: "WORKS_PROJECT_GEOLOCATION", value: project?.details?.projectDetails?.applicationDetails?.[2]?.values?.[3]?.value || "NA"},
+                { title: "WORKS_PROJECT_CITY", value: project?.details?.projectDetails?.applicationDetails?.[2]?.values?.[2]?.value || "NA"},
+                { title: "WORKS_PROJECT_WARD", value: project?.details?.projectDetails?.applicationDetails?.[2]?.values?.[1]?.value || "NA"},
+                { title: "WORKS_PROJECT_LOCALITY", value: project?.details?.projectDetails?.applicationDetails?.[2]?.values?.[0]?.value || "NA"},
+            ]
+        };
+
+        const documentDetails = {
+            title: "",
+            asSectionHeader: true,
+            additionalDetails: project?.details?.projectDetails?.applicationDetails?.[3]?.additionalDetails
+        }
+
+        const details = {applicationDetails : [basicDetails, projectDetails, locationDetails, documentDetails]}
 
         return {
-            projectDetails : projectDetails,
+            projectDetails : details,
             processInstancesDetails: [],
             applicationData: {},
             workflowDetails: [],
@@ -52,7 +83,7 @@ const ProjectDetails = () => {
     }
 
     const { data, isLoading } = Digit.Hooks.project.useViewProjectDetailsInEstimate(t, tenantId, searchParams, filters, headerLocale);
-
+    
     useEffect(()=>{
         setProjectData(preProcessApplicationData(data?.projectDetails?.searchedProject));
     },[data]);
