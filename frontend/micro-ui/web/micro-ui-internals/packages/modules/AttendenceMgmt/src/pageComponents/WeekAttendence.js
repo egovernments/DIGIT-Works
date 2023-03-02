6c4df4c70e60e34fc2e63d1ac52dd7e586aad9c2
Prachi Rane <prachirane@192.168.1.5>
2023-03-02 15:30:19 +0530

    [PFM-2492] | [Prachi] | Added code to update UI for attendance table half, full radio button

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/AttendenceMgmt/src/pageComponents/WeekAttendence.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/AttendenceMgmt/src/pageComponents/WeekAttendence.js
@@ -37,20 +37,18 @@ const WeekAttendence = ({ state, dispatch, modify, setSaveAttendanceState, weekD
     const classSelector = (state) => {
       switch (state) {
         case "half":
-          return ["radio-outer-circle selected", "radio-half-inner-circle"];
+          return ["radio-half-circle"];
         case "full":
-          return ["radio-outer-circle selected", "radio-full-inner-circle"];
+          return ["radio-full-circle"];
 
         default:
-          return ["radio-outer-circle unselected", ""];
+          return ["radio-no-circle"];
       }
     };
 
     return (
       <div className="modern-radio-container week-table">
-        <div className={`${classSelector(state)?.[0]}`}>
-          <div className={`${classSelector(state)?.[1]}`}></div>
-        </div>
+        <div className={`${classSelector(state)?.[0]}`}></div>
       </div>
     );
   };
