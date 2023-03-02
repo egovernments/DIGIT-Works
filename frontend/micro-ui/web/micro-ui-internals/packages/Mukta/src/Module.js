752eb0715678318c77f57844f1c013c2c3fa0320
Jagankumar <53823168+jagankumar-egov@users.noreply.github.com>
2023-03-02 14:27:03 +0530

    PFM-2498 :: UI Search wage seeker added the static changes

--- frontend/micro-ui/web/micro-ui-internals/packages/Mukta/src/Module.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/Mukta/src/Module.js
@@ -3,6 +3,7 @@ import React from "react";
 import { useRouteMatch } from "react-router-dom";
 import EmployeeApp from "./pages/employee";
 import { CustomisedHooks } from "./hooks";
+import WorksCard from "./components/WorksCard";
 import { UICustomizations } from "./configs/UICustomizations";
 
 const MuktaModule = ({ stateCode, userType, tenants }) => {
@@ -24,6 +25,7 @@ const MuktaModule = ({ stateCode, userType, tenants }) => {
 
 const componentsToRegister = {
   MuktaModule,
+  WorksCard
 };
 
 const overrideHooks = () => {
