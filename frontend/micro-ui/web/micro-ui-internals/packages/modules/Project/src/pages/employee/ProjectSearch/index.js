6584518babcfac72fb15fe754b1be164f42b07d7
Jagankumar <53823168+jagankumar-egov@users.noreply.github.com>
2023-03-03 13:33:11 +0530

    PFM-2393 :: Projects search screen added a link to add project

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/Project/src/pages/employee/ProjectSearch/index.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/Project/src/pages/employee/ProjectSearch/index.js
@@ -1,34 +1,46 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
-import { Header, InboxSearchComposer, Loader } from "@egovernments/digit-ui-react-components";
+import { Header, InboxSearchComposer, Loader, Button, AddFilled } from "@egovernments/digit-ui-react-components";
+import { useHistory } from "react-router-dom";
+
 import searchConfig from "../../../configs/searchConfig";
 
 const ProjectSearch = () => {
-    const { t } = useTranslation();
+  const { t } = useTranslation();
+  const history = useHistory();
 
-    // const configs = searchConfig();
-    const tenant = Digit.ULBService.getStateId();
-    const { isLoading, data } = Digit.Hooks.useCustomMDMS(
-        tenant,
-        "commonUiConfig",
-        [
-            {
-                "name": "SearchProjectConfig"
-            }
-        ]
-    );
-   
-    const configs = data?.commonUiConfig?.SearchProjectConfig?.[0]
-    
-    if(isLoading) return <Loader />
-    return (
-        <React.Fragment>
-            <Header styles={{ fontSize: "32px" }}>{t(configs?.label)}</Header>
-            <div className="inbox-search-wrapper">
-                <InboxSearchComposer configs={configs}></InboxSearchComposer>
-            </div>
-        </React.Fragment>
-    )
-}
+  // const configs = searchConfig();
+  const tenant = Digit.ULBService.getStateId();
+  const { isLoading, data } = Digit.Hooks.useCustomMDMS(tenant, "commonUiConfig", [
+    {
+      name: "SearchProjectConfig",
+    },
+  ]);
+
+  const configs = data?.commonUiConfig?.SearchProjectConfig?.[0];
+
+  if (isLoading) return <Loader />;
+  return (
+    <React.Fragment>
+      <div className="jk-header-btn-wrapper">
+        <Header styles={{ fontSize: "32px" }}>{t(configs?.label)}</Header>
+        {Digit.Utils.didEmployeeHasRole(configs?.actionRole) && (
+          <Button
+            label={t(configs?.actionLabel)}
+            variation="secondary"
+            icon={<AddFilled />}
+            onButtonClick={() => {
+              history.push(`/${window?.contextPath}/employee/${configs?.actionLink}`);
+            }}
+            type="button"
+          />
+        )}
+      </div>
+      <div className="inbox-search-wrapper">
+        <InboxSearchComposer configs={configs}></InboxSearchComposer>
+      </div>
+    </React.Fragment>
+  );
+};
 
 export default ProjectSearch;
