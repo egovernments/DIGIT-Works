fae839376acefb373a95c169e0bdce7836c0a596
pintu-eGov <pintu.kumar@egovernments.org>
2023-03-14 12:09:28 +0530

    alter the tenant id in address

--- backend/estimate-service/src/main/resources/db/migration/main/V20230301124030__alter_table.sql
+++ backend/estimate-service/src/main/resources/db/migration/main/V20230301124030__alter_table.sql
@@ -11,7 +11,7 @@ ALTER TABLE eg_wms_estimate_address ALTER COLUMN address_number DROP NOT NULL;
 ALTER TABLE eg_wms_estimate_address ALTER COLUMN address_line_1 DROP NOT NULL;
 ALTER TABLE eg_wms_estimate_address ALTER COLUMN city DROP NOT NULL;
 ALTER TABLE eg_wms_estimate_address ALTER COLUMN pin_code DROP NOT NULL;
-ALTER TABLE eg_wms_estimate_address ALTER COLUMN tenant_id DROP NOT NULL;
+
 
 
 ALTER TABLE eg_wms_estimate_amount_detail ALTER COLUMN type DROP NOT NULL;
---
+++ backend/estimate-service/src/main/resources/db/migration/main/V20230304134030__alter_table.sql
@@ -0,0 +1,1 @@
+ALTER TABLE eg_wms_estimate_address ALTER COLUMN tenant_id DROP NOT NULL;
