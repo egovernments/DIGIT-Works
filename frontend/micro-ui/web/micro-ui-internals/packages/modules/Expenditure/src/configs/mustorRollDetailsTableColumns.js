7be1e8af8afd9a2a0a21df1cb7fee38d7c2c5edb
prachirane-eGov <117716375+prachirane-eGov@users.noreply.github.com>
2023-03-16 11:10:33 +0530

    PFM-2579 :: Bill inbox, Search, View - (#467)
    * [Prachi] | Added base code for bill inbox screen
    
    * [Prachi] | Added local config for Bill Search screen
    
    * [Prachi] | Sync Bill inbox changes
    
    * [Prachi] | Bill Search changes
    
    * [Prachi] | Added base code for View bill
    
    * [Prachi] | Added base code for View bill
    
    * [Prachi] | Updated Bill Inbox config to accept muster inbox API
    
    * [Prachi] | Updated Bill Search config to accept Muster Search API for search
    
    * [Prachi] | Added code for Bill View page
    
    * [Prachi] | Feedback changes for Bill screens
    
    * [Prachi] | Feedback changes for Bill screens
    
    * [Prachi] | updated inbox bill path
    
    * Updated the Bill package version
    
    * Incremented to 10
    
    ---------
    
    Co-authored-by: Prachi Rane <prachirane@Prachis-MacBook-Pro.local>
    Co-authored-by: Prachi Rane <prachirane@192.168.1.4>
    Co-authored-by: Jagankumar <53823168+jagankumar-egov@users.noreply.github.com>

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/Expenditure/src/configs/mustorRollDetailsTableColumns.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/Expenditure/src/configs/mustorRollDetailsTableColumns.js
@@ -3,28 +3,118 @@ import React from "react";
 const GetCell = (value) => <span className="cell-text">{value}</span>;
 
 const renderTotalLabel = (value) => {
-    return (
-      <div className="total-attendence-label">
-        <span>{value}</span>
-      </div>
-    );
+  return (
+    <div className="total-attendence-label">
+      <span>{value}</span>
+    </div>
+  );
 };
 
 const renderBankAccountDetails = (value) => {
     return (
       <div className="column-bank-details">
-        <p className="detail">{value.accountNo}</p>
-        <p className="detail">{value.ifsc}</p>
+        <p className="detail">{value?.accountNo}</p>
+        <p className="detail">{value?.ifsc}</p>
       </div>
     );
 };
 
 export const mustorRollDetailsTableColumns = (t) => [
+  {
+    Header: () => <p>{t("WORKS_SNO")}</p>,
+    accessor: "sno",
+    Cell: ({ value, column, row }) => {
+      if (row.original.type === "total") {
+        return renderTotalLabel(t(row.original.sno));
+      }
+      return String(t(value));
+    },
+  },
+  {
+    Header: () => <p>{t("EXP_REG_ID")}</p>,
+    accessor: "registerId",
+    Cell: ({ value, column, row }) => {
+      return String(t(value));
+    },
+  },
+  {
+    Header: () => <p>{t("EXP_NAME_OF_THE_INDIVIDUAL")}</p>,
+    accessor: "nameOfIndividual",
+    Cell: ({ value, column, row }) => {
+      return String(t(value));
+    },
+  },
+  {
+    Header: () => <p>{t("EXP_FATHER_GUARDIAN_NAME")}</p>,
+    accessor: "guardianName",
+    Cell: ({ value, column, row }) => {
+      return String(t(value));
+    },
+  },
+  {
+    Header: () => <p>{t("ATM_SKILL")}</p>,
+    accessor: "skill",
+    Cell: ({ value, column, row }) => {
+      return String(t(value));
+    }
+  },
+  {
+    Header: () => <p>{"Days worked"}</p>,
+    accessor: "actualWorkingDays",
+    Cell: ({ value, column, row }) => {
+      return String(t(value));
+    }
+  },
+  {
+    Header: () => <p>{"Days measured"}</p>,
+    accessor: "modifiedWorkingDays",
+    Cell: ({ value, column, row }) => {
+      return String(t(value));
+    }
+  },
+  {
+    Header: () => <p>{"Per day wage (₹)"}</p>,
+    accessor: "amount",
+    Cell: ({ value, column, row }) => {
+      return Digit.Utils.dss.formatterWithoutRound(value, 'number');
+    }
+  },
+  {
+    Header: () => <p>{"Total Wage (₹)"}</p>,
+    accessor: "modifiedAmount",
+    Cell: ({ value, column, row }) => {
+      return Digit.Utils.dss.formatterWithoutRound(value, 'number');
+    }
+  },
+  {
+    Header: () => <p>{t("EXP_BANK_ACCOUNT_DETAILS")}</p>,
+    accessor: "bankAccountDetails",
+    Cell: ({ value, column, row }) => {
+      return renderBankAccountDetails(value);
+    }
+  },
+  {
+    Header: () => <p>{"Payment Status"}</p>,
+    accessor: "paymentStatus",
+    Cell: ({ value, column, row }) => {
+      return String(t(value));
+    }
+  },
+  // {
+  //     Header: t("EXP_AADHAR_NO"),
+  //     accessor: "aadharNumber",
+  //     Cell: ({ value, column, row }) => {
+  //       return String(t(value));
+  //     }
+  // }    
+]
+
+/*
+export const mustorRollDetailsTableColumns = (t) => [
     {
         Header: t("EXP_SNO"),
         accessor: "sno",
         Cell: ({value, row}) => {
-            
             if (row.original.type === "total") {
               return renderTotalLabel(t(value))
             }
@@ -59,3 +149,4 @@ export const mustorRollDetailsTableColumns = (t) => [
         accessor: (row) => (GetCell(row?.aadhar)),
     }    
 ]
+*/
