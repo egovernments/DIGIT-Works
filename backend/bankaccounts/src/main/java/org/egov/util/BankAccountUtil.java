87415787fa2bef583d0d1ff4ac91264dfa6a5446
pintu-eGov <87848739+pintu-eGov@users.noreply.github.com>
2023-03-17 17:04:23 +0530

    Create bank account (#488)
    * Create bank account
    
    * commented out the request info validation
    
    * added the default(hard coded) value for created by since request info is null
    
    * added service common jar
    
    * added the postman collection
    
    ---------
    
    Co-authored-by: pintu-eGov <pintu.kumar@egovernments.org>

---
+++ backend/bankaccounts/src/main/java/org/egov/util/BankAccountUtil.java
@@ -0,0 +1,57 @@
+package org.egov.util;
+
+
+import digit.models.coremodels.AuditDetails;
+import lombok.extern.slf4j.Slf4j;
+import org.egov.web.models.BankAccount;
+import org.egov.web.models.BankAccountDetails;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@Slf4j
+public class BankAccountUtil {
+
+    /**
+     * Method to set auditDetails for create/update flow of Bank Account
+     *
+     * @param by
+     * @param isCreate
+     * @return
+     */
+    public void setAuditDetailsForBankAccount(String by, List<BankAccount> bankAccountList, Boolean isCreate) {
+        log.info("BankAccountUtil::setAuditDetailsForBankAccount");
+        Long time = System.currentTimeMillis();
+        for (BankAccount bankAccount : bankAccountList) {
+            if (isCreate) {
+                AuditDetails auditDetailsForCreate = AuditDetails.builder().createdBy(by).lastModifiedBy(by).createdTime(time).lastModifiedTime(time).build();
+                bankAccount.setAuditDetails(auditDetailsForCreate);
+            } else {
+                AuditDetails auditDetailsForUpdate = AuditDetails.builder().lastModifiedBy(by).lastModifiedTime(time).build();
+                bankAccount.setAuditDetails(auditDetailsForUpdate);
+            }
+        }
+    }
+
+    /**
+     * Method to set auditDetails for create/update flow of bank account details
+     *
+     * @param by
+     * @param isCreate
+     * @return
+     */
+    public void setAuditDetailsForBankAccountDetail(String by, List<BankAccountDetails> bankAccountDetailsList, Boolean isCreate) {
+        log.info("BankAccountUtil::setAuditDetailsForBankAccountDetail");
+        Long time = System.currentTimeMillis();
+        for (BankAccountDetails bankAccountDetails : bankAccountDetailsList) {
+            if (isCreate) {
+                AuditDetails auditDetailsForCreate = AuditDetails.builder().createdBy(by).lastModifiedBy(by).createdTime(time).lastModifiedTime(time).build();
+                bankAccountDetails.setAuditDetails(auditDetailsForCreate);
+            } else {
+                AuditDetails auditDetailsForUpdate = AuditDetails.builder().lastModifiedBy(by).lastModifiedTime(time).build();
+                bankAccountDetails.setAuditDetails(auditDetailsForUpdate);
+            }
+        }
+    }
+}
