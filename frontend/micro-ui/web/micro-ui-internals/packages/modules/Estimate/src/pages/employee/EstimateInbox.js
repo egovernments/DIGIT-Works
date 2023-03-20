6a76d2c4e9564eb1a8eb9ab5d0b828d956b67229
Nipun Arora <aroranipun1@gmail.com>
2023-03-20 12:35:53 +0530

    Inbox Cache issue
    -> Sending cloned config everytime we change location and come back to inbox
    -> Mustor and Bill inbox already working fiine

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/Estimate/src/pages/employee/EstimateInbox.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/Estimate/src/pages/employee/EstimateInbox.js
@@ -1,13 +1,16 @@
-import React, {useMemo} from "react";
+import React, {useMemo,useState,useEffect} from "react";
 import { useTranslation } from "react-i18next";
 import { Header, InboxSearchComposer,Loader } from "@egovernments/digit-ui-react-components";
 import inboxConfig from "../../configs/inboxConfig";
 import inboxConfigMukta from "../../configs/inboxConfigMukta";
+import { useLocation } from "react-router-dom";
+
 const EstimateInbox = () => {
     const { t } = useTranslation();
-
+    const location = useLocation()
     //fetch this config from mdms and pass it to the preProcess fn
     // let configs = inboxConfigMukta(t);
+    const [pageConfig,setPageConfig] = useState(null)
     const moduleName = Digit.Utils.getConfigModuleName()
     const tenant = Digit.ULBService.getStateId();
     const { isLoading, data } = Digit.Hooks.useCustomMDMS(tenant,
@@ -18,18 +21,26 @@ const EstimateInbox = () => {
             },
         ]);
         
-    const configs = data?.[moduleName]?.EstimateInboxConfig?.[0]
+    // let configs = data?.[moduleName]?.EstimateInboxConfig?.[0]
+ 
 
     const updatedConfig = useMemo(
-        () => Digit.Utils.preProcessMDMSConfigInboxSearch(t, configs,"sections.search.uiConfig.fields",{}),
-        [data]);
+        () => Digit.Utils.preProcessMDMSConfigInboxSearch(t, pageConfig,"sections.search.uiConfig.fields",{}),
+        [data,pageConfig]);
+
+    
+    useEffect(() => {
+        setPageConfig(_.cloneDeep(data?.[moduleName]?.EstimateInboxConfig?.[0]))
+        
+    }, [data,location])
     
     
-    if(isLoading) return <Loader />
+    if(isLoading || !pageConfig) return <Loader />
     return (
         <React.Fragment>
             <Header styles={{ fontSize: "32px" }}>{t(updatedConfig?.label)}</Header>
             <div className="inbox-search-wrapper">
+                {/* <InboxSearchComposer configs={updatedConfig}></InboxSearchComposer> */}
                 <InboxSearchComposer configs={updatedConfig}></InboxSearchComposer>
             </div>
         </React.Fragment>
