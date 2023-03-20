ba04470d0fdd5ae501ecf833a0fc3d68fe225a95
Nipun Arora <aroranipun1@gmail.com>
2023-03-20 17:51:38 +0530

    PFM-2579 :: Added updated Modal for mustor approval and supporting changes in formmcomposer (#513)
    * Added updated Modal for mustor approval and supporting changes in formcomposer
    
    * Requested changes
    
    Flag -> We'll have to revert later
    
    * Update Reason.js
    
    ---------
    
    Co-authored-by: Jagankumar <53823168+jagankumar-egov@users.noreply.github.com>

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/templates/ApplicationDetails/config/configAttendanceApproveModal.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/templates/ApplicationDetails/config/configAttendanceApproveModal.js
@@ -3,13 +3,26 @@ const configAttendanceApproveModal = ({ t, action }) => {
   if (action?.applicationStatus === "APPROVED") {
     return {
       label: {
-        heading: t("ATM_PROCESSINGMODAL_HEADER"),
-        submit: t("ATM_FORWARD_FOR_APPROVAL"),
+        heading: t("APPROVE_MUSTOR_ROLL"),
+        submit: t("ATM_APPROVE"),
         cancel: t("ES_COMMON_CANCEL"),
       },
       form: [
         {
           body: [
+            {
+              label: " ",
+              type: "checkbox",
+              disable: false,
+              populators: {
+                name: "acceptTerms",
+                title: "MUSTOR_APPROVAL_CHECKBOX",
+                isMandatory: true,
+                // styles:{"marginLeft":"38px"},
+                labelStyles: {marginLeft:"40px"},
+                customLabelMarkup: true
+              }
+            },
             {
               label: t("WF_COMMON_COMMENTS"),
               type: "textarea",
