bf34c72e2b55cc7426897b0f50f24569772e89ce
Nipun Arora <aroranipun1@gmail.com>
2023-03-20 17:49:40 +0530

    Requested changes
    Flag -> We'll have to revert later

--- frontend/micro-ui/web/micro-ui-internals/packages/react-components/src/atoms/Reason.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/react-components/src/atoms/Reason.js
@@ -1,10 +1,28 @@
-import React from "react";
+import React,{Fragment} from "react";
 
-const Reason = ({ headComment, otherComment }) => (
-    <div className="checkpoint-comments-wrap">
-        <h4>{headComment}</h4>
-        <p>{otherComment}</p>
-    </div>
-);
+const Reason = ({ headComment, otherComment, additionalComment }) => {
+    
+    const getMarkup = () => {
+        if(additionalComment) {
+            return <div className="checkpoint-comments-wrap" style={{marginBottom:"1rem"}}>
+                <p>I certify that appropriate amount of work has been completed. Muster roll has been verified against Measurement Book.</p>
+                <br />
+                <p>
+                    <b> Note</b>: Once approved Payment Advice will be generated and send to JIT-FS for payment processing.
+                </p>
+                </div>
+            
+        }
+        else {
+            return <div className="checkpoint-comments-wrap">
+                <h4>{headComment}</h4>
+                <p>{otherComment}</p>
+            </div>
+        }
+    }
+    return (
+        getMarkup()
+    );
+}
 
 export default Reason;
