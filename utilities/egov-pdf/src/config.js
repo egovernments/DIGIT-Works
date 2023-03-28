b6238dcbb29c42eaf13d40c9582c15081dc7e90f
Jagankumar <53823168+jagankumar-egov@users.noreply.github.com>
2023-03-28 14:01:08 +0530

    Updated the Egov-pdf config and cleaned up

--- utilities/egov-pdf/src/config.js
+++ utilities/egov-pdf/src/config.js
@@ -21,34 +21,6 @@ module.exports = {
   DB_PORT: process.env.DB_PORT || 5432,
   pdf: {
     epass_pdf_template: process.env.EPASS_TEMPLATE || "tlcertificate",
-    tlcertificate_pdf_template: process.env.TL_CERTIFICATE || "tlcertificate",
-    tlrenewalcertificate_pdf_template:
-      process.env.TL_RENEWALCERTIFICATE || "tlrenewalcertificate",
-    tlreceipt_pdf_template: process.env.TL_RECEIPT || "tradelicense-receipt",
-    tlbill_pdf_template: process.env.TL_BILL || "tradelicense-bill",
-    ptreceipt_pdf_template: process.env.PT_RECEIPT || "property-receipt",
-    ptmutationcertificate_pdf_template:
-      process.env.PT_MUTATION_CERTIFICATE || "ptmutationcertificate",
-    ptbill_pdf_template: process.env.PT_BILL || "property-bill",
-    consolidated_receipt_template:
-      process.env.CONSOLIDATED_RECEIPT || "consolidatedreceipt",
-    consolidated_bill_template:
-      process.env.CONSOLIDATED_BILL || "consolidatedbill",
-    mcollect_challan_template:
-      process.env.MCOLLECT_CHALLAN || "mcollect-challan",
-    mcollect_bill_template:
-      process.env.MCOLLECT_BILL || "mcollect-bill",
-    bill_amendment_template:
-      process.env.BILL_AMENDMENT || "bill-amendment-credit-note",
-    wns_one_time_receipt:
-    process.env.WNS_ONE_TIME_RECEIPT || "ws-onetime-receipt",
-    wns_bill:
-    process.env.WNS_BILL || "ws-bill",
-    birth_certificate:
-            process.env.BIRTH_CERTIFICATE || "birth-certificate",
-    death_certificate:
-        process.env.DEATH_CERTIFICATE || "death-certificate"
-        
   },
   app: {
     port: parseInt(process.env.APP_PORT) || 8080,
@@ -58,38 +30,16 @@ module.exports = {
   host: {
     mdms: process.env.EGOV_MDMS_HOST || HOST,
     epass: process.env.EGOV_TLSERVICES_HOST || HOST,
-    tl: process.env.EGOV_TRADELICENSESERVICES_HOST || HOST,
-    pt: process.env.EGOV_PTSERVICES_HOST || HOST,
     pdf: process.env.EGOV_PDF_HOST || HOST,
     user: process.env.EGOV_USER_HOST || HOST,
-    payments: process.env.EGOV_PAYMENTS_HOST || HOST,
-    bill: process.env.EGOV_SEARCHER_HOST || HOST,
-    workflow: process.env.EGOV_WORKFLOW_HOST || HOST,
-    challan: process.env.EGOV_ECHALLAN_HOST || 'http://echallan-services.egov:8080/',
-    mcollectBilling: process.env.EGOV_BILLING_HOST || 'http://billing-service.egov:8080/',
-    wns: process.env.EGOV_WNS_HOST || HOST
+    workflow: process.env.EGOV_WORKFLOW_HOST || HOST
   },
   paths: {
     pdf_create: "/pdf-service/v1/_createnosave",
     epass_search: "/tl-services/v1/_search",
-    tl_search: "/tl-services/v1/_search",
-    pt_search: "/property-services/property/_search",
     user_search: "/user/_search",
     mdms_search: "/egov-mdms-service/v1/_search",
     download_url: "/download/epass",
-    payment_search: "/collection-services/payments/$module/_search",
-    bill_search: "/egov-searcher/bill-genie/billswithaddranduser/_get",
     workflow_search: "/egov-workflow-v2/egov-wf/process/_search",
-    mcollect_challan_search:"/echallan-services/eChallan/v1/_search",
-    mcollect_bill:"/billing-service/bill/v2/_search",
-    bill_genie_getBill:"/egov-searcher/bill-genie/mcollectbills/_get",
-    bill_ammendment_search: "/billing-service/amendment/_search",
-    water_search: "ws-services/wc/_search",
-    sewerage_search: "sw-services/swc/_search",
-    searcher_water_open_search:"/egov-searcher/water-services/wateropensearch/_get",
-    searcher_sewerage_open_search:"/egov-searcher/sewerage-services/sewerageopensearch/_get",
-    bill_genie_waterBills:"/egov-searcher/bill-genie/waterbills/_get",
-    bill_genie_sewerageBills:"/egov-searcher/bill-genie/seweragebills/_get",
-    fetch_bill: "/billing-service/bill/v2/_fetchbill"
   },
 };
