bdb62660c8e0f68c8cffc2acaeea23f6d98cf4de
Hariom Sinha <hariom.sinha@egovernments.org>
2023-03-29 18:30:42 +0530

    integrated assignee modal for submit WO

---
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/Contracts/src/configs/getWOModalConfig.js
@@ -0,0 +1,50 @@
+import { Dropdown, Loader } from '@egovernments/digit-ui-react-components';
+import React from 'react'
+
+const getWOModalConfig = ({
+    t,
+    approvers,
+    selectedApprover,
+    setSelectedApprover,
+    approverLoading = false,
+}) => {
+    let checkConditions = true
+    return {
+        label: {
+            heading: `COMMON_CHECK_FORWARD`,
+            submit: `WORKS_FORWARD`,
+            cancel: "WORKS_CANCEL",
+        },
+        form: [
+            {
+                body: [
+                    {
+                        label: !checkConditions ? null : t("WORKS_ASSIGNEE_NAME"),
+                        isMandatory: false,
+                        type: "goToDefaultCase",
+                        populators: !checkConditions ? null : (
+                            approverLoading ? <Loader /> : <Dropdown
+                                name={"approver"}
+                                option={approvers}
+                                autoComplete="off"
+                                optionKey="name_designation"
+                                id="fieldInspector"
+                                select={setSelectedApprover}
+                                selected={selectedApprover}
+                            />
+                        ),
+                    },
+                    {
+                        label: t("WF_COMMON_COMMENTS"),
+                        type: "textarea",
+                        populators: {
+                            name: "comments",
+                        },
+                    },
+                ]
+            }
+        ]
+    }
+}
+
+export default getWOModalConfig
