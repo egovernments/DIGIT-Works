361436d7546b4f1644c1cbf224df134bed90bff7
harshal-egov <117713108+harshal-egov@users.noreply.github.com>
2023-04-06 16:13:11 +0530

    sms-integration with contract-service (#572)
    * PFM-2123: Contract search API code setup
    
    * PFM-2123: Contract search API enrichment
    
    * PFM-2123: Contract search core logic implementation
    
    * PFM-1050 | Create contract initial draft
    
    * PFM-1050 | Changes after merge
    
    * PFM-1050 | Updated to correct search fields
    
    * Add: contract-approval workflow json
    
    * Add: contractType, orgId, fromDate, toDate search parameters to contract-search
    
    * PFM-1050 | Changes related to create and update API
    
    * PFM-1050 | Changes related to create and update API
    
    * Add: workflow APIs code setup
    
    * Refactor: Remove RequestInfo from searchContracts method parameters | Add filter logic to search contract.
    
    * Refactor: Add requestInfo in contractCriteria object
    
    * PFM-1050 | Updated contract service API spec | Added ER diagram
    
    * PFM-1050 | Updated contract service API spec
    
    * PFM-1050 | Updated contract service | Incorporated works specific changes
    
    * Add: contract-service workflow integration
    
    * PFM-1050 | Updated contract service | Incorporated works specific changes
    
    * Add: Code for enrich issue date on contract-approval and mark contract inactive on contract reject
    
    * Add: Code for enrich start date, end date on contract-acceptance | add REJECT action to CBOAdmin
    
    * Add: SMS-notification logic for contract-service for REJECT and APPROVE workflow actions.
    
    * Add: Integrate url-shortner service in sms-notification templates.
    
    * Refactor: tenantId and rootTenantId in NotificationService
    
    * Add: Organisation-service integration with contract-service
    
    * Refactor: Organisation-service integration with contract-service merge-conflicts
    
    * Refactor: sms-integration with contract-service
    
    * Refactor: sms-integration with contract-service
    
    * Refactor: make due date configurable
    
    ---------
    
    Co-authored-by: vishalgupta-eGov <vishal.gupta@egovernments.org>

--- backend/contract-service/src/main/java/org/egov/works/util/ContractServiceConstants.java
+++ backend/contract-service/src/main/java/org/egov/works/util/ContractServiceConstants.java
@@ -1,5 +1,9 @@
 package org.egov.works.util;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class ContractServiceConstants {
     public static final String MASTER_TENANTS = "tenants";
     public static final String MDMS_TENANT_MODULE_NAME = "tenant";
@@ -23,7 +27,7 @@ public class ContractServiceConstants {
     //public static final String JSON_PATH_FOR_DOCUMENT_TYPE_VERIFICATION = MDMS_RESP_CONSTANT + MDMS_WORKS_MODULE_NAME + "." + MASTER_DOCUMENT_TYPE + ".*";
     public static final String JSON_PATH_FOR_OIC_ROLES_VERIFICATION = MDMS_RESP_CONSTANT + MDMS_WORKS_MODULE_NAME + "." + MASTER_OIC_ROLES;
     public static final String JSON_PATH_FOR_OVER_HEADS_VERIFICATION = MDMS_RESP_CONSTANT + MDMS_WORKS_MODULE_NAME + "." + MASTER_OVER_HEADS+ ".*."+COMMON_CODE_FILTER;
-    public static final String HRMS_USER_ROLES_CODE = "$.Employees.*.user.roles.*code";
+
     public static final String REGISTER_ACTIVE_CODE_CONSTANT = "$.[?(@.active==true && @.code =='";
     public static final String CREATE_REGISTER_CONSTANT ="' && @.createRegister==true)]";
     public static final String PROJECT_NAME_CONSTANT = "projectName";
@@ -31,4 +35,51 @@ public class ContractServiceConstants {
     public static final String PROJECT_TYPE_CONSTANT = "projectType";
     public static final String PROJECT_ID_CONSTANT = "projectId";
     public static final String ORG_NAME_CONSTANT = "orgName";
+
+    //HRMS User Constants
+    public static final String HRMS_USER_USERNAME_CODE = "$.Employees.*.user.userName";
+
+    public static final String HRMS_USER_MOBILE_NO = "$.Employees.*.user.mobileNumber";
+
+    public static final String HRMS_USER_DESIGNATION = "$.Employees.*.assignments.*.designation";
+
+    public static final String HRMS_USER_ROLES_CODE = "$.Employees.*.user.roles.*.code";
+
+
+
+    //Localization CONSTANTS
+    public static final String CONTRACTS_NOTIFICATION_ENG_LOCALE_CODE = "en_IN";
+
+    public static final String CONTRACTS_MODULE_CODE = "rainmaker-contracts";
+
+    public static final String CONTRACTS_REJECT_LOCALIZATION_CODE = "CONTRACTS_NOTIFICATION_WORKFLOW_REJECT_ACTION";
+
+    public static final String CONTRACTS_APPROVE_CREATOR_LOCALIZATION_CODE = "CONTRACTS_NOTIFICATION_TO_CREATOR_WORKFLOW_APPROVE_ACTION";
+
+    public static final String CONTRACTS_APPROVE_CBO_LOCALIZATION_CODE = "CONTRACTS_NOTIFICATION_TO_CBOADMIN_WORKFLOW_APPROVE_ACTION";
+
+
+    public static final String CONTRACTS_LOCALIZATION_CODES_JSONPATH = "$.messages.*.code";
+    public static final String CONTRACTS_LOCALIZATION_MSGS_JSONPATH = "$.messages.*.message";
+
+    //Project service Constants
+    public static final String PROJECT_NAME_CODE ="$.Projects.*.name";
+
+    public static final String PROJECT_BOUNDARY_CODE ="$.Projects.*.address.boundary";
+
+    public static final String PROJECT_BOUNDARY_TYPE_CODE ="$.Projects.*.address.boundaryType";
+
+    //Location Service Constants
+    public static final String LOCATION_BOUNDARY_NAME_CODE ="$.*.*.boundary.*.name";
+
+    //Organisation Constants
+
+    public static final String ORGANISATION_NAME_CODE ="$.organisations.*.name";
+    public static final String ORGANISATION_PERSON_CODE ="$.organisations.*.contactDetails.*.contactName";
+    public static final String ORGANISATION_MOBILE_NUMBER_CODE ="$.organisations.*.contactDetails.*.contactMobileNumber";
+
+
+
+
+
 }
