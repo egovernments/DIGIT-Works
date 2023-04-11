123855bd86eeeef0a4d2d3c64a447dd2594bfef5
Hariom Sinha <hariom.sinha@egovernments.org>
2023-04-11 11:26:24 +0530

    updated locality for projects

--- frontend/micro-ui/web/micro-ui-internals/packages/Mukta/src/services/Search.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/Mukta/src/services/Search.js
@@ -38,7 +38,7 @@ const createProjectsArray = (t, project, searchParams, headerLocale) => {
                 { title: "WORKS_GEO_LOCATION",value: currentProject?.address?.addressLine1 || "NA" },
                 { title: "WORKS_CITY",value: currentProject?.address?.city ? t(`TENANT_TENANTS_${Digit.Utils.locale.getTransformedLocale(currentProject?.address?.city)}`) : "NA" }, //will check with Backend
                 { title: "WORKS_WARD", value: currentProject?.address?.boundary ? t(`${headerLocale}_ADMIN_${currentProject?.address?.boundary}`) : "NA"  }, ///backend to update this
-                { title: "WORKS_LOCALITY",value: currentProject?.additionalDetails?.locality ? t(`${headerLocale}_ADMIN_${currentProject?.additionalDetails?.locality?.code}`) : "NA" },
+                { title: "WORKS_LOCALITY",value: currentProject?.additionalDetails?.locality ? t(`${headerLocale}_ADMIN_${currentProject?.additionalDetails?.locality}`) : "NA" },
             ]
         };
 
