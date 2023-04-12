4972c78788cbbd17ef862aa7546aa81214cef683
Hariom Sinha <hariom.sinha@egovernments.org>
2023-04-12 13:17:43 +0530

    fixed padding issues

--- frontend/micro-ui/web/micro-ui-internals/packages/react-components/src/hoc/UploadFileComposer.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/react-components/src/hoc/UploadFileComposer.js
@@ -69,7 +69,7 @@ const UploadFileComposer = ({module, config, Controller, control, register, form
   // if(isLoading) return <Loader />
   return (
     <React.Fragment>
-      <Header styles={{fontSize: "24px"}}>{t('WORKS_RELEVANT_DOCUMENTS')}</Header>
+      <Header styles={{fontSize: "24px", marginTop : "40px"}}>{t('WORKS_RELEVANT_DOCUMENTS')}</Header>
       <CitizenInfoLabel info={t("ES_COMMON_INFO")} text={t(docConfig?.bannerLabel)} className="doc-banner"></CitizenInfoLabel>
       {
         docConfig?.documents?.map((item, index) => {
