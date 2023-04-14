182968d37c4ce78a12527ce336d6119b20507462
Subhashini Srinivasan <subhashini.srinivasan@egovernments.org>
2023-04-14 09:15:04 +0530

    Added modifications to the schema

---
+++ backend/expense-calculator/src/main/resources/db/migration/main/V20230414090602__create_documents_table.sql
@@ -0,0 +1,15 @@
+CREATE TABLE eg_works_calculation_documents (
+  id                          character varying(256),
+  filestore_id                character varying(64),
+  document_type               character varying(64),
+  document_uid                character varying(256),
+  status                      character varying(64),
+  calculation_id              character varying(256),
+  additional_details          JSONB,
+  created_by                  character varying(256)  NOT NULL,
+  last_modified_by            character varying(256),
+  created_time                bigint,
+  last_modified_time          bigint,
+  CONSTRAINT pk_eg_works_calculation_documents PRIMARY KEY (id),
+  CONSTRAINT fk_eg_works_calculation_documents FOREIGN KEY (calculation_id) REFERENCES eg_works_calculation (id)
+);
