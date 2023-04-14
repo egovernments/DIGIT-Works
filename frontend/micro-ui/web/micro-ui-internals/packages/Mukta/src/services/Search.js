13e356cee0af97959afe2c83fe464e56932338f0
Hariom Sinha <hariom.sinha@egovernments.org>
2023-04-14 16:00:28 +0530

    fixed financial details issue

--- frontend/micro-ui/web/micro-ui-internals/packages/Mukta/src/services/Search.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/Mukta/src/services/Search.js
@@ -98,7 +98,6 @@ const createProjectsArray = (t, project, searchParams, headerLocale) => {
                 headerDetails, 
                 projectDetails, 
                 locationDetails, 
-                financialDetails,
                 documentDetails
             }
         // }
@@ -124,7 +123,7 @@ export const Search = {
         
             //searched Project details
             projectDetails.searchedProject['basicDetails'] = projects?.searchedProject?.basicDetails;
-            projectDetails.searchedProject['details']['projectDetails'] = {applicationDetails : [projects?.searchedProject?.headerDetails, projects?.searchedProject?.projectDetails, projects?.searchedProject?.locationDetails,projects?.searchedProject?.financialDetails, projects?.searchedProject?.documentDetails]}; //rest categories will come here
+            projectDetails.searchedProject['details']['projectDetails'] = {applicationDetails : [projects?.searchedProject?.headerDetails, projects?.searchedProject?.projectDetails, projects?.searchedProject?.locationDetails, projects?.searchedProject?.documentDetails]}; //rest categories will come here
     
         }
 
