c389d0b17af436514040bd5de993ae4426135a9a
pintu-eGov <pintu.kumar@egovernments.org>
2023-04-18 15:22:03 +0530

    bill-payment : flyway scripts changes and added logger

--- backend/expense/src/main/resources/db/migration/main/V20230408180150__eg_expense_payment_create_ddl.sql
+++ backend/expense/src/main/resources/db/migration/main/V20230408180150__eg_expense_payment_create_ddl.sql
@@ -1,26 +1,27 @@
 CREATE TABLE IF NOT EXISTS eg_expense_payment
 (
-id character varying(64)   NOT NULL,
-tenantid character varying(250)   NOT NULL,
-netPayableAmount numeric(12,2),
-netPaidAmount numeric(12,2),
+id character varying(256)   NOT NULL,
+tenant_id character varying(64)   NOT NULL,
+net_payable_amount numeric(12,2),
+net_paid_amount numeric(12,2),
 status character varying(64) NOT NULL,
-createdby character varying(64)   NOT NULL,
-createdtime bigint NOT NULL,
-lastmodifiedby character varying(64) NOT NULL,
-lastmodifiedtime bigint NOT NULL,
-additionaldetails jsonb,
+created_by character varying(64)   NOT NULL,
+created_time bigint NOT NULL,
+last_modified_by character varying(64) NOT NULL,
+last_modified_time bigint NOT NULL,
+additional_details jsonb,
 
-CONSTRAINT pk_eg_expense_payment PRIMARY KEY (id,tenantid)
+CONSTRAINT pk_eg_expense_payment PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS eg_expense_bill_payment
 (
+id character varying(256)   NOT NULL,
+payment_id character varying(256)   NOT NULL,
+bill_id character varying(256)   NOT NULL,
+tenant_id character varying(64)   NOT NULL,
 
-paymentid character varying(64)   NOT NULL,
-billid character varying(64)   NOT NULL,
-tenantid character varying(250)   NOT NULL,
-
-CONSTRAINT pk_eg_expense_bill_payment PRIMARY KEY (paymentid,billid,tenantid)
+CONSTRAINT pk_eg_expense_bill_payment PRIMARY KEY (id),
+CONSTRAINT fk_eg_expense_bill_payment FOREIGN KEY (payment_id) REFERENCES eg_expense_payment (id)
 );
 
