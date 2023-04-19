3b50513fd6846f127981e81f7fc36c29573edcce
Ramkrishna-egov <85437265+Ramkrishna-egov@users.noreply.github.com>
2023-04-19 18:39:03 +0530

    Health Main Parallel branch merged with develop and few Label CSS changes (#714)
    * Health Main Parallel branch merged, and few Label CSS changes
    
    * ORG Profile Translator added for Class

--- frontend/works_shg_app/lib/pages/login.dart
+++ frontend/works_shg_app/lib/pages/login.dart
@@ -160,8 +160,8 @@ class _LoginPage extends State<LoginPage> {
             return MobileView(
               getLoginCard(context),
               GlobalVariables.stateInfoListModel!.bannerUrl.toString(),
-              logoBottomPosition: constraints.maxWidth / 3,
-              cardBottomPosition: constraints.maxWidth / 1.5,
+              logoBottomPosition: constraints.maxHeight / 4,
+              cardBottomPosition: constraints.maxHeight / 3,
             );
           } else {
             return DesktopView(getLoginCard(context),
