15a3a680846dd1659d381cf0ece457fdfcdf75aa
Jagankumar <53823168+jagankumar-egov@users.noreply.github.com>
2023-04-23 18:07:30 +0530

    PFM-2801 :: Sidebar and other Audit css issue fix (#742)
    * PFM-2801 :: Sidebar and other Audit css issue fix
    
    * pfm-2801: fixed other changes
    
    * PFM-2801 :: FIxed other look and feel changes according to UX Audit
    
    * PFM-2800 : Login screen enter issue fixed

--- frontend/micro-ui/web/micro-ui-internals/packages/react-components/src/atoms/Amount.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/react-components/src/atoms/Amount.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Amount = (props) => {
     return (
-        <p style={props?.customStyle}>{props?.value ? `${Digit.Utils.dss.formatterWithoutRound(props?.value, "number")}` : t("ES_COMMON_NA")}</p>
+        <p style={props?.customStyle}>{props?.value ? `${Digit?.Utils?.dss?.formatterWithoutRound(props?.value, "number")}` : t("ES_COMMON_NA")}</p>
     )
 }
 
