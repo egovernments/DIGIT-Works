7a859e2377ade6fb0025d0e60aa73d54916bf362
Jagankumar <53823168+jagankumar-egov@users.noreply.github.com>
2023-04-24 17:21:49 +0530

    PFM-2801  :: Audit fixes. ui changes and pdf download name changes

--- frontend/micro-ui/web/micro-ui-internals/packages/react-components/src/hoc/ResultsTable.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/react-components/src/hoc/ResultsTable.js
@@ -176,7 +176,6 @@ const ResultsTable = ({ tableContainerClass, config,data,isLoading,isFetching,fu
                         style: {
                             padding: "20px 18px",
                             fontSize: "16px",
-                            wordBreak : "break-all",
                             whiteSpace: "break-spaces",
                         },
                     };
