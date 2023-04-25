38fd7d4bf83a21bca60543a6ebe5b2f3e592f586
Jagankumar <53823168+jagankumar-egov@users.noreply.github.com>
2023-04-25 11:29:26 +0530

    PFM-2730 :: Fixed the UI Audit and updated the pdf file name (#757)
    * PFM-2730 :: Fixed the UI Audit and updated the pdf file name
    
    * updated versions
    
    * @egovernments/digit-ui-module-project version updated
    
    * Update package.json
    
    * [PFM-2961] - Updated UICustomizations for search and wageseeker config (#758)
    
    * [PFM-2805] | [Prachi] | Updated local configs for organization and muster
    
    * [PFM-2824] | [Prachi] | Updated UI customizations to trim text input params
    
    * [PFM-2824] | [Prachi] | Updated UI customizations to trim text input params
    
    * [PFM-2848] | [Prachi] | Updated configs for create, search org and wage seeker: added limits as per contracts
    
    * [PFM-2848] | [Prachi] | Updated configs for org, regByDept field converted dropdown to text, added default valid to date
    
    * [PFM-2848] | [Prachi] | Updated package json for masters
    
    * [PFM-2848] | [Prachi] | alignment issues in org create table
    
    * [PFM-2848] | [Prachi] | alignment issues in org create table
    
    * [Audit-Fix] | [Prachi] | Audit fixes for Search Screen
    
    * [PFM-2903] | [Prachi] | Updated payload to set default identifier type PAN and junk value if nothing is entered by user
    
    * [PFM-2961] | [Prachi] | Updated UICustomizations for search Individual and search org screen
    
    * [PFM-2961] | [Prachi] | Updated package json for masters
    
    ---------
    
    Co-authored-by: Prachi Rane <prachirane@Prachis-MacBook-Pro-2.local>
    Co-authored-by: Prachi Rane <prachirane@192.168.1.10>
    Co-authored-by: Prachi Rane <prachirane@192.168.1.2>
    Co-authored-by: Prachi Rane <prachirane@192.168.1.4>
    
    ---------
    
    Co-authored-by: prachirane-eGov <117716375+prachirane-eGov@users.noreply.github.com>
    Co-authored-by: Prachi Rane <prachirane@Prachis-MacBook-Pro-2.local>
    Co-authored-by: Prachi Rane <prachirane@192.168.1.10>
    Co-authored-by: Prachi Rane <prachirane@192.168.1.2>
    Co-authored-by: Prachi Rane <prachirane@192.168.1.4>

--- frontend/micro-ui/web/micro-ui-internals/packages/Mukta/src/configs/UICustomizations.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/Mukta/src/configs/UICustomizations.js
@@ -767,6 +767,14 @@ export const UICustomizations = {
     }
   },
   SearchWageSeekerConfig: {
+    customValidationCheck: (data) => {
+      //checking both to and from date are present
+      const { createdFrom, createdTo } = data;
+      if ((createdFrom === "" && createdTo !== "") || (createdFrom !== "" && createdTo === ""))
+        return { warning: true, label: "ES_COMMON_ENTER_DATE_RANGE" };
+
+      return false;
+    },
     preProcess: (data) => {
       data.params = { ...data.params, tenantId: Digit.ULBService.getCurrentTenantId() };
 
@@ -861,6 +869,14 @@ export const UICustomizations = {
     }
   },
   SearchOrganisationConfig: {
+    customValidationCheck: (data) => {
+      //checking both to and from date are present
+      const { createdFrom, createdTo } = data;
+      if ((createdFrom === "" && createdTo !== "") || (createdFrom !== "" && createdTo === ""))
+        return { warning: true, label: "ES_COMMON_ENTER_DATE_RANGE" };
+
+      return false;
+    },
     preProcess: (data) => {  
         let requestBody = { ...data.body.SearchCriteria };
         const pathConfig = {
