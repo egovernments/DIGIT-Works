b869ce7f5a88f77deda31373062e58c097c30dc0
Ramkrishna-egov <ramkrishna.sahoo@egovernments.org>
2023-04-26 14:29:58 +0530

    PFM-2971:: Muster Creation Fixes

--- frontend/works_shg_app/lib/models/user_search/user_search_model.dart
+++ frontend/works_shg_app/lib/models/user_search/user_search_model.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: invalid_annotation_target
 
 import 'package:freezed_annotation/freezed_annotation.dart';
-import 'package:works_shg_app/models/UserDetails/user_details_model.dart';
+import 'package:works_shg_app/models/user_details/user_details_model.dart';
 
 part 'user_search_model.freezed.dart';
 part 'user_search_model.g.dart';
