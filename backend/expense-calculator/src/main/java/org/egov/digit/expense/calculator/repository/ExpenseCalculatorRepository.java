4c22b6fc483b67e37b9e79cb3248f98daadfc8dd
harshal-egov <117713108+harshal-egov@users.noreply.github.com>
2023-04-28 13:15:41 +0530

    PFM-2968: Add calculation search API (#782)

--- backend/expense-calculator/src/main/java/org/egov/digit/expense/calculator/repository/ExpenseCalculatorRepository.java
+++ backend/expense-calculator/src/main/java/org/egov/digit/expense/calculator/repository/ExpenseCalculatorRepository.java
@@ -2,6 +2,8 @@ package org.egov.digit.expense.calculator.repository;
 
 import org.egov.digit.expense.calculator.repository.querybuilder.ExpenseCalculatorQueryBuilder;
 import org.egov.digit.expense.calculator.repository.rowmapper.ExpenseCalculatorRowMapper;
+import org.egov.digit.expense.calculator.web.models.CalculatorSearchCriteria;
+import org.egov.digit.expense.calculator.web.models.CalculatorSearchRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
@@ -32,5 +34,17 @@ public class ExpenseCalculatorRepository {
         String query = queryBuilder.getMusterRollsOfContract(contractId, billType, billIds, preparedStmtList);
         List<String> musterrollIds = jdbcTemplate.query(query, rowMapper, preparedStmtList.toArray());
         return musterrollIds;
+    }
+
+    /**
+     * Fetch the record from DB based on the calculatorSearchCritera
+     * @param calculatorSearchRequest
+     * @return
+     */
+    public List<String> getBillIds(CalculatorSearchRequest calculatorSearchRequest) {
+        List<Object> preparedStmtList = new ArrayList<>();
+        String query = queryBuilder.getBillIds(calculatorSearchRequest,preparedStmtList);
+        List<String> billIds = jdbcTemplate.queryForList(query,String.class,preparedStmtList.toArray());
+        return billIds;
     }
 }
--- backend/expense-calculator/src/main/java/org/egov/digit/expense/calculator/repository/querybuilder/ExpenseCalculatorQueryBuilder.java
+++ backend/expense-calculator/src/main/java/org/egov/digit/expense/calculator/repository/querybuilder/ExpenseCalculatorQueryBuilder.java
@@ -1,16 +1,26 @@
 package org.egov.digit.expense.calculator.repository.querybuilder;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import org.apache.commons.lang3.StringUtils;
+import org.egov.digit.expense.calculator.web.models.CalculatorSearchCriteria;
+import org.egov.digit.expense.calculator.web.models.CalculatorSearchRequest;
 import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class ExpenseCalculatorQueryBuilder {
 
 
     private static final String FETCH_CALCULATE_QUERY = "SELECT musterroll_num FROM eg_works_calculation ";
+
+    private static final String FETCH_CALCULATE_BILL_IDS_QUERY = "SELECT bill_id FROM eg_works_calculation ";
+
 
 
     public String getMusterRollsOfContract(String contractId, String billType, List<String> billIds, List<Object> preparedStmtList) {
@@ -32,6 +42,107 @@ public class ExpenseCalculatorQueryBuilder {
             addClauseIfRequired(preparedStmtList, queryBuilder);
             queryBuilder.append("bill_id IN (").append(createQuery(billIds)).append(")");
             addToPreparedStatement(preparedStmtList, billIds);
+        }
+
+        return queryBuilder.toString();
+    }
+
+    public String getBillIds(CalculatorSearchRequest calculatorSearchRequest, List<Object> preparedStmtList) {
+        StringBuilder queryBuilder = new StringBuilder(FETCH_CALCULATE_BILL_IDS_QUERY);
+
+        List<String> projectNumbers=new ArrayList<>();
+        List<String> contractNumbers=new ArrayList<>();
+        List<String> orgNumbers=new ArrayList<>();
+        List<String> musterRollNumbers=new ArrayList<>();
+        List<String> billNumbers=new ArrayList<>();
+        List<String> billTypes=new ArrayList<>();
+        List<String> tenantIds=new ArrayList<>();
+        List<String> billRefIds=new ArrayList<>();
+
+
+
+        if(!CollectionUtils.isEmpty(calculatorSearchRequest.getSearchCriterias())){
+            for(CalculatorSearchCriteria calculatorSearchCriteria: calculatorSearchRequest.getSearchCriterias()){
+                if(StringUtils.isNotBlank(calculatorSearchCriteria.getProjectNumber())){
+                    projectNumbers.add(calculatorSearchCriteria.getProjectNumber());
+                }
+
+                if(StringUtils.isNotBlank(calculatorSearchCriteria.getContractNumber())){
+                    contractNumbers.add(calculatorSearchCriteria.getContractNumber());
+                }
+
+                if(StringUtils.isNotBlank(calculatorSearchCriteria.getOrgNumber())){
+                    orgNumbers.add(calculatorSearchCriteria.getOrgNumber());
+                }
+
+                if(StringUtils.isNotBlank(calculatorSearchCriteria.getMusterRollNumber())){
+                    musterRollNumbers.add(calculatorSearchCriteria.getMusterRollNumber());
+                }
+
+                if(StringUtils.isNotBlank(calculatorSearchCriteria.getBillNumber())){
+                    billNumbers.add(calculatorSearchCriteria.getBillNumber());
+                }
+
+                if(StringUtils.isNotBlank(calculatorSearchCriteria.getBillType())){
+                    billTypes.add(calculatorSearchCriteria.getBillType());
+                }
+
+                if(StringUtils.isNotBlank(calculatorSearchCriteria.getTenantId())){
+                    tenantIds.add(calculatorSearchCriteria.getTenantId());
+                }
+
+                if(StringUtils.isNotBlank(calculatorSearchCriteria.getBillReferenceId())){
+                    billRefIds.add(calculatorSearchCriteria.getBillReferenceId());
+                }
+            }
+        }
+
+        if (!tenantIds.isEmpty()) {
+            addClauseIfRequired(preparedStmtList, queryBuilder);
+            queryBuilder.append(" tenant_id IN (").append(createQuery(tenantIds)).append(")");
+            addToPreparedStatement(preparedStmtList,tenantIds);
+        }
+
+        if (!projectNumbers.isEmpty()) {
+            addClauseIfRequired(preparedStmtList, queryBuilder);
+            queryBuilder.append(" project_number IN (").append(createQuery(projectNumbers)).append(")");
+            addToPreparedStatement(preparedStmtList,projectNumbers);
+        }
+
+        if (!contractNumbers.isEmpty()) {
+            addClauseIfRequired(preparedStmtList, queryBuilder);
+            queryBuilder.append(" contract_number IN (").append(createQuery(contractNumbers)).append(")");
+            addToPreparedStatement(preparedStmtList,contractNumbers);
+        }
+
+        if (!orgNumbers.isEmpty()) {
+            addClauseIfRequired(preparedStmtList, queryBuilder);
+            queryBuilder.append(" org_id IN (").append(createQuery(orgNumbers)).append(")");
+            addToPreparedStatement(preparedStmtList,orgNumbers);
+        }
+
+        if (!musterRollNumbers.isEmpty()) {
+            addClauseIfRequired(preparedStmtList, queryBuilder);
+            queryBuilder.append(" musterroll_number IN (").append(createQuery(musterRollNumbers)).append(")");
+            addToPreparedStatement(preparedStmtList,musterRollNumbers);
+        }
+
+        if (!billNumbers.isEmpty()) {
+            addClauseIfRequired(preparedStmtList, queryBuilder);
+            queryBuilder.append(" bill_number IN (").append(createQuery(billNumbers)).append(")");
+            addToPreparedStatement(preparedStmtList,billNumbers);
+        }
+
+        if (!billTypes.isEmpty()) {
+            addClauseIfRequired(preparedStmtList, queryBuilder);
+            queryBuilder.append(" business_service IN (").append(createQuery(billTypes)).append(")");
+            addToPreparedStatement(preparedStmtList,billTypes);
+        }
+
+        if (!billRefIds.isEmpty()) {
+            addClauseIfRequired(preparedStmtList, queryBuilder);
+            queryBuilder.append(" bill_reference IN (").append(createQuery(billRefIds)).append(")");
+            addToPreparedStatement(preparedStmtList,billRefIds);
         }
 
         return queryBuilder.toString();
