df1b3356bb880d2d90ef01339b9cd92bac392245
kavi_elrey@1993 <25226238+kavi-egov@users.noreply.github.com>
2023-05-09 10:51:47 +0530

    lineitem default value set (#908)

--- backend/expense/src/main/java/org/egov/digit/expense/web/validators/BillValidator.java
+++ backend/expense/src/main/java/org/egov/digit/expense/web/validators/BillValidator.java
@@ -247,7 +247,7 @@ public class BillValidator {
 			for (LineItem item : billDetail.getLineItems()) {
 
 				BigDecimal amount = item.getAmount();
-				BigDecimal paidAmount = item.getPaidAmount();
+				BigDecimal paidAmount = item.getPaidAmount() != null ? item.getPaidAmount() : BigDecimal.ZERO;
 
 				if (!headCodeList.contains(item.getHeadCode()))
 					missingHeadCodes.add(item.getHeadCode());
@@ -257,15 +257,16 @@ public class BillValidator {
 							"The tax amount : " + amount + " cannot be lesser than the paid amount : " + paidAmount);
 			}
 
-			for (LineItem item : billDetail.getPayableLineItems()) {
+			for (LineItem payableLineItem : billDetail.getPayableLineItems()) {
 
-				BigDecimal amount = item.getAmount();
-				BigDecimal paidAmount = item.getPaidAmount();
+				BigDecimal amount = payableLineItem.getAmount();
+				BigDecimal paidAmount = payableLineItem.getPaidAmount() != null ? payableLineItem.getPaidAmount()
+						: BigDecimal.ZERO;
 				billDetailAmount = billDetailAmount.add(amount);
 				billDetailPaidAmount = billDetailPaidAmount.add(paidAmount);
 
-				if (!headCodeList.contains(item.getHeadCode()))
-					missingHeadCodes.add(item.getHeadCode());
+				if (!headCodeList.contains(payableLineItem.getHeadCode()))
+					missingHeadCodes.add(payableLineItem.getHeadCode());
 
 				if (amount.compareTo(paidAmount) < 0)
 					errorMap.put("EG_EXPENSE_LINEITEM_INVALID_AMOUNT",
