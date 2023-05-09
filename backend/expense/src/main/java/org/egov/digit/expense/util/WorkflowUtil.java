ac70bbf032035ad7bf7102ac23bc392c0a0a9494
kavi_elrey <25226238+kavi-egov@users.noreply.github.com>
2023-05-09 18:17:46 +0530

    docs added & assignes

--- backend/expense/src/main/java/org/egov/digit/expense/util/WorkflowUtil.java
+++ backend/expense/src/main/java/org/egov/digit/expense/util/WorkflowUtil.java
@@ -117,6 +117,16 @@ public class WorkflowUtil {
     	Workflow workflowFromRequest = billRequest.getWorkflow();
     	List<User> assignes = new ArrayList<>();
     	
+    	for (String userId : workflowFromRequest.getAssignes()) {
+    		
+    		User user = User.builder()
+    				.tenantId(bill.getTenantId())
+    				.uuid(userId)
+    				.build();
+    		
+    		assignes.add(user);
+    	}
+    	
     	ProcessInstance processInstance = ProcessInstance.builder()
     			.documents(workflowFromRequest.getVerificationDocuments())
     			.moduleName(configs.getExpenseWorkflowModuleName())
