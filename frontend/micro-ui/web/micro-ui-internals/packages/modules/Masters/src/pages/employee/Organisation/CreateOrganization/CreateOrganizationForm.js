444b0c3a68a00260e8251d2dafd7b0b44e72798e
Ramkrishna-egov <85437265+Ramkrishna-egov@users.noreply.github.com>
2023-05-25 21:27:58 +0530

    PFM-3311, PFM-3192,PFM-3194:: Validation on Dates in Estimate and Org, Bank name null fix, CBO Documents mapping in WO  (#1048)
    * Test Org andEstimate
    
    * Revert "Test Org andEstimate"
    
    This reverts commit fc019e161a0de4abfc5a007c3b1ae5ccb724e791.
    
    * Revert "Revert "Test Org andEstimate""
    
    This reverts commit 9f91f065088d50ef5e97099a54cb41d0a942a2a6.
    
    * PFM-3192, PFM-3194
    
    * Revert Estimate  Plain Search
    
    * Sonar cloud Bug fixes
    
    * PFM-3367, PFM-3192
    
    * Update package.json
    
    * Update package.json
    
    * Update package.json
    
    ---------
    
    Co-authored-by: Jagankumar <53823168+jagankumar-egov@users.noreply.github.com>

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/Masters/src/pages/employee/Organisation/CreateOrganization/CreateOrganizationForm.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/Masters/src/pages/employee/Organisation/CreateOrganization/CreateOrganizationForm.js
@@ -31,6 +31,7 @@ const CreateOrganizationForm = ({ createOrganizationConfig, sessionFormData, set
     const [selectedWard, setSelectedWard] = useState(sessionFormData?.locDetails_ward?.code || '')
     const [selectedOrg, setSelectedOrg] = useState('')
     const [showDuplicateUserError, setShowDuplicateUserError] = useState(false)
+    const [showValidToError, setShowValidToError] = useState(false)
 
     const { mutate: CreateOrganisationMutation } = Digit.Hooks.organisation.useCreateOrganisation();
     const { mutate: UpdateOrganisationMutation } = Digit.Hooks.organisation.useUpdateOrganisation();
@@ -40,6 +41,7 @@ const CreateOrganizationForm = ({ createOrganizationConfig, sessionFormData, set
 
     //location data
     const ULB = Digit.Utils.locale.getCityLocale(tenantId);
+    const ORG_VALIDTO_DATE = '2099-03-31';
     let ULBOptions = []
     ULBOptions.push({code: tenantId, name: t(ULB),  i18nKey: ULB });
 
@@ -159,6 +161,13 @@ const CreateOrganizationForm = ({ createOrganizationConfig, sessionFormData, set
         }
     },[showDuplicateUserError]);
 
+    useEffect(() => {
+        if(showValidToError) {
+            setTimeout(()=>{
+                setShowValidToError(false);
+            },3000);
+        }
+    },[showValidToError]);
     const onFormValueChange = async (setValue, formData, formState, reset, setError, clearErrors, trigger, getValues) => {
         if (!_.isEqual(sessionFormData, formData)) {
             const difference = _.pickBy(sessionFormData, (v, k) => !_.isEqual(formData[k], v));
@@ -257,7 +266,11 @@ const CreateOrganizationForm = ({ createOrganizationConfig, sessionFormData, set
     }
 
     const onSubmit = async (data) => {
-        const orgPayload = getOrgPayload({formData: data, orgDataFromAPI, tenantId, isModify})
+        if((data?.funDetails_validTo ? Digit.Utils.pt.convertDateToEpoch(data?.funDetails_validTo) : Digit.Utils.pt.convertDateToEpoch(ORG_VALIDTO_DATE)) < Digit.Utils.pt.convertDateToEpoch(data?.funDetails_validFrom)){
+            setShowValidToError(true);
+        }
+        else{
+            const orgPayload = getOrgPayload({formData: data, orgDataFromAPI, tenantId, isModify})
         if(isModify) {
             const bankAccountPayload = getBankAccountUpdatePayload({formData: data, apiData: orgDataFromAPI, tenantId, isModify, referenceId: '', isWageSeeker: false});
             handleResponseForUpdate(orgPayload, bankAccountPayload);
@@ -269,6 +282,7 @@ const CreateOrganizationForm = ({ createOrganizationConfig, sessionFormData, set
             }
             handleResponseForCreate(orgPayload, data);
         }
+    }
     }   
 
     if(locationDataFetching || orgDataFetching) return <Loader/>
@@ -297,6 +311,9 @@ const CreateOrganizationForm = ({ createOrganizationConfig, sessionFormData, set
             />
             {
                 showDuplicateUserError && <Toast error={true} label={t("ES_COMMON_MOBILE_EXISTS_ERROR")} isDleteBtn={true} onClose={() => setShowDuplicateUserError(false)} />
+            }
+            {
+                showValidToError && <Toast error={true} label={t("DATE_VALIDATION_MSG")} isDleteBtn={true} onClose={() => setShowValidToError(false)} />
             }
         </React.Fragment>
     )
