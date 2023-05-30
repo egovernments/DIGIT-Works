854f1090d1ccd9a3d65918d4816b2ecd56fd6e65
Nipun Arora <aroranipun1@gmail.com>
2023-05-30 15:05:50 +0530

    pfm-3362

--- frontend/micro-ui/web/micro-ui-internals/packages/libraries/src/hooks/works/useSearchOrg.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/libraries/src/hooks/works/useSearchOrg.js
@@ -2,7 +2,7 @@ import { useQuery } from "react-query";
 import { WorksService } from "../../services/elements/Works";
 
 const useSearchOrg = (payload,config) => {
-    return useQuery(["ORG_SEARCH"], () => WorksService.searchOrg(payload),config)
+    return useQuery(["ORG_SEARCH",payload], () => WorksService.searchOrg(payload),config)
 }
 
 export default useSearchOrg;
