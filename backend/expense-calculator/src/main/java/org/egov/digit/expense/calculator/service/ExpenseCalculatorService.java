bb273ea8041928b99b9399f30aaa41f7aa33d909
Shailesh Pandey <110380977+shailesh-egov@users.noreply.github.com>
2023-06-06 12:44:03 +0530

    [PFM-3446] Added orgid in metadata of supervision bill generation (#1100)

--- backend/expense-calculator/src/main/java/org/egov/digit/expense/calculator/service/ExpenseCalculatorService.java
+++ backend/expense-calculator/src/main/java/org/egov/digit/expense/calculator/service/ExpenseCalculatorService.java
@@ -1,20 +1,5 @@
 package org.egov.digit.expense.calculator.service;
 
-import static org.egov.digit.expense.calculator.util.ExpenseCalculatorServiceConstants.CONTRACT_ID_CONSTANT;
-import static org.egov.digit.expense.calculator.util.ExpenseCalculatorServiceConstants.JSON_PATH_FOR_APPLICABLE_CHARGES;
-import static org.egov.digit.expense.calculator.util.ExpenseCalculatorServiceConstants.JSON_PATH_FOR_BUSINESS_SERVICE_VERIFICATION;
-import static org.egov.digit.expense.calculator.util.ExpenseCalculatorServiceConstants.JSON_PATH_FOR_HEAD_CODES;
-import static org.egov.digit.expense.calculator.util.ExpenseCalculatorServiceConstants.JSON_PATH_FOR_LABOUR_CHARGES;
-import static org.egov.digit.expense.calculator.util.ExpenseCalculatorServiceConstants.JSON_PATH_FOR_PAYER;
-import static org.egov.digit.expense.calculator.util.ExpenseCalculatorServiceConstants.MDMS_APPLICABLE_CHARGES;
-import static org.egov.digit.expense.calculator.util.ExpenseCalculatorServiceConstants.MDMS_BUSINESS_SERVICE;
-import static org.egov.digit.expense.calculator.util.ExpenseCalculatorServiceConstants.MDMS_HEAD_CODES;
-import static org.egov.digit.expense.calculator.util.ExpenseCalculatorServiceConstants.MDMS_PAYER_LIST;
-import static org.egov.digit.expense.calculator.util.ExpenseCalculatorServiceConstants.PROJECT_ID_CONSTANT;
-import static org.egov.digit.expense.calculator.util.ExpenseCalculatorServiceConstants.PROJECT_ID_OF_CONSTANT;
-import static org.egov.digit.expense.calculator.util.ExpenseCalculatorServiceConstants.SUCCESSFUL_CONSTANT;
-import static org.egov.digit.expense.calculator.util.ExpenseCalculatorServiceConstants.WF_SUBMIT_ACTION_CONSTANT;
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
@@ -65,6 +50,8 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import lombok.extern.slf4j.Slf4j;
+
+import static org.egov.digit.expense.calculator.util.ExpenseCalculatorServiceConstants.*;
 
 @Slf4j
 @Service
@@ -252,6 +239,8 @@ public class ExpenseCalculatorService {
 			if (contract.getContractNumber()!=null && projectIdOptional.isPresent()) {
 				contractProjectMapping.put(PROJECT_ID_OF_CONSTANT + contract.getContractNumber(), projectIdOptional.get());
 			}
+            // Put OrgId in meta
+            contractProjectMapping.put(ORG_ID_CONSTANT,contract.getOrgId());
 			metaInfo.putAll(contractProjectMapping);
 
 		}
