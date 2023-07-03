8d0e9e20fd0e9e3a7dfa667c9a3a51ae1921fbf7
shubhang-egov <shubhang.dhanesha@egovernments.org>
2023-07-03 12:11:15 +0530

    PFM-3712-add only if payable line item is active and generate payment only if puchase bill is approved

--- backend/expense/src/main/java/org/egov/digit/expense/kafka/Consumer.java
+++ backend/expense/src/main/java/org/egov/digit/expense/kafka/Consumer.java
@@ -1,6 +1,5 @@
 package org.egov.digit.expense.kafka;
 
-import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,11 +7,14 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.egov.digit.expense.service.PaymentService;
 import org.egov.digit.expense.web.models.*;
+import org.egov.digit.expense.web.models.enums.Status;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Component;
+
+import static org.egov.digit.expense.config.Constants.*;
 
 @Component
 @Slf4j
@@ -28,19 +30,24 @@ public class Consumer {
         try {
             BillRequest bill = objectMapper.readValue(consumerRecord, BillRequest.class);
 
+            if(bill.getBill().getBusinessService().equalsIgnoreCase(EXPENSE_SUPERVISION_CODE) ||
+                    (bill.getBill().getBusinessService().equalsIgnoreCase(EXPENSE_WAGES_CODE)) ||
+                    (bill.getBill().getBusinessService().equalsIgnoreCase(EXPENSE_PURCHASE_CODE) && bill.getWorkflow().getAction().equalsIgnoreCase(APPROVE_CODE))){
             List<PaymentBillDetail> paymentBillDetails = new ArrayList<>();
             for(BillDetail billDetail : bill.getBill().getBillDetails()) {
                 List<PaymentLineItem> payableLineItems = new ArrayList<>();
                 for (LineItem lineItem : billDetail.getPayableLineItems()) {
-                    PaymentLineItem payableLineItem = PaymentLineItem.builder().lineItemId(lineItem.getId())
-                            .paidAmount(BigDecimal.ZERO).tenantId(lineItem.getTenantId()).build();
-                    payableLineItems.add(payableLineItem);
+                    if(lineItem.getStatus().equals(Status.ACTIVE)) {
+                        PaymentLineItem payableLineItem = PaymentLineItem.builder().lineItemId(lineItem.getId())
+                                .paidAmount(lineItem.getAmount()).tenantId(lineItem.getTenantId()).build();
+                        payableLineItems.add(payableLineItem);
+                    }
                 }
                 PaymentBillDetail paymentBillDetail = PaymentBillDetail.builder()
                         .billDetailId(billDetail.getBillId())
                         .payableLineItems(payableLineItems)
                         .totalAmount(billDetail.getTotalAmount())
-                        .totalPaidAmount(BigDecimal.ZERO).build();
+                        .totalPaidAmount(billDetail.getTotalAmount()).build();
                 paymentBillDetails.add(paymentBillDetail);
             }
 
@@ -50,12 +57,12 @@ public class Consumer {
                     .billDetails(paymentBillDetails)
                     .tenantId(bill.getBill().getTenantId())
                     .totalAmount(bill.getBill().getTotalAmount())
-                    .totalPaidAmount(BigDecimal.ZERO).build();
+                    .totalPaidAmount(bill.getBill().getTotalAmount()).build();
             paymentBills.add(paymentBill);
 
             Payment payment = Payment.builder()
                     .bills(paymentBills)
-                    .netPaidAmount(BigDecimal.ZERO)
+                    .netPaidAmount(bill.getBill().getTotalAmount())
                     .netPayableAmount(bill.getBill().getTotalAmount())
                     .tenantId(bill.getBill().getTenantId()).build();
 
@@ -66,7 +73,7 @@ public class Consumer {
             PaymentResponse paymentResponse = paymentService.create(paymentRequest);
             log.info(paymentResponse.toString());
 
-        }catch (Exception e){
+        }}catch (Exception e){
             log.error("Error while fetching bills", e);
         }
 
