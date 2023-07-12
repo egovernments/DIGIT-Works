0c20c9aeb185bd399eb96050d07aaa1da98ba1b2
Ramkrishna-egov <ramkrishna.sahoo@egovernments.org>
2023-07-12 13:48:23 +0530

    flutter training project

--- frontend/flutter_training/lib/lib/utils/models/file_picker_data.dart
+++
@@ -1,7 +0,0 @@
-import 'dart:io';
-import 'dart:typed_data';
-
-class FilePickerData {
-  static File? imageFile;
-  static Uint8List? bytes;
-}
--- frontend/flutter_training/lib/lib/utils/string_extension.dart
+++
@@ -1,8 +0,0 @@
-extension StringExtension on String {
-  String capitalize() {
-    if (this.isEmpty) {
-      return this;
-    }
-    return this[0].toUpperCase() + this.substring(1);
-  }
-}
