a03ae1fc3caaa5c16c829e0347d0048ee895d051
Nipun Arora <aroranipun1@gmail.com>
2023-07-13 17:44:46 +0530

    Update svgindex.js

--- frontend/micro-ui/web/micro-ui-internals/packages/react-components/src/atoms/svgindex.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/react-components/src/atoms/svgindex.js
@@ -176,7 +176,7 @@ const StarEmpty = ({ className, onClick, styles }) => (
   </svg>
 );
 
-const DownloadImgIcon = (styles) => (
+const DownloadImgIcon = ({styles}) => (
   <svg width="14" height="17" style={styles} viewBox="0 0 14 17" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path d="M14 6H10V0H4V6H0L7 13L14 6ZM0 15V17H14V15H0Z" fill="#F47738" />
   </svg>
