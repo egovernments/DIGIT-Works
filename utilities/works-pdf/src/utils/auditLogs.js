0027ab8e6f65fb412005dee8c6dccb816d6ac763
SumelRattan-eGov <sumel.rattan@egovernments.org>
2023-07-14 14:46:40 +0530

    added-try-catch

--- utilities/works-pdf/src/utils/auditLogs.js
+++ utilities/works-pdf/src/utils/auditLogs.js
@@ -1,5 +1,6 @@
 const get = require("lodash.get");
 const { create_audit_details } = require("../api");
+const { logger } = require("../logger");
 
 async function createEgPaymentsExcelLog(requestData, values) {
     await createAuditLogs(requestData, "CREATE", {
@@ -13,26 +14,33 @@ async function createEgPaymentsExcelLog(requestData, values) {
 }
 
 async function createAuditLogs(requestData, operation, values){
-    let RequestInfo = requestData.RequestInfo;
-    let AuditLogs = []
-    let keyValueMap = {}
-    Object.keys(values).forEach(function(key) {
-        keyValueMap[key] = values[key]
-    });
-    let AuditLogObj = {
-        "userUUID" : RequestInfo.userInfo.uuid,
-        "module": "PA",
-        "tenantId": RequestInfo.userInfo.tenantId,
-        "transactionCode": "PA."+operation,
-        "changeDate": new Date().getTime(),
-        "entityName": "eg_payments_excel",
-        "objectId": values.paymentId,
-        keyValueMap,
-        "operationType": operation
+    try{
+        logger.info("Creating Audit Logs.")
+        let RequestInfo = requestData.RequestInfo;
+        let AuditLogs = []
+        let keyValueMap = {}
+        Object.keys(values).forEach(function(key) {
+            keyValueMap[key] = values[key]
+        });
+        let AuditLogObj = {
+            "userUUID" : RequestInfo.userInfo.uuid,
+            "module": "PA",
+            "tenantId": RequestInfo.userInfo.tenantId,
+            "transactionCode": "PA."+operation,
+            "changeDate": new Date().getTime(),
+            "entityName": "eg_payments_excel",
+            "objectId": values.paymentId,
+            keyValueMap,
+            "operationType": operation
+        }
+        AuditLogs.push(AuditLogObj);
+        let request = {RequestInfo, AuditLogs}
+        await create_audit_details(request);
+    } catch (error) {
+        logger.error(error.stack || error);
+        return null;
     }
-    AuditLogs.push(AuditLogObj);
-    let request = {RequestInfo, AuditLogs}
-    await create_audit_details(request);
+    
 }
 
 
