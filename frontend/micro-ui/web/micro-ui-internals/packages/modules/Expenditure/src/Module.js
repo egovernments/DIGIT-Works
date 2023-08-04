ece48ea29b902dcdfba73762567e157489ea181c
Nipun Arora <aroranipun1@gmail.com>
2023-08-04 11:46:27 +0530

    added rainmaker-ifms locs call in expenditure module (#1285)

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/Expenditure/src/Module.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/Expenditure/src/Module.js
@@ -25,7 +25,7 @@ const ExpenditureModule = ({ stateCode, userType, tenants }) => {
     const { path, url } = useRouteMatch();
     const language = Digit.StoreData.getCurrentLanguage();
     const tenantId = Digit.ULBService.getCurrentTenantId();
-    const moduleCode = ["Expenditure","common-masters","workflow",tenantId];
+    const moduleCode = ["Expenditure","common-masters","workflow","ifms",tenantId];
     const { isLoading, data: store } = Digit.Services.useStore({
         stateCode,
         moduleCode,
