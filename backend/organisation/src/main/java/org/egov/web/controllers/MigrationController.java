4421c6205ffcee9c371e178298ea4d5cc496a9a4
shubhang-egov <shubhang.dhanesha@egovernments.org>
2023-08-24 19:41:21 +0530

    Migration scipt for migrating previously generated users to individual table

---
+++ backend/organisation/src/main/java/org/egov/web/controllers/MigrationController.java
@@ -0,0 +1,40 @@
+package org.egov.web.controllers;
+
+
+import io.swagger.annotations.ApiParam;
+import org.egov.common.contract.request.RequestInfo;
+import org.egov.common.contract.response.ResponseInfo;
+import org.egov.util.ResponseInfoFactory;
+import org.egov.util.UserMigrationUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
+
+@Controller
+@RequestMapping("/organisation/v1")
+public class MigrationController {
+
+    @Autowired
+    private ResponseInfoFactory responseInfoFactory;
+    @Autowired
+    private UserMigrationUtil userMigrationUtil;
+
+    @RequestMapping(value = "/_migrate", method = RequestMethod.POST)
+    public ResponseEntity<ResponseInfo> orgServicesOrganisationV1CreatePOST(
+            @ApiParam(value = "", allowableValues = "application/json") @RequestHeader(value = "Content-Type", required = false) String contentType,
+            @ApiParam(value = "") @Valid @RequestBody RequestInfo requestInfo) {
+
+        userMigrationUtil.migrate(requestInfo);
+        ResponseInfo responseInfo = responseInfoFactory.createResponseInfoFromRequestInfo(requestInfo, true);
+        return new ResponseEntity<ResponseInfo>(responseInfo, HttpStatus.OK);
+
+    }
+
+}
