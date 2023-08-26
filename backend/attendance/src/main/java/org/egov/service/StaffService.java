7f91056075371edc65a2aa9953f90f2d92cb6f2d
Shailesh Pandey <shailesh.kumar@egovernments.org>
2023-08-26 11:39:54 +0530

    Commented validator

--- backend/attendance/src/main/java/org/egov/service/StaffService.java
+++ backend/attendance/src/main/java/org/egov/service/StaffService.java
@@ -79,7 +79,7 @@ public class StaffService {
 
         //validator call by passing staff request and the data from db call
         log.info("staffServiceValidator called to validate Create StaffPermission request");
-        staffServiceValidator.validateStaffPermissionOnCreate(staffPermissionRequest, staffPermissionListFromDB, attendanceRegisterListFromDB);
+        // staffServiceValidator.validateStaffPermissionOnCreate(staffPermissionRequest, staffPermissionListFromDB, attendanceRegisterListFromDB);
 
         //enrichment call by passing staff request and data from db call
         log.info("staffEnrichmentService called to enrich Create StaffPermission request");
