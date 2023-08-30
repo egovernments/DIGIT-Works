838475078b9fac27fcde0821d7348678c7608913
Tulika-eGov <tulika.kundu@egovernments.org>
2023-08-30 14:40:36 +0530

    PFM-4458 : removed debugger

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/Contracts/src/pages/employee/ViewContractDetails.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/Contracts/src/pages/employee/ViewContractDetails.js
@@ -92,7 +92,6 @@ const ViewContractDetails = () => {
             setToast({show : true, label : t("COMMON_PROJECT_NOT_FOUND"), error : true});
         }
     },[isProjectError]);
-    debugger;
 
       useEffect(() => {
         if(!data?.additionalDetails?.isTimeExtAlreadyInWorkflow && data) {
