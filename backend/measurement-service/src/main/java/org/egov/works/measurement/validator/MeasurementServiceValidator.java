271a5647b8d77b8a29976bb0e7236cd49aa0728c
Shailesh Pandey <110380977+shailesh-egov@users.noreply.github.com>
2023-09-14 14:36:08 +0530

    Added validator

---
+++ backend/measurement-service/src/main/java/org/egov/works/measurement/validator/MeasurementServiceValidator.java
@@ -0,0 +1,11 @@
+package org.egov.works.measurement.validator;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class MeasurementServiceValidator {
+
+
+}
