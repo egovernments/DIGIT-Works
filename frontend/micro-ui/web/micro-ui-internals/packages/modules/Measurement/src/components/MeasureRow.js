4a1b2491ac446bcc15533da394bbbdc30da12ee8
ansh-egov <ansh.goyal@egovernments.org>
2023-09-14 18:27:52 +0530

    created the measurement table with all the logic just need to change some calculate logic

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/Measurement/src/components/MeasureRow.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/Measurement/src/components/MeasureRow.js
@@ -1,19 +1,64 @@
-import { Card } from "@egovernments/digit-ui-react-components";
-import React, { useState } from "react";
+import { Card, TextInput } from "@egovernments/digit-ui-react-components";
+import React, { useReducer, useState } from "react";
+import { useTranslation } from "react-i18next";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
-
-
-
-const MeasureRow = () => {
+const MeasureRow = ({ value, index, state, dispatch }) => {
   const tenantId = Digit.ULBService.getCurrentTenantId();
   const { t } = useTranslation();
   const history = useHistory();
 
-
   return (
-    <Card>
-        View
-    </Card>
+    <tr key={index}>
+      <td>{value.no}</td>
+      <td>{value.type}</td>
+      <td>{value.desc}</td>
+      <td>
+        <TextInput
+          value={state.tableState[index].number} // Replace field1 with the appropriate field name.
+          onChange={(newValue) => {
+            dispatch({
+              type: "UPDATE_ROW",
+              state: { id: index + 1, value: newValue.target.value, row: value, type: "number" },
+            });
+          }}
+        />
+      </td>
+      <td>
+        <TextInput
+          value={state.tableState[index].length} // Replace field1 with the appropriate field name.
+          onChange={(newValue) => {
+            dispatch({
+              type: "UPDATE_ROW",
+              state: { id: index + 1, value: newValue.target.value, row: value, type: "length" },
+            });
+          }}
+        />
+      </td>
+      <td>
+        <TextInput
+          value={state.tableState[index].width} // Replace field1 with the appropriate field name.
+          onChange={(newValue) => {
+            dispatch({
+              type: "UPDATE_ROW",
+              state: { id: index + 1, value: newValue.target.value, row: value, type: "width" },
+            });
+          }}
+        />
+      </td>
+      <td>
+        <TextInput
+          value={state.tableState[index].depth} // Replace field1 with the appropriate field name.
+          onChange={(newValue) => {
+            dispatch({
+              type: "UPDATE_ROW",
+              state: { id: index + 1, value: newValue.target.value, row: value, type: "depth" },
+            });
+          }}
+        />
+      </td>
+      <td>{value.quantity}</td>
+    </tr>
   );
 };
 
