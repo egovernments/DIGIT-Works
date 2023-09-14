89f324d68e1986e19c1ce9c991a77dbd8f589291
prerna-egov <prerna.kumari@egovernments.org>
2023-09-14 18:29:53 +0530

    added base view measurement page

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/Measurement/src/pages/employee/ViewMeasurement.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/Measurement/src/pages/employee/ViewMeasurement.js
@@ -1,19 +1,46 @@
 import { Card } from "@egovernments/digit-ui-react-components";
 import React, { useState } from "react";
-
-
-
+import { useTranslation } from "react-i18next";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import ApplicationDetails from "../../../../templates/ApplicationDetails";
 
 const ViewMeasurement = () => {
-  const tenantId = Digit.ULBService.getCurrentTenantId();
   const { t } = useTranslation();
   const history = useHistory();
+  const tenantId = Digit.ULBService.getCurrentTenantId();
+  console.log(tenantId);
+  const contractNumber = { contractNumber: "WO/2023-24/000783" };
+  const subEstimateNumber = "EP/2022-23/09/000094/000070";
 
+  const payload = {
+    contractNumber: "WO/2023-24/000783",
+    tenantId: tenantId,
+  };
+  let { isLoading, isError, data: applicationDetails, error } = Digit.Hooks.measurement.useViewMeasurement(
+    tenantId,
+    payload
+  );
+
+  console.log(applicationDetails);
 
   return (
-    <Card>
+    <React.Fragment>
+      <Card>
         View
-    </Card>
+        <ApplicationDetails
+          applicationDetails={applicationDetails?.applicationDetails}
+          isLoading={isLoading}
+          applicationData={applicationDetails?.applicationData}
+          moduleCode="contracts"
+          isDataLoading={false}
+          workflowDetails={applicationDetails?.workflowDetails}
+          // showTimeLine={"fgv"}
+          mutate={() => {}}
+          tenantId={tenantId}
+          showT
+        />
+      </Card>
+    </React.Fragment>
   );
 };
 
