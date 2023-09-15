883f98acaa98b5259100528af64983e4b1ce113a
rachana-egov <137176770+rachana-egov@users.noreply.github.com>
2023-09-15 18:44:45 +0530

    Update CreateMeasurement.js

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/Measurement/src/pages/employee/CreateMeasurement.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/Measurement/src/pages/employee/CreateMeasurement.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from "react";
 import { useTranslation } from "react-i18next";
 import { useHistory } from "react-router-dom";
 import { CreateConfig } from "../../configs/MeasurementCreateConfig";
+import _ from "lodash";
 
 
 
@@ -10,6 +11,11 @@ const CreateMeasurement = () => {
   const tenantId = Digit.ULBService.getCurrentTenantId();
   const { t } = useTranslation();
   const history = useHistory();
+  const MeasurementSession = Digit.Hooks.useSessionStorage("MEASUREMENT_CREATE", {})
+
+  const [sessionFormData, setSessionFormData, clearSessionFormData] = MeasurementSession;
+
+  const [createState, setState] = useState(sessionFormData || {});
 
 
   // State to hold estimate data
@@ -39,6 +45,12 @@ const CreateMeasurement = () => {
 
 
 
+  useEffect(() => {
+    if (!_.isEqual(sessionFormData, createState)) {
+      setSessionFormData({ ...createState });
+    }
+  }, [createState]);
+
   //fetching estimate data
   const { isLoading: isEstimateLoading, data: estimate, isError: isEstimateError } = Digit.Hooks.estimates.useEstimateSearch({
     tenantId,
@@ -47,6 +59,27 @@ const CreateMeasurement = () => {
       enabled: isEstimateEnabled,
     }
   })
+
+  useEffect(() => {
+    if (estimate && !createState?.SOR) {
+      // get estimateDetails from the estimate data and get SOR and NONSOR data seperate
+      const estimateDetails = estimate?.estimateDetails || [];
+      // console.log(estimateDetails, "eeeeeeeeeeee")
+      const sorCategoryArray = [];
+      const nonSorCategoryArray = [];
+
+      estimateDetails.reduce((_, currentItem) => {
+        if (currentItem.category === 'SOR') {
+          sorCategoryArray.push(currentItem);
+        } else if (currentItem.category === 'NONSOR') {
+          nonSorCategoryArray.push(currentItem);
+        }
+      }, null);
+      setState({ SOR: sorCategoryArray, NONSOR: nonSorCategoryArray, isLocalStateUpadted: true });
+    }
+  }, [estimate]);
+  ////// In progress
+
 
 
   // Define the request criteria for creating a measurement
@@ -72,41 +105,42 @@ const CreateMeasurement = () => {
   };
 
 
+  const onFormValueChange = (setValue, formData, formState, reset, setError, clearErrors, trigger, getValues) => {
+    // setSessionFormData({ ...sessionFormData, ...formData });
 
+    if (!_.isEqual(formData, createState)) {
+      setState({ ...formData })
+    }
+    console.log(formData, "formData")
+    console.log(createState, "createStateData")
+    // console.log(sessionFormData, "sessionFormData")
+  }
 
   if (isContractLoading || isEstimateLoading) {
     return <Loader />
   }
 
-  const estimateDetails = estimate?.estimateDetails || [];
-  // console.log(estimateDetails, "eeeeeeeeeeee")
-  const sorCategoryArray = [];
-  const nonSorCategoryArray = [];
 
-  estimateDetails.reduce((_, currentItem) => {
-    if (currentItem.category === 'SOR') {
-      sorCategoryArray.push(currentItem);
-    } else if (currentItem.category === 'NONSOR') {
-      nonSorCategoryArray.push(currentItem);
-    }
-  }, null);
-
+  console.log(createState,
+    "createStatecreateState");
 
   return (
     <FormComposerV2
-      heading={t("Measurement Book")}
-      label={t("Submit Bar")}
-      description={"Description"}
-      text={"Sample Text if required"}
+      // heading={t("Measurement Book")}
+      // label={t("Submit Bar")}
+      // description={"Description"}
+      // text={"Sample Text if required"}
       config={CreateConfig({ defaultValues: contract }).CreateConfig[0]?.form?.map((config) => {
+        console.log(config, "cccccccccccccc")
         return {
           ...config,
           body: config.body.filter((a) => !a.hideInEmployee),
         };
       })}
-      defaultValues={{ SOR: sorCategoryArray, NONSOR: nonSorCategoryArray }}
+      defaultValues={{ ...createState }}
       onSubmit={onSubmit}
       fieldStyle={{ marginRight: 0 }}
+      onFormValueChange={onFormValueChange}
     />
   );
 };
