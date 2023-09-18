70ff6cb60cbad0043801508f45848a4a5d3fb02b
Bhavya-egov <bhavya.mangal@egovernments.org>
2023-09-18 16:47:28 +0530

    feat: added consumer to listen to save topic and push to enrich topic after enriching

--- backend/measurement-service/src/main/java/org/egov/works/measurement/web/controllers/MeasurementApiController.java
+++ backend/measurement-service/src/main/java/org/egov/works/measurement/web/controllers/MeasurementApiController.java
@@ -81,11 +81,11 @@ public class MeasurementApiController {
                 for (String referenceId : referenceIds) {
                     // Use each referenceId as needed for your search
                     System.out.println(referenceId);
-                    List<Measurement> measurements = measurementService.searchMeasurements(criteria);
+//                    List<Measurement> measurements = measurementService.searchMeasurements(criteria);
 
                     // Create a MeasurementResponse using the search results
 
-                    response.setMeasurements(measurements);
+//                    response.setMeasurements(measurements);
 
                     // Return a successful response for each referenceId
                     return new ResponseEntity<MeasurementResponse>(response, HttpStatus.OK);
