4e74d48cdaf2eecc730fad260ca0675bb1d8ec8e
prerna-egov <137176760+prerna-egov@users.noreply.github.com>
2023-09-21 15:58:17 +0530

    updated view screen according to figma (#1394)
    * added fields according to updated figma
    
    * rendering multiple cards
    
    * added localized codes
    
    * Revert "added localized codes"
    
    This reverts commit 9fda248f73d880aaa5c991202f80fa82ff82f5b4.
    
    * Revert "added fields according to updated figma"
    
    This reverts commit fe6beed7aa646adb6d910efc1332d66c074a58ec.
    
    * added localized codes

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/Measurement/src/pages/employee/ViewMeasurement.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/Measurement/src/pages/employee/ViewMeasurement.js
@@ -1,4 +1,4 @@
-import { Card } from "@egovernments/digit-ui-react-components";
+import { Header, Card, Loader } from "@egovernments/digit-ui-react-components";
 import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
@@ -10,37 +10,48 @@ const ViewMeasurement = () => {
   const history = useHistory();
   const tenantId = Digit.ULBService.getCurrentTenantId();
   console.log(tenantId);
-  const contractNumber = { contractNumber: "WO/2023-24/000783" };
+  const contractNumber = { contractNumber: "WO/2023-24/000351" };
   const subEstimateNumber = "EP/2022-23/09/000094/000070";
+  const searchparams = new URLSearchParams(location.search);
+  const workOrderNumber = searchparams.get("workOrderNumber")
 
   const payload = {
-    contractNumber: "WO/2023-24/000783",
+    contractNumber: workOrderNumber,
     tenantId: tenantId,
   };
-  let { isLoading, isError, data: applicationDetails, error } = Digit.Hooks.measurement.useViewMeasurement(
-    tenantId,
-    payload
-  );
+  let { isLoading, isError, data: applicationDetails, error } = Digit.Hooks.measurement.useViewMeasurement(tenantId, payload);
 
-  console.log(applicationDetails);
+  const projectDetails = {"applicationDetails" : [applicationDetails?.applicationDetails?.applicationDetails[0]]};
+  const documentDetails = {"applicationDetails" : [applicationDetails?.applicationDetails?.applicationDetails[1]]};
+
+
 
   return (
     <React.Fragment>
-      <Card>
-        View
-        <ApplicationDetails
-          applicationDetails={applicationDetails?.applicationDetails}
-          isLoading={isLoading}
-          applicationData={applicationDetails?.applicationData}
-          moduleCode="contracts"
-          isDataLoading={false}
-          workflowDetails={applicationDetails?.workflowDetails}
-          mutate={() => {}}
-          tenantId={tenantId}
-          showT
-        />
-         <MeasurementHistory contractNumber={"WO/2023-24/000784"}/>
-      </Card>
+      <Header className="works-header-view">{t("MB_VIEW_MEASUREMENT_BOOK")}</Header>
+      <ApplicationDetails
+        applicationDetails={projectDetails}
+        isLoading={isLoading}
+        applicationData={applicationDetails?.applicationData}
+        moduleCode="contracts"
+        isDataLoading={false}
+        workflowDetails={applicationDetails?.workflowDetails}
+        mutate={() => {}}
+        tenantId={tenantId}
+        showTimeLine = {false}
+      />
+      <MeasurementHistory contractNumber={"WO/2023-24/000784"} />
+      <ApplicationDetails
+        applicationDetails={documentDetails}
+        isLoading={isLoading}
+        applicationData={applicationDetails?.applicationData}
+        moduleCode="contracts"
+        isDataLoading={false}
+        workflowDetails={applicationDetails?.workflowDetails}
+        mutate={() => {}}
+        tenantId={tenantId}
+        showTimeLine = {false}
+      />
     </React.Fragment>
   );
 };
