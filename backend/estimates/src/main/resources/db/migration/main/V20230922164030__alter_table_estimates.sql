980562165849f5d6da2fee95a1042b89317e5c33
Your Name <youremail@domain.com>
2023-09-22 18:10:06 +0530

    added new features in create api

---
+++ backend/estimates/src/main/resources/db/migration/main/V20230922164030__alter_table_estimates.sql
@@ -0,0 +1,7 @@
+ALTER TABLE eg_wms_estimate_detail ADD COLUMN length NUMERIC DEFAULT NULL;
+
+ALTER TABLE eg_wms_estimate_detail ADD COLUMN width NUMERIC DEFAULT NULL;
+
+ALTER TABLE eg_wms_estimate_detail ADD COLUMN height NUMERIC DEFAULT NULL;
+
+ALTER TABLE eg_wms_estimate_detail ADD COLUMN is_deduction boolean DEFAULT FALSE;
