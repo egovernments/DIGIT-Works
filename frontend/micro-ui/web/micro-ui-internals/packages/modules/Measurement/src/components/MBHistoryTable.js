0d780ba68090944d50ff794e7ce1867cb0923c10
prerna-egov <prerna.kumari@egovernments.org>
2023-09-23 20:19:22 +0530

    modified mbhistory table

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/Measurement/src/components/MBHistoryTable.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/Measurement/src/components/MBHistoryTable.js
@@ -1,5 +1,5 @@
 
-import { Card, Header, Button } from "@egovernments/digit-ui-react-components";
+import { Card, Header, Button, Loader } from "@egovernments/digit-ui-react-components";
 import React from "react";
 import { useTranslation } from "react-i18next";
 import { useState } from "react";
@@ -14,10 +14,9 @@ const CustomCollapsibleTable = ({ children, isTableCollapsed }) => {
 
 
 
-const MeasurementHistory = ({ contractNumber }) => {
+const MeasurementHistory = ({ contractNumber, measurementNumber }) => {
   const { t } = useTranslation();
   const tenantId = Digit.ULBService.getCurrentTenantId();
-
 
   const [isTableCollapsed, setIsTableCollapsed] = useState(false);
 
@@ -31,8 +30,8 @@ const MeasurementHistory = ({ contractNumber }) => {
     body: {
       criteria: {
         tenantId : tenantId,
-        referenceId: [contractNumber],
-        ids: ["70380648-45c2-4407-bf91-27ede3c481e5"],
+        referenceId: [contractNumber]
+        // ids: ["70380648-45c2-4407-bf91-27ede3c481e5"],
       },
       pagination: {
         "limit": 10,
@@ -43,45 +42,7 @@ const MeasurementHistory = ({ contractNumber }) => {
     },
   };
 
-  var dummyResponse = {
-    responseInfo: null,
-    measurements: [
-      {
-        id: "1a39840f-6b30-4d19-9d1b-e5c88bd88d55",
-        tenantId: "tenant1",
-        measurementNumber: "mbNumber1",
-        physicalRefNumber: "phyRef1",
-        referenceId: "6470d652-7684-44dd-a5d4-d1cc14e451b2",
-        entryDate: 1631606400,
-        measures: [
-          {
-            id: null,
-            referenceId: "6470d652-7684-44dd-a5d4-d1cc14e451b2",
-            targetId: null,
-            length: 10.5,
-            breadth: 5.2,
-            height: 3.0,
-            numItems: 100,
-            currentValue: null,
-            cumulativeValue: null,
-            isActive: null,
-            comments: null,
-            documents: null,
-            auditDetails: null,
-            additionalDetails: null,
-          },
-        ],
-        isActive: true,
-        auditDetails: {
-          createdBy: "user1",
-          lastModifiedBy: "user1",
-          createdTime: 1631606400,
-          lastModifiedTime: 1631606400,
-        },
-        additionalDetails: '{"key": "value1"}',
-      },
-    ],
-  };
+
   const { isLoading, data } = Digit.Hooks.useCustomAPIHook(requestCriteria);
   const columns = [
     { label: t("MB_SNO"), key: "sno" },
@@ -93,51 +54,61 @@ const MeasurementHistory = ({ contractNumber }) => {
     { label: t("MB_ONLY_AMOUNT"), key: "amount" },
   ];
 
-  const rows = [
-    {
-      sno: 1,
-      mbref: dummyResponse?.measurements[0]?.measurementNumber,
-      musterid: "M123",
-      mbDate: Digit.Utils.pt.convertDateToEpoch(dummyResponse?.measurements[0]?.entryDate),
-      period: "September",
-      status: "Paid",
-      amount: 1000,
-    },
-  ];
+  const filteredArray = data?.measurements.filter(item => item.measurementNumber !== measurementNumber);
+
+  const filteredRows = filteredArray?.map((item, index) =>({
+    sno :index + 1,
+    mbref : item?.measurementNumber,
+    musterid : "M1234",
+    mbDate : item?.entryDate,
+    period : "test",
+    status : "Submitted",
+    amount : 1000
+  }))
+
+  if (isLoading) {
+    return <Loader></Loader>
+  }
 
   return (
-
-    <Card className = "override-card">
+    <Card className="override-card">
       <Header className="works-header-view">{t("MB_HISTORY")}</Header>
-      <CustomCollapsibleTable
-        isTableCollapsed={isTableCollapsed}
-      >
-
-      <table className="table reports-table sub-work-table">
-        <thead>
-          <tr>
-            {columns.map((column, index) => (
-              <th key={index}>{column.label}</th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {rows.map((row, rowIndex) => (
-            <tr key={rowIndex}>
-              {columns.map((column, columnIndex) => (
-                <td key={columnIndex}>{row[column.key]}</td>
+      <CustomCollapsibleTable isTableCollapsed={isTableCollapsed}>
+        <table className="table reports-table sub-work-table">
+          <thead>
+            <tr>
+              {columns.map((column, index) => (
+                <th key={index}>{column.label}</th>
               ))}
             </tr>
-          ))}
-        </tbody>
-      </table>
-
+          </thead>
+          <tbody>
+            {filteredRows.map((row, rowIndex) => (
+              <tr key={rowIndex}>
+                {columns.map((column, columnIndex) => (
+                  <td key={columnIndex}>
+                    {column.key === "mbref" ? (
+                      <a
+                        href={`${window.location.origin}${window.location.pathname}?tenantId=${tenantId}&workOrderNumber=${contractNumber}&mbNumber=${row.mbref}`}
+                        style={{ color: "#f37f12" }}
+                      >
+                        {row[column.key]}
+                      </a>
+                    ) : (
+                      row[column.key]
+                    )}
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </CustomCollapsibleTable>
-      <Button 
-      className={"collapse-button"}
-      onButtonClick={toggleTableCollapse}
-      label={isTableCollapsed ? 'Show MB History' : 'Hide MB History'}>
-      </Button>
+      <Button
+        className={"collapse-button"}
+        onButtonClick={toggleTableCollapse}
+        label={isTableCollapsed ? "Show MB History" : "Hide MB History"}
+      ></Button>
     </Card>
   );
 };
