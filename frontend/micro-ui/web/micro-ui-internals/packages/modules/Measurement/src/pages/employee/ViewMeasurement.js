e48ddffaed83204e3e52ccacb3b272cfa119b84d
prerna-egov <prerna.kumari@egovernments.org>
2023-09-23 20:18:41 +0530

    added worklow in view screen

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/Measurement/src/pages/employee/ViewMeasurement.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/Measurement/src/pages/employee/ViewMeasurement.js
@@ -1,5 +1,4 @@
-
-import { Header, Card, Loader, Button } from "@egovernments/digit-ui-react-components";
+import { Header, Card, Loader, Button, WorkflowActions } from "@egovernments/digit-ui-react-components";
 
 import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
@@ -11,45 +10,56 @@ import MeasureTable from "../../components/MeasureTable";
 const ViewMeasurement = () => {
   const { t } = useTranslation();
   const history = useHistory();
+  const businessService = Digit?.Customizations?.["commonUiConfig"]?.getBusinessService("measurement");
   const tenantId = Digit.ULBService.getCurrentTenantId();
   const searchparams = new URLSearchParams(location.search);
   const workOrderNumber = searchparams.get("workOrderNumber");
-  const mbNumber = searchparams.get("mbNumber")
+  const mbNumber = searchparams.get("mbNumber");
+  console.log(businessService, "bbbbbbbbb");
+
+  const pagination = {
+    pagination: {
+      limit: 10,
+      offSet: 0,
+      sortBy: "string",
+      order: "DESC",
+    },
+  };
 
   const criteria = {
-    "criteria" : {
-      tenantId : tenantId,
-      referenceId : [workOrderNumber],
-      measurementNumber : mbNumber
-    }
-  }
+    criteria: {
+      tenantId: tenantId,
+      referenceId: [workOrderNumber],
+      measurementNumber: mbNumber,
+    },
+    ...pagination,
+  };
 
   let { isLoading, isError, data: applicationDetails, error } = Digit.Hooks.measurement.useViewMeasurement(tenantId, criteria);
 
-  const projectDetails = {"applicationDetails" : [applicationDetails?.applicationDetails?.applicationDetails[0]]};
-  const imageDetails = {"applicationDetails" : [applicationDetails?.applicationDetails?.applicationDetails[2]]};
+  const projectDetails = { applicationDetails: [applicationDetails?.applicationDetails?.applicationDetails[0]] };
+  const imageDetails = { applicationDetails: [applicationDetails?.applicationDetails?.applicationDetails[2]] };
 
+  const sorEstimates = applicationDetails?.applicationData?.estimate[0]?.estimateDetails.filter((item) => item.category === "SOR");
+  const nonSorEstimates = applicationDetails?.applicationData?.estimate[0]?.estimateDetails.filter((item) => item.category === "NONSOR");
 
-  const sorEstimates = applicationDetails?.applicationData?.estimate[0]?.estimateDetails.filter(item => item.category === 'SOR');
-  const nonSorEstimates = applicationDetails?.applicationData?.estimate[0]?.estimateDetails.filter(item => item.category === 'NONSOR');
-
-  const measures = applicationDetails?.applicationData?.measurements[0].measures
-  console.log(measures, "measure")
+  const measures = applicationDetails?.applicationData?.measurements[0].measures;
+  console.log(measures, "measure");
 
   console.log(sorEstimates, "estimate");
 
   const tableData = {
-    "data" : {
-      "SOR" : sorEstimates,
-      "NONSOR" : nonSorEstimates
+    data: {
+      SOR: sorEstimates,
+      NONSOR: nonSorEstimates,
     },
-    "config" : {
-      "key" : "SOR"
-    }
-  }
+    config: {
+      key: "SOR",
+    },
+  };
 
   if (isLoading) {
-    return <Loader/>
+    return <Loader />;
   }
 
   return (
@@ -59,38 +69,55 @@ const ViewMeasurement = () => {
         applicationDetails={projectDetails}
         isLoading={isLoading}
         applicationData={applicationDetails?.applicationData}
-        moduleCode="contracts"
+        moduleCode="measurements"
         workflowDetails={applicationDetails?.workflowDetails}
         mutate={() => {}}
         tenantId={tenantId}
         showTimeLine={false}
       />
-      <MeasurementHistory contractNumber={workOrderNumber} />
-      <Card className="override-card view-utilization-card">
-        <Button 
-        className={"view-utilization-btn"} 
-        label={"View Utilisation Statement"}>
-        </Button>
-        <Card>Total amount</Card>
-      </Card>
-      <MeasureTable {...tableData} isView = {true} measureData = {measures}/>
+      <MeasurementHistory contractNumber={workOrderNumber} measurementNumber={mbNumber} />
+      <MeasureTable {...tableData} isView={true} measureData={measures} />
+      <MeasureTable
+        {...tableData}
+        config={{
+          key: "NONSOR",
+        }}
+        isView={true}
+        measureData={measures}
+      />
       <ApplicationDetails
         applicationDetails={imageDetails}
         isLoading={isLoading}
         applicationData={applicationDetails?.applicationData}
-        moduleCode="contracts"
+        moduleCode="measurements"
         workflowDetails={applicationDetails?.workflowDetails}
         mutate={() => {}}
         tenantId={tenantId}
         showTimeLine={false}
       />
       <ApplicationDetails
+        // applicationDetails={applicationDetails?.applicationDetails}
+        isLoading={isLoading}
         applicationData={applicationDetails?.applicationData}
-        moduleCode="contracts"
-        workflowDetails={applicationDetails?.workflowDetails}
-        mutate={() => {}}
+        moduleCode="measurements"
+        // workflowDetails={applicationDetails?.workflowDetails}
+        // mutate={() => {}}
+        timelineStatusPrefix={`WF_${businessService}_`}
+        businessService={businessService}
         tenantId={tenantId}
         showTimeLine={true}
+        applicationNo={mbNumber}
+      />
+      <WorkflowActions
+        forcedActionPrefix={`WF_${businessService}_ACTION`}
+        businessService={businessService}
+        applicationNo={mbNumber}
+        tenantId={tenantId}
+        applicationDetails={applicationDetails?.applicationData}
+        url={Digit.Utils.Urls.measurement.update}
+        // setStateChanged={setStateChanged}
+        moduleCode="measurements"
+        // editApplicationNumber={editApplicationNumber}
       />
     </React.Fragment>
   );
