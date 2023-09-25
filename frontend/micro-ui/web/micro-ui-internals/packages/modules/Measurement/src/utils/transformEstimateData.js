b23ee371f2a55b4ed7d1685b41fe59c5dd6ec567
rachana-egov <137176770+rachana-egov@users.noreply.github.com>
2023-09-25 11:19:14 +0530

    Update transformEstimateData.js

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/Measurement/src/utils/transformEstimateData.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/Measurement/src/utils/transformEstimateData.js
@@ -4,68 +4,6 @@ output is array of object of type which is passed
 
 */
 
-const measures = [
-    {
-        "id": "91925a44-ee04-4599-97de-6289674af7b9",
-        "referenceId": "4e6bf7f2-8b44-4aff-8351-6a023244c44c",
-        "targetId": "cd0631db-8718-4aa8-80c1-9ddc71f61dde",
-        "length": 1,
-        "breadth": 1,
-        "height": 2,
-        "numItems": 2,
-        "currentValue": 4,
-        "cumulativeValue": 146,
-        "isActive": true,
-        "comments": null,
-        "auditDetails": {
-            "createdBy": "40e3b45a-0f64-4e8c-8768-aab82c095b2d",
-            "lastModifiedBy": null,
-            "createdTime": 1695563037793,
-            "lastModifiedTime": 1695563037793
-        },
-        "additionalDetails": null
-    },
-    {
-        "id": "04f94cc2-eb9c-412a-b8d7-52e07380c8e0",
-        "referenceId": "4e6bf7f2-8b44-4aff-8351-6a023244c44c",
-        "targetId": "30b604ca-90c1-4999-98bc-69a29599af5c",
-        "length": 2,
-        "breadth": 1,
-        "height": 1,
-        "numItems": 3,
-        "currentValue": 6,
-        "cumulativeValue": 82,
-        "isActive": true,
-        "comments": null,
-        "auditDetails": {
-            "createdBy": "40e3b45a-0f64-4e8c-8768-aab82c095b2d",
-            "lastModifiedBy": null,
-            "createdTime": 1695563037793,
-            "lastModifiedTime": 1695563037793
-        },
-        "additionalDetails": null
-    },
-    {
-        "id": "9d7735b1-c590-42e5-8d0b-92392eeeb71f",
-        "referenceId": "4e6bf7f2-8b44-4aff-8351-6a023244c44c",
-        "targetId": "42bdc619-251b-402a-864c-edc0a9f37306",
-        "length": 2,
-        "breadth": 1,
-        "height": 2,
-        "numItems": 1,
-        "currentValue": 4,
-        "cumulativeValue": 2048,
-        "isActive": true,
-        "comments": null,
-        "auditDetails": {
-            "createdBy": "40e3b45a-0f64-4e8c-8768-aab82c095b2d",
-            "lastModifiedBy": null,
-            "createdTime": 1695563037793,
-            "lastModifiedTime": 1695563037793
-        },
-        "additionalDetails": null
-    }
-]
 
 export const transformEstimateData = (lineItems, contract, type, measurement = {}) => {
     const transformedContract = transformContractObject(contract)
