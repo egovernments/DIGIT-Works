daa46d74c479997740f2c743ee4866cf2b9a09f9
Jagankumar <53823168+jagankumar-egov@users.noreply.github.com>
2023-09-25 13:44:34 +0530

    Update index.ts

--- utilities/mukta-services/src/server/config/index.ts
+++ utilities/mukta-services/src/server/config/index.ts
@@ -33,7 +33,7 @@ const config = {
   host: {
     serverHost: HOST,
     localization: process.env.EGOV_LOCALIZATION_HOST || HOST,
-    mdms: process.env.EGOV_MDMS_HOST || "http://localhost:8094/",
+    mdms: process.env.EGOV_MDMS_HOST|| HOST || "http://localhost:8094/",
     pdf: process.env.EGOV_PDF_HOST || HOST || "http://localhost:8087/",
     user: process.env.EGOV_USER_HOST || HOST || "http://localhost:8089/",
     workflow:
