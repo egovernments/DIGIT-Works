13840ab2e6c06bb408da2133e258fef0791c4895
ashish-egov <ashish.tiwari@egovernments.org>
2023-09-27 14:27:22 +0530

    Refactor : refactored configuration and application properties

--- backend/measurement-service/src/main/java/org/egov/works/measurement/web/models/Document.java
+++ backend/measurement-service/src/main/java/org/egov/works/measurement/web/models/Document.java
@@ -1,13 +1,10 @@
 package org.egov.works.measurement.web.models;
 
-import java.util.Objects;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonCreator;
 import io.swagger.v3.oas.annotations.media.Schema;
 import org.springframework.validation.annotation.Validated;
 
-import javax.validation.Valid;
 import javax.validation.constraints.*;
 
 import lombok.AllArgsConstructor;
