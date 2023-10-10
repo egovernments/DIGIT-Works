aced78b2a96f60dea8ba9dac06860f8d85edc032
manastanmay-eGov <manastanmay@egovernments.org>
2023-10-10 18:03:18 +0530

    Checking the cacert path

--- reference-adapters/ifms-adapter/src/main/java/org/egov/Main.java
+++ reference-adapters/ifms-adapter/src/main/java/org/egov/Main.java
@@ -1,13 +1,23 @@
 package org.egov;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class Main {
 
     public static void main(String args[]) {
         SpringApplication.run(Main.class, args);
+
+        String cacertsPath = System.getProperty("javax.net.ssl.trustStore");
+        if (cacertsPath == null) {
+            // If "javax.net.ssl.trustStore" property is not set, try to find the default cacerts file
+            cacertsPath = System.getProperty("java.home") + "/lib/security/cacerts";
+        }
+        log.info("JVM cacerts is located at: " + cacertsPath);
+        System.out.println("JVM cacerts is located at: " + cacertsPath);
     }
 
 }
