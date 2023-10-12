43aa8c5212eafc07a54f7aec3a0d3e13567e4693
Bhavya-egov <bhavya.mangal@egovernments.org>
2023-10-12 11:36:54 +0530

    changes in copy orgRequest

--- backend/organisation/src/main/java/org/egov/service/OrganisationService.java
+++ backend/organisation/src/main/java/org/egov/service/OrganisationService.java
@@ -58,39 +58,95 @@ public class OrganisationService {
         organisationEnrichmentService.enrichCreateOrgRegistryWithoutWorkFlow(orgRequest);
         //userService.createUser(orgRequest);
         individualService.createIndividual(orgRequest);
-        OrgRequest clonedOrgRequest = new OrgRequest();
-
-        clonedOrgRequest.setOrganisations(orgRequest.getOrganisations());
-        clonedOrgRequest.setRequestInfo(orgRequest.getRequestInfo());
-
-        List<Organisation> organisationList = orgRequest.getOrganisations();
-        List<Organisation> clonedOrganisations = new ArrayList<>();
-        for(Organisation organisation: organisationList){
-            Organisation clonedOrg = new Organisation();
-            List<ContactDetails> contactDetailsList = organisation.getContactDetails();
-            List<ContactDetails> CopyContactDetails = new ArrayList<>();
-            for(ContactDetails originalContact: contactDetailsList){
-                ContactDetails clonedContact = new ContactDetails();
-                clonedContact.setContactName(originalContact.getContactName());
-                clonedContact.setContactMobileNumber(originalContact.getContactMobileNumber());
-                clonedContact.setId(originalContact.getId());
-                clonedContact.setContactEmail(originalContact.getContactEmail());
-
-                CopyContactDetails.add(clonedContact);
-            }
-
-            clonedOrganisations.add(clonedOrg);
-            clonedOrgRequest.getOrganisations().get(0).setContactDetails(CopyContactDetails);
-        }
-
-        clonedOrgRequest.setOrganisations(clonedOrganisations);
+//        OrgRequest clonedOrgRequest = new OrgRequest();
+//
+//        clonedOrgRequest.setOrganisations(orgRequest.getOrganisations());
+//        clonedOrgRequest.setRequestInfo(orgRequest.getRequestInfo());
+//
+//        List<Organisation> organisationList = orgRequest.getOrganisations();
+//        List<Organisation> clonedOrganisations = new ArrayList<>();
+//        for(Organisation organisation: organisationList){
+//            Organisation clonedOrg = new Organisation();
+//            List<ContactDetails> contactDetailsList = organisation.getContactDetails();
+//            List<ContactDetails> CopyContactDetails = new ArrayList<>();
+//            for(ContactDetails originalContact: contactDetailsList){
+//                ContactDetails clonedContact = new ContactDetails();
+//                clonedContact.setContactName(originalContact.getContactName());
+//                clonedContact.setContactMobileNumber(originalContact.getContactMobileNumber());
+//                clonedContact.setId(originalContact.getId());
+//                clonedContact.setContactEmail(originalContact.getContactEmail());
+//
+//                CopyContactDetails.add(clonedContact);
+//            }
+//            clonedOrg.setContactDetails(CopyContactDetails);
+//            clonedOrganisations.add(clonedOrg);
+//            clonedOrgRequest.getOrganisations().get(0).setContactDetails(CopyContactDetails);
+//        }
+//
+//        clonedOrgRequest.setOrganisations(clonedOrganisations);
 
 
 //        OrgRequest orgRequest1 = new OrgRequest();
 //        orgRequest1.setOrganisations(orgRequest.getOrganisations());
 //        orgRequest1.setRequestInfo(orgRequest.getRequestInfo());
 
-//        OrgRequest orgRequest1 = OrgRequest.builder().organisations(orgRequest.getOrganisations()).requestInfo(orgRequest.getRequestInfo()).build();
+            // Create a new OrgRequest for the deep copy
+            OrgRequest clonedOrgRequest = new OrgRequest();
+
+            // Set the RequestInfo
+            clonedOrgRequest.setRequestInfo(orgRequest.getRequestInfo());
+
+            // Create a new list for cloned Organisations
+            List<Organisation> clonedOrganisations = new ArrayList<>();
+
+            // Iterate through the Organisations in orgRequest
+            for (Organisation originalOrg : orgRequest.getOrganisations()) {
+                // Create a new Organisation for the deep copy
+                Organisation clonedOrg = new Organisation();
+
+                // Copy primitive properties from the original Organisation
+                clonedOrg.setTenantId(originalOrg.getTenantId());
+                clonedOrg.setName(originalOrg.getName());
+                clonedOrg.setApplicationStatus(originalOrg.getApplicationStatus());
+                clonedOrg.setDateOfIncorporation(originalOrg.getDateOfIncorporation());
+
+                // Create a new list for cloned ContactDetails
+                List<ContactDetails> clonedContactDetails = new ArrayList<>();
+
+                // Iterate through the ContactDetails in the original Organisation
+                for (ContactDetails originalContact : originalOrg.getContactDetails()) {
+                    // Create a new ContactDetails for the deep copy
+                    ContactDetails clonedContact = new ContactDetails();
+
+                    // Copy properties from the original ContactDetails
+                    clonedContact.setContactName(originalContact.getContactName());
+                    clonedContact.setContactMobileNumber(originalContact.getContactMobileNumber());
+                    clonedContact.setContactEmail(originalContact.getContactEmail());
+                    clonedContact.setId(originalContact.getId());
+
+                    // Add the cloned ContactDetails to the list
+                    clonedContactDetails.add(clonedContact);
+                }
+
+                // Set the cloned ContactDetails list in the cloned Organisation
+                clonedOrg.setContactDetails(clonedContactDetails);
+
+                // Copy the remaining arrays from the original Organisation without deep copying
+                clonedOrg.setOrgAddress(originalOrg.getOrgAddress());
+                clonedOrg.setAdditionalDetails(originalOrg.getAdditionalDetails());
+                clonedOrg.setFunctions(originalOrg.getFunctions());
+                clonedOrg.setIdentifiers(originalOrg.getIdentifiers());
+
+                // Add the cloned Organisation to the list of cloned Organisations
+                clonedOrganisations.add(clonedOrg);
+            }
+
+            // Set the list of cloned Organisations in the cloned OrgRequest
+            clonedOrgRequest.setOrganisations(clonedOrganisations);
+
+
+
+
         encryptionService.encryptDetails(clonedOrgRequest,ORGANISATION_ENCRYPT_KEY);
         producer.push(configuration.getOrgKafkaCreateTopic(), clonedOrgRequest);
         try {
