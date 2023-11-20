85212df2312e20d8bb5fae420a488e966862f513
Tulika-eGov <tulika.kundu@egovernments.org>
2023-11-20 14:22:15 +0530

    MB create fix

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/Measurement/src/utils/transformEstimateData.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/Measurement/src/utils/transformEstimateData.js
@@ -19,30 +19,30 @@ export const transformEstimateData = (lineItems, contract, type, measurement = {
       }
       return acc;
     }, {});
-  const transformMeasurementData = isMeasurement ? transformMeasureObject(measurement) : transformMeasureObject(lastMeasuredObject);
+  const transformMeasurementData = isMeasurement ||  lastMeasuredObject ? transformMeasureObject(measurement) : transformMeasureObject(lastMeasuredObject);
   return Object.keys(transformedEstimateObject).map((key, index) => {
     const measures = transformedEstimateObject[key].map((estimate, index) =>{
-     const measuredObject= isMeasurement ? transformMeasurementData?.lineItemsObject[transformedContract?.lineItemsObject[estimate.id]?.contractLineItemId]:{};
+     const measuredObject= transformMeasurementData?.lineItemsObject ? transformMeasurementData?.lineItemsObject[transformedContract?.lineItemsObject[estimate?.id]?.contractLineItemId] : {};
     return ({
       sNo: index + 1,
       targetId: transformedContract?.lineItemsObject[estimate.id]?.contractLineItemId,
       isDeduction: estimate?.isDeduction,
       description: estimate?.description,
-      id: isMeasurement ? measuredObject?.id : null,
-      height: isMeasurement ? measuredObject?.height : 0,
-      width: isMeasurement ? measuredObject?.breadth : 0,
-      length: isMeasurement ? measuredObject?.length : 0,
-      number: isMeasurement ? measuredObject?.numItems : 0,
-      noOfunit: isMeasurement ? measuredObject?.currentValue    : 0,
-      rowAmount: isMeasurement ? measuredObject?.additionalDetails?.mbAmount        : 0,
+      id: measuredObject?.id || null,
+      height: measuredObject?.height || 0,
+      width: measuredObject?.breadth || 0,
+      length: measuredObject?.length || 0,
+      number: measuredObject?.numItems || 0,
+      noOfunit:  measuredObject?.currentValue || 0,
+      rowAmount: measuredObject?.additionalDetails?.mbAmount || 0,
       consumedRowQuantity: transformMeasurementData?.lineItemsObject?.[transformedContract?.lineItemsObject?.[estimate?.id]?.contractLineItemId]?.cumulativeValue || 0,
     })
   });
     return {
-      amount: isMeasurement ? measures?.reduce((acc, curr) => curr.isDeduction == true ? acc - curr?.rowAmount : acc + curr?.rowAmount, 0) : 0,
-      consumedQ: isMeasurement ? measures?.reduce((acc, curr) => curr.isDeduction == true ? acc - curr?.consumedRowQuantity : acc + curr?.consumedRowQuantity, 0) : 0,
+      amount: measures?.reduce((acc, curr) => curr.isDeduction == true ? acc - curr?.rowAmount : acc + curr?.rowAmount, 0) || 0,
+      consumedQ: measures?.reduce((acc, curr) => curr.isDeduction == true ? acc - curr?.consumedRowQuantity : acc + curr?.consumedRowQuantity, 0) || 0,
       sNo: index + 1,
-      currentMBEntry: isMeasurement ? measures?.reduce((acc, curr) => curr.isDeduction == true ? acc - curr?.noOfunit : acc + curr?.noOfunit, 0) : 0,
+      currentMBEntry: measures?.reduce((acc, curr) => curr.isDeduction == true ? acc - curr?.noOfunit : acc + curr?.noOfunit, 0) || 0,
       uom: transformedEstimateObject[key]?.[0]?.uom,
       description: transformedEstimateObject[key]?.[0]?.name,
       unitRate: transformedEstimateObject[key]?.[0]?.unitRate,
