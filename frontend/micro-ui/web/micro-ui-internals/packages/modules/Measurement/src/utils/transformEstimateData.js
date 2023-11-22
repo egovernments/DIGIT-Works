bc9daa31e551ab5e2a88bda753d384e092993887
Tulika-eGov <74046137+Tulika-eGov@users.noreply.github.com>
2023-11-22 12:13:26 +0530

    Mb fix 3 (#1545)
    * MB create fix
    
    * SOlving update MB issue
    
    * PFM-4931 : measurement save as draft issue fixfor preloading of data

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/Measurement/src/utils/transformEstimateData.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/Measurement/src/utils/transformEstimateData.js
@@ -6,6 +6,7 @@ output is array of object of type which is passed
 
 export const transformEstimateData = (lineItems, contract, type, measurement = {}, allMeasurements = []) => {
   /* logic to be updated according to business usecase*/
+  let isCreateorUpdate = window.location.href.includes("create") || window.location.href.includes("update");
   const lastMeasuredObject = allMeasurements?.filter?.((e) => e?.isActive)?.[0] || {};
   const transformedContract = transformContractObject(contract);
   const isMeasurement = measurement && Object.keys(measurement)?.length > 0;
@@ -22,27 +23,27 @@ export const transformEstimateData = (lineItems, contract, type, measurement = {
   const transformMeasurementData = isMeasurement ? transformMeasureObject(measurement) : transformMeasureObject(lastMeasuredObject);
   return Object.keys(transformedEstimateObject).map((key, index) => {
     const measures = transformedEstimateObject[key].map((estimate, index) =>{
-     const measuredObject= isMeasurement ? transformMeasurementData?.lineItemsObject[transformedContract?.lineItemsObject[estimate.id]?.contractLineItemId]:{};
+     const measuredObject= transformMeasurementData?.lineItemsObject && !isCreateorUpdate ? transformMeasurementData?.lineItemsObject[transformedContract?.lineItemsObject[estimate?.id]?.contractLineItemId] : {};
     return ({
       sNo: index + 1,
       targetId: transformedContract?.lineItemsObject[estimate.id]?.contractLineItemId,
       isDeduction: estimate?.isDeduction,
       description: estimate?.description,
-      id: isMeasurement ? measuredObject?.id : null,
-      height: isMeasurement ? measuredObject?.height : 0,
-      width: isMeasurement ? measuredObject?.breadth : 0,
-      length: isMeasurement ? measuredObject?.length : 0,
-      number: isMeasurement ? measuredObject?.numItems : 0,
-      noOfunit: isMeasurement ? measuredObject?.currentValue    : 0,
-      rowAmount: isMeasurement ? measuredObject?.additionalDetails?.mbAmount        : 0,
+      id: measuredObject?.id || null,
+      height: measuredObject?.height || 0,
+      width: measuredObject?.breadth || 0,
+      length: measuredObject?.length || 0,
+      number: measuredObject?.numItems || 0,
+      noOfunit:  measuredObject?.currentValue || 0,
+      rowAmount: measuredObject?.additionalDetails?.mbAmount || 0,
       consumedRowQuantity: transformMeasurementData?.lineItemsObject?.[transformedContract?.lineItemsObject?.[estimate?.id]?.contractLineItemId]?.cumulativeValue || 0,
     })
   });
     return {
-      amount: isMeasurement ? measures?.reduce((acc, curr) => curr.isDeduction == true ? acc - curr?.rowAmount : acc + curr?.rowAmount, 0) : 0,
-      consumedQ: isMeasurement ? measures?.reduce((acc, curr) => curr.isDeduction == true ? acc - curr?.consumedRowQuantity : acc + curr?.consumedRowQuantity, 0) : 0,
+      amount: measures?.reduce((acc, curr) => curr.isDeduction == true ? acc - curr?.rowAmount : acc + curr?.rowAmount, 0) || 0,
+      consumedQ: measures?.reduce((acc, curr) => curr.isDeduction == true ? acc - curr?.consumedRowQuantity : acc + curr?.consumedRowQuantity, 0) || 0,
       sNo: index + 1,
-      currentMBEntry: isMeasurement ? measures?.reduce((acc, curr) => curr.isDeduction == true ? acc - curr?.noOfunit : acc + curr?.noOfunit, 0) : 0,
+      currentMBEntry: measures?.reduce((acc, curr) => curr.isDeduction == true ? acc - curr?.noOfunit : acc + curr?.noOfunit, 0) || 0,
       uom: transformedEstimateObject[key]?.[0]?.uom,
       description: transformedEstimateObject[key]?.[0]?.name,
       unitRate: transformedEstimateObject[key]?.[0]?.unitRate,
@@ -85,6 +86,7 @@ export const transformMeasureObject = (measurement = {}) => {
 
 export const getDefaultValues = (data, t) => {
   const { contract, estimate, allMeasurements, measurement, musterRollNumber, period } = data;
+  console.log(allMeasurements);
   const SOR = transformEstimateData(estimate?.estimateDetails, contract, "SOR", measurement, allMeasurements);
   const NONSOR = transformEstimateData(estimate?.estimateDetails, contract, "NON-SOR", measurement, allMeasurements);
 
