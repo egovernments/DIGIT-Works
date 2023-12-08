139367afd0c1aed77606b91cec5f4c0ecc947098
Tulika-eGov <tulika.kundu@egovernments.org>
2023-12-08 19:09:13 +0530

    PFM-4277: Added redirection url for MB

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/Expenditure/src/components/CreateBill/MBDetailes.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/Expenditure/src/components/CreateBill/MBDetailes.js
@@ -3,14 +3,14 @@ import { useTranslation } from 'react-i18next'
 import { CardSectionHeader, Card, CardHeader, CardLabel, LinkLabel, Row, StatusTable} from "@egovernments/digit-ui-react-components";
 import ApplicationDetails from "../../../../templates/ApplicationDetails";
 
-const getMBLinks = (mblinks) => {
+const getMBLinks = (mblinks, tenantId, workOrderNumber) => {
 
     return(
     <React.Fragment>
     <div style={{}}>
-    {mblinks?.map((ob) => (
-    <LinkLabel style={{marginRight:"20px", display:"inline"}} onClick={""}>
-        {ob}
+    {mblinks?.map((measurementNumber) => (
+    <LinkLabel style={{marginRight:"20px", display:"inline"}} onClick={() => history.push(`/${window.contextPath}/employee/measurement/view?tenantId=${tenantId}&workOrderNumber=${workOrderNumber}&mbNumber=${measurementNumber}`)}>
+        {measurementNumber}
     </LinkLabel>
     ))}
     </div>
@@ -30,7 +30,7 @@ const MBDetailes = ({ formdata }) => {
   return (
         <div style={{marginTop:"2rem", marginBottom:"2rem"}}>
         <StatusTable>
-        <Row className="border-none" label={t("WORKS_MB_NUMBERS")} text={getMBLinks(allMeasurementsIds)} textStyle={{ whiteSpace: "pre" }} />
+        <Row className="border-none" label={t("WORKS_MB_NUMBERS")} text={getMBLinks(allMeasurementsIds, tenantId, workOrderNumber)} textStyle={{ whiteSpace: "pre" }} />
         <Row className="border-none" label={t("WORKS_TOTAL_MATERIAL_UTILIZED")} text={totalMaterialAmount || "NA"} textStyle={{ whiteSpace: "pre" }} />
         <Row className="border-none" label={t("WORKS_TOTAL_PAID_AMOUNT")} text={totalPaidAmountForSuccessfulBills || "NA"} textStyle={{ whiteSpace: "pre" }} />
         <Row className="border-none" label={t("WORKS_TOTAL_UNPAID_AMOUNT")} text={totalMaterialAmount - totalPaidAmountForSuccessfulBills || "NA"} textStyle={{ whiteSpace: "pre" }} />
