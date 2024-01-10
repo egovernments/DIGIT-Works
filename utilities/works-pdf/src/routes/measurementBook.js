75a594fba9bee0d33779bd665fd7e628a5ce0e9a
Lokendra Tyagi <lokendra.tyagi@egovernments.org>
2024-01-10 11:22:09 +0530

    fixed location

--- utilities/works-pdf/src/routes/measurementBook.js
+++ utilities/works-pdf/src/routes/measurementBook.js
@@ -3,18 +3,56 @@ const router = express.Router();
 const url = require("url");
 const config = require("../config");
 
-const { search_measurementBookDetails, create_pdf } = require("../api");
+const { search_measurementBookDetails, create_pdf, search_localization} = require("../api");
 const { asyncMiddleware } = require("../utils/asyncMiddleware");
 const { pdf } = require("../config");
 const { logger } = require("../logger");
 
 const { transformEstimateData } = require("../utils/transformEstimateData");
+const { getLanguageFromRequest, getStateLocalizationModule, getCityLocalizationModule, getStateLocalizationPrefix, getCityLocalizationPrefix, getLocalizationByKey } = require("../utils/localization");
+
 
 function renderError(res, errorMessage, errorCode) {
     if (errorCode == undefined) errorCode = 500;
     res.status(errorCode).send({ errorMessage })
 
 }
+
+async function getStateCityLocalizaitons(request, tenantId) {
+    let localizationMaps = {};
+    let lang = getLanguageFromRequest(request);
+    let modules = [getStateLocalizationModule(tenantId),getCityLocalizationModule(tenantId)].join(",");
+    let localRequest = {}
+    localRequest["RequestInfo"] = request["RequestInfo"];
+    let localizations = await search_localization(localRequest, lang, modules, tenantId);
+    if (localizations?.data?.messages?.length) {
+        localizations.data.messages.forEach(localObj => {
+            localizationMaps[localObj.code] = localObj.message;
+        });
+    }
+    return localizationMaps;
+}
+
+function updateLocalization(estimate, localizationMaps, tenantId) {
+        if (estimate?.city) {
+            estimate.city = estimate.city.toUpperCase();
+            cityKey = "TENANT_TENANTS_" + estimate.city.split(".").join("_");
+            estimate.city = getLocalizationByKey(cityKey, localizationMaps);
+        }
+        if (estimate?.locality) {
+            let localityKey = getCityLocalizationPrefix(tenantId);
+            localityKey = localityKey + "_ADMIN_" + estimate.locality;
+            estimate.locality = getLocalizationByKey(localityKey, localizationMaps);
+        }
+        if (estimate?.ward) {
+            let boundaryKey = getCityLocalizationPrefix(tenantId);
+            boundaryKey = boundaryKey + "_ADMIN_" + estimate.ward;
+            estimate.ward = getLocalizationByKey(boundaryKey, localizationMaps);
+        }
+    
+    return estimate;
+}
+
 router.post(
     "/measurement-book",
     asyncMiddleware(async function (req, res, next) {
@@ -50,9 +88,15 @@ router.post(
             const measurement = resMeasurement.data?.measurement;
             const allMeasurements = resMeasurement.data?.allMeasurements;
             const estimateDetails = resMeasurement.data?.estimate?.estimateDetails;
-            const estimate = resMeasurement.data?.estimate;
-            const completeLocation = estimate.additionalDetails.locality + ', ' + estimate.additionalDetails.location.ward + ', ' + estimate.additionalDetails.location.city;
-            estimate.location = completeLocation;
+            var estimate = resMeasurement.data?.estimate;
+            estimate.locality = estimate.additionalDetails.locality;
+            estimate.ward = estimate.additionalDetails.location.ward ;
+            estimate.city = estimate.additionalDetails.location.city ;
+            // const completeLocation = estimate.additionalDetails.locality + ', ' + estimate.additionalDetails.location.ward + ', ' + estimate.additionalDetails.location.city;
+            // estimate.location = completeLocation;
+
+            let localizationMap = await getStateCityLocalizaitons(requestinfo, tenantId);
+            estimate = updateLocalization(estimate, localizationMap, tenantId);
 
             // convert startDateTime and endDateTime into dd/mm/yyyy format and show only date in a variable named measurement period
             const startDate = new Date(measurement.additionalDetails.startDate);
