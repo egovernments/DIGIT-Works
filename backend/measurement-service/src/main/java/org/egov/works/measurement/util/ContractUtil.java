02d3afd1064728f924ecf6ce7d094452f86bf2eb
manastanmay-eGov <manastanmay@egovernments.org>
2024-01-16 14:52:08 +0530

    Business Service Filter logic Updated

--- backend/measurement-service/src/main/java/org/egov/works/measurement/util/ContractUtil.java
+++ backend/measurement-service/src/main/java/org/egov/works/measurement/util/ContractUtil.java
@@ -86,13 +86,19 @@ public class ContractUtil {
         // return if no contract is present
         if (!isValidContract) return false;
 
+        if (contractResponse.getContracts().get(0).getBusinessService().equals(CONTRACT_SERVICE)
+                && !contractResponse.getContracts().get(0).getWfStatus().equalsIgnoreCase(ACCEPTED_STATUS))
+            throw new CustomException(CONTRACT_NOT_ACCEPTED_CODE, CONTRACT_NOT_ACCEPTED_MSG);
+
         if( contractResponse.getContracts().get(0).getBusinessService().equals(CONTRACT_REVISION_ESTIMATE)
                 && !contractResponse.getContracts().get(0).getWfStatus().equalsIgnoreCase(APPROVED_STATUS))
             throw new CustomException(CONTRACT_NOT_APPROVED_CODE, CONTRACT_NOT_APPROVED_MSG);
 
-        if (!contractResponse.getContracts().get(0).getBusinessService().equals(BUSINESS_SERVICE_REVISION_CONTRACT)
+        if (contractResponse.getContracts().get(0).getBusinessService().equals(BUSINESS_SERVICE_TE_CONTRACT)
                 && !contractResponse.getContracts().get(0).getWfStatus().equalsIgnoreCase(ACCEPTED_STATUS))
-            throw new CustomException(CONTRACT_NOT_ACCEPTED_CODE, CONTRACT_NOT_ACCEPTED_MSG);
+            throw new CustomException(CONTRACT_NOT_APPROVED_CODE, CONTRACT_NOT_APPROVED_MSG);
+
+
 
         boolean isValidEntryDate = ((measurement.getEntryDate().compareTo(contractResponse.getContracts().get(0).getStartDate()) >= 0)
                 && (measurement.getEntryDate().compareTo(contractResponse.getContracts().get(0).getEndDate()) <= 0));
