246a295796611d44043fe82ba66ea992c799e513
Shailesh Pandey <110380977+shailesh-egov@users.noreply.github.com>
2024-04-16 11:30:42 +0530

    Added rest template config to disable SSL check

--- reference-adapters/ifms-adapter/src/main/java/org/egov/config/MainConfiguration.java
+++ reference-adapters/ifms-adapter/src/main/java/org/egov/config/MainConfiguration.java
@@ -3,6 +3,10 @@ package org.egov.config;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.TrustStrategy;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.egov.tracer.config.TracerConfiguration;
 import org.egov.tracer.config.TracerProperties;
 import org.egov.tracer.http.RestTemplateLoggingInterceptor;
@@ -13,14 +17,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Primary;
-import org.springframework.http.client.BufferingClientHttpRequestFactory;
-import org.springframework.http.client.ClientHttpRequestInterceptor;
-import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.http.client.*;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.util.CollectionUtils;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.PostConstruct;
+import javax.net.ssl.*;
+import java.net.HttpURLConnection;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.X509Certificate;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -46,14 +54,52 @@ public class MainConfiguration {
         return objectMapper;
     }
 
+//    @Bean
+//    @Primary
+//    public RestTemplate restTemplate(TracerProperties tracerProperties) {
+//        SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
+//        requestFactory.setOutputStreaming(false);
+//        RestTemplate restTemplate = new RestTemplate(new BufferingClientHttpRequestFactory(requestFactory));
+//        restTemplate.setInterceptors(Collections.singletonList(new RestTemplateLoggingInterceptor(tracerProperties)));
+//        return restTemplate;
+//    }
+
+    /**
+     * It will disable ssl certification check
+     * Remove this after testing
+     * @return
+     * @throws Exception
+     */
     @Bean
-    @Primary
-    public RestTemplate restTemplate(TracerProperties tracerProperties) {
+    public RestTemplate restTemplate() throws Exception {
+        TrustManager[] trustAllCertificates = new TrustManager[]{
+                new X509TrustManager() {
+                    public X509Certificate[] getAcceptedIssuers() {
+                        return null;
+                    }
+
+                    public void checkClientTrusted(X509Certificate[] certs, String authType) {
+                    }
+
+                    public void checkServerTrusted(X509Certificate[] certs, String authType) {
+                    }
+                }
+        };
+
+        SSLContext sslContext = SSLContext.getInstance("TLS");
+        sslContext.init(null, trustAllCertificates, new java.security.SecureRandom());
+        HttpsURLConnection.setDefaultSSLSocketFactory(sslContext.getSocketFactory());
+        HttpsURLConnection.setDefaultHostnameVerifier(new HostnameVerifier() {
+            public boolean verify(String hostname, SSLSession session) {
+                return true;
+            }
+        });
+
         SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
-        requestFactory.setOutputStreaming(false);
-        RestTemplate restTemplate = new RestTemplate(new BufferingClientHttpRequestFactory(requestFactory));
-        restTemplate.setInterceptors(Collections.singletonList(new RestTemplateLoggingInterceptor(tracerProperties)));
-        return restTemplate;
+        requestFactory.setConnectTimeout(3000);
+        requestFactory.setReadTimeout(3000);
+
+        return new RestTemplate(requestFactory);
     }
 
     @Primary
