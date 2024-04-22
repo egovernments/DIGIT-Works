9e3fd0e5ce253ba7a35cd94334b2346c048a222f
ansh-egov <ansh.goyal@egovernments.org>
2024-04-22 13:39:48 +0530

    changed program endpoint in config file

--- reference-adapters/mukta-ifix-adapter/src/main/java/org/egov/works/mukta/adapter/config/MuktaAdaptorConfig.java
+++ reference-adapters/mukta-ifix-adapter/src/main/java/org/egov/works/mukta/adapter/config/MuktaAdaptorConfig.java
@@ -90,7 +90,7 @@ public class MuktaAdaptorConfig {
 
     @Value("${egov.program.service.host}")
     private String programServiceHost;
-    @Value("${egov.program.service.disbursement.create.endpoint}")
+    @Value("${program.service.disbursement.create.endpoint}")
     private String programServiceDisbursementEndpoint;
 
     // Kafka Topics
