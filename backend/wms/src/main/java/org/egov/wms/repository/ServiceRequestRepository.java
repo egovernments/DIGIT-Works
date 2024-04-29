8d33d5b62dab408bbee1007cc59be1eabdc96a5f
prerna-egov <137176760+prerna-egov@users.noreply.github.com>
2024-04-29 15:36:23 +0530

    autowired restTemplate

--- backend/wms/src/main/java/org/egov/wms/repository/ServiceRequestRepository.java
+++ backend/wms/src/main/java/org/egov/wms/repository/ServiceRequestRepository.java
@@ -20,7 +20,8 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class ServiceRequestRepository {
 	private ObjectMapper mapper;
-
+	
+	@Autowired
 	private RestTemplate restTemplate;
 
 	private ESAuthUtil esAuthUtil;
