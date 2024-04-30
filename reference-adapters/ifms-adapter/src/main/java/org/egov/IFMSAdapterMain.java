33bdb58be912da015de422f673de4aacad35b235
prerna-egov <prerna.kumari@egovernments.org>
2024-04-30 16:02:27 +0530

    removed self-signed ssl method for bean creation

--- reference-adapters/ifms-adapter/src/main/java/org/egov/IFMSAdapterMain.java
+++ reference-adapters/ifms-adapter/src/main/java/org/egov/IFMSAdapterMain.java
@@ -12,42 +12,10 @@ import java.security.cert.X509Certificate;
 @SpringBootApplication
 public class IFMSAdapterMain {
 
-    public static void trustSelfSignedSSL() {
-        try {
-            SSLContext ctx = SSLContext.getInstance("TLS");
-            X509TrustManager tm = new X509TrustManager() {
-                public void checkClientTrusted(X509Certificate[] xcs, String string) throws CertificateException {
-                }
-
-                public void checkServerTrusted(X509Certificate[] xcs, String string) throws CertificateException {
-                }
-
-                public X509Certificate[] getAcceptedIssuers() {
-                    return null;
-                }
-            };
-            ctx.init(null, new TrustManager[]{tm}, null);
-            SSLContext.setDefault(ctx);
-
-            // Disable hostname verification
-            HttpsURLConnection.setDefaultHostnameVerifier(new HostnameVerifier() {
-                public boolean verify(String hostname, javax.net.ssl.SSLSession sslSession) {
-                    return true;
-                }
-            });
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-    }
 
     public static void main(String args[]) {
         SpringApplication.run(IFMSAdapterMain.class, args);
     }
 
-    @Bean
-    public RestTemplate restTemplate() {
-        trustSelfSignedSSL();
-        return new RestTemplate();
-    }
 
 }
