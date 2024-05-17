1c3c2e4a0b999f7e2182586476391b26ab6767bf
ansh-egov <ansh.goyal@egovernments.org>
2024-05-17 11:11:48 +0530

    Update MeasurementApiController.java
    Refractored some code

--- backend/measurement-registry/src/main/java/org/egov/works/measurement/web/controllers/MeasurementApiController.java
+++ backend/measurement-registry/src/main/java/org/egov/works/measurement/web/controllers/MeasurementApiController.java
@@ -39,9 +39,9 @@ public class MeasurementApiController {
         if (criteria != null) {
             List<Measurement> measurements = measurementRegistry.searchMeasurements(criteria, body);
             response.setMeasurements(measurements);
-            return new ResponseEntity<MeasurementResponse>(response, HttpStatus.OK);
+            return new ResponseEntity<>(response, HttpStatus.OK);
         }
-        return new ResponseEntity<MeasurementResponse>(response, HttpStatus.OK);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
 
