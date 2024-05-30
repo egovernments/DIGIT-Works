59551067f5b00f6292b1ed2244d77ce579a67d15
shubhang-eGov <shubhang.dhanesha@egovernments.org>
2024-05-30 13:24:12 +0530

    UCEM-99-removed unwanted properties in Configuration.java

--- backend/rate-analysis/src/main/java/org/egov/works/config/Configuration.java
+++ backend/rate-analysis/src/main/java/org/egov/works/config/Configuration.java
@@ -16,67 +16,12 @@ import org.springframework.stereotype.Component;
 public class Configuration {
 
 
-    // User Config
-    @Value("${egov.user.host}")
-    private String userHost;
-
-    @Value("${egov.user.context.path}")
-    private String userContextPath;
-
-    @Value("${egov.user.create.path}")
-    private String userCreateEndpoint;
-
-    @Value("${egov.user.search.path}")
-    private String userSearchEndpoint;
-
-    @Value("${egov.user.update.path}")
-    private String userUpdateEndpoint;
-
-
-    //Idgen Config
-    @Value("${egov.idgen.host}")
-    private String idGenHost;
-
-    @Value("${egov.idgen.path}")
-    private String idGenPath;
-
-
-    //Workflow Config
-    @Value("${egov.workflow.host}")
-    private String wfHost;
-
-    @Value("${egov.workflow.transition.path}")
-    private String wfTransitionPath;
-
-    @Value("${egov.workflow.businessservice.search.path}")
-    private String wfBusinessServiceSearchPath;
-
-    @Value("${egov.workflow.processinstance.search.path}")
-    private String wfProcessInstanceSearchPath;
-
-
     //MDMS
-    @Value("${egov.mdms.host}")
+    @Value("${egov.mdms.v2.host}")
     private String mdmsHost;
 
-    @Value("${egov.mdms.search.endpoint}")
+    @Value("${egov.mdms.v2.search.endpoint}")
     private String mdmsEndPoint;
-
-
-    //HRMS
-    @Value("${egov.hrms.host}")
-    private String hrmsHost;
-
-    @Value("${egov.hrms.search.endpoint}")
-    private String hrmsEndPoint;
-
-
-    //URLShortening
-    @Value("${egov.url.shortner.host}")
-    private String urlShortnerHost;
-
-    @Value("${egov.url.shortner.endpoint}")
-    private String urlShortnerEndpoint;
 
 
     //SMSNotification
