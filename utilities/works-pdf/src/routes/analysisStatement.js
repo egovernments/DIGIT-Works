c06a0a9807dac1f12da4e93920603b9594107e27
ansh-egov <ansh.goyal@egovernments.org>
2024-06-15 10:36:09 +0530

    added code for pdf

--- utilities/works-pdf/src/routes/analysisStatement.js
+++ utilities/works-pdf/src/routes/analysisStatement.js
@@ -1,13 +1,69 @@
 const express = require("express");
 const router = express.Router();
+const config = require("../config");
+
 
 const data = require("./statementRes.json");
 const {asyncMiddleware} = require("../utils/asyncMiddleware");
 const {transformStatementData} = require("../utils/transformStatementData");
+const {create_pdf} = require("../api");
+
+function renderError(res, errorMessage, errorCode) {
+    if (errorCode == undefined) errorCode = 500;
+    res.status(errorCode).send({ errorMessage })
+
+}
 
 router.post("/rate-analysis-statement", asyncMiddleware(async function (req, res, next) {
-    transformStatementData(data);
-    console.log(statement)
+    const requestInfo = req.body;
+    const tenantId = req.query.tenantId;
+    if(requestInfo == undefined) {
+        return renderError(res, "requestinfo can not be null", 400);
+    }
+    try {
+        const AnalysisStatement = transformStatementData(data);
+        if(AnalysisStatement){
+            var pdfResponse;
+            const pdfKey = config.pdf.rateAnalysisStatement_template;
+            try {
+
+                pdfResponse = await create_pdf(
+                    tenantId,
+                    pdfKey,
+                    AnalysisStatement,
+                    requestInfo
+                )
+                const filename = `${pdfKey}_${new Date().getTime()}`;
+
+                res.writeHead(200, {
+
+                    "Content-Type": "application/pdf",
+                    "Content-Disposition": `attachment; filename=${filename}.pdf`,
+                });
+
+                pdfResponse.data.pipe(res);
+
+            }
+
+
+            catch (ex) {
+
+                return renderError(res, "Failed to generate PDF for detailed estimate", 500);
+
+            }
+        } else {
+
+            return renderError(
+                res,
+                "There is no estimate created using this estimate number",
+                404
+            );
+
+        }
+    }catch (ex){
+        return renderError(res, "Failed to transform analysis statement", 500);
+    }
+    console.log(AnalysisStatement)
 }))
 
 module.exports = router;
--- utilities/works-pdf/src/utils/transformStatementData.js
+++ utilities/works-pdf/src/utils/transformStatementData.js
@@ -7,19 +7,67 @@ const transformStatementData = (data) => {
         statement = data["statement"][0];
 
         statement["sorDetails"].forEach(sorDetail => {
-            sorDetail["lineItems"].forEach(lineItem => {
-                const amountDetail = lineItem["amountDetails"][0];
+            if(sorDetail["lineItems"].length > 0){
+                sorDetail["lineItems"].forEach(lineItem => {
+                    const amountDetail = lineItem["basicSorDetails"][0];
+                    const sorDetail = lineItem["additionalDetails"]["sorDetails"];
 
-                // Check if the sorType already exists in the map
-                if (!sorMap.has(lineItem["sorType"])) {
-                    sorMap.set(lineItem["sorType"], []);
-                }
-
-                // Add the amountDetail to the appropriate sorType entry
-                sorMap.get(lineItem["sorType"]).push(amountDetail);
-            });
+                    if(sorMap.has(lineItem["sorId"])){
+                        const amountDetailDup = sorMap.get(lineItem["sorId"]);
+                        amountDetail.amount += amountDetailDup.amount;
+                        amountDetail.quantity += amountDetailDup.quantity;
+                    }else{
+                        amountDetail.description = "Code: "+ sorDetail.id+ " \n \n" + sorDetail.description;
+                        amountDetail.sorType = lineItem.sorType;
+                    }
+                    sorMap.set(lineItem["sorId"], amountDetail);
+                });
+            }else{
+                const sorId = sorDetail["sorId"];
+                sorDetail["basicSorDetails"].forEach(basicSorDetail => {
+                    const sorDetail = lineItem["additionalDetails"]["sorDetails"];
+                    if(sorMap.has(sorId)){
+                        const amountDetail = sorMap.get(sorId);
+                        amountDetail.amount += basicSorDetail.amount;
+                        amountDetail.quantity += basicSorDetail.quantity;
+                    }else{
+                        amountDetail.description = "Code: "+ sorDetail.id+ " \n \n" + sorDetail.description;
+                        sorMap.set(sorId,basicSorDetail);
+                    }
+                })
+            }
         });
-        console.log(sorMap);
+        const sorTypeToSorMap = new Map();
+        for(let value of sorMap.values()){
+            if(!sorTypeToSorMap.has(value.type)){
+                sorTypeToSorMap.set(value.type, []);
+            }
+            value.Sno = sorTypeToSorMap.get(value.type).length + 1;
+            sorTypeToSorMap.get(value.type).push(value);
+        }
+        const ProjectID  = "PJ/2024-25/002428"
+        const ProjectName = "Test Project"
+        const ProjectLocation =  "GAJPATI NAGAR, Ward 1, Testing"
+        const ProjectDescription = "Test Project"
+        const tenantId = "od.jatni";
+        const resultArray = [...sorTypeToSorMap.entries()].map(([sorType, amountDetails], index) => ({
+            sorType,
+            amountDetails,
+            ProjectID,
+            ProjectName,
+            ProjectDescription,
+            ProjectLocation,
+            tenantId
+        }));
+        resultArray.forEach(result => {
+            var totalAmount = 0;
+            result.amountDetails.forEach(amountDetail => {
+                totalAmount += amountDetail.amount;
+            })
+            result.totalEstimatedAmount = totalAmount;
+        })
+        const AnalysisStatement = resultArray;
+        return AnalysisStatement;
     }catch (ex){
         console.log(ex);
     }
