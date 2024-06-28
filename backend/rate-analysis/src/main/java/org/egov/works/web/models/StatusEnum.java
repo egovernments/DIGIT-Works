f559f06659c06385c5f8f0f48606aae615ef619c
ansh-egov <ansh.goyal@egovernments.org>
2024-06-28 14:27:36 +0530

    changed IN PROGRESS to IN_PROGRESS

--- backend/rate-analysis/src/main/java/org/egov/works/web/models/StatusEnum.java
+++ backend/rate-analysis/src/main/java/org/egov/works/web/models/StatusEnum.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
 
 public enum StatusEnum {
-    IN_PROGRESS("IN PROGRESS"),
+    IN_PROGRESS("IN_PROGRESS"),
 
     SCHEDULED("SCHEDULED"),
 
