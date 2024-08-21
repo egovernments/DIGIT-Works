d6521be6704f672d75954481a4b372d79f2b81c7
shubhang-egov <shubhang.dhanesha@egovernments.org>
2024-08-21 12:51:49 +0530

    Added model validations for all

--- backend/bankaccounts/src/main/java/org/egov/BankAccountMain.java
+++ backend/bankaccounts/src/main/java/org/egov/BankAccountMain.java
@@ -10,11 +10,11 @@ import org.springframework.context.annotation.Import;
 @Import({TracerConfiguration.class})
 @SpringBootApplication
 @ComponentScan(basePackages = {"org.egov", "org.egov.web.controllers", "org.egov.config"})
-public class Main {
+public class BankAccountMain {
 
 
     public static void main(String[] args) throws Exception {
-        SpringApplication.run(Main.class, args);
+        SpringApplication.run(BankAccountMain.class, args);
     }
 
 }
