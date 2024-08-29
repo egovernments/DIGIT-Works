a7cfeeb42133bcc8378d2bd9b2c54e190f7357db
shubhang-eGov <70943369+shubhang-eGov@users.noreply.github.com>
2024-08-29 17:40:03 +0530

    Works lts (#1807)
    * PFM-5883 Upgraded to spring 3, java 17, postgres 42.7.1 and using jakarta instead of javax
    
    * PFM-5883 Removed digit models use from all except NotificationService
    
    * PFM-5883 Removed works common models and taking it from health, work-common models libraries
    
    * PFM-5883 SonarLint autowire change
    
    * Changed host url from v1 to v2
    
    * updated dockerfile name
    
    * PFM-5883-added validator dependency
    
    * Added model validations and upgraded health library
    
    * Fixed test cases

--- backend/estimates/src/main/java/org/egov/web/models/EstimateSearchCriteria.java
+++ backend/estimates/src/main/java/org/egov/web/models/EstimateSearchCriteria.java
@@ -2,9 +2,10 @@ package org.egov.web.models;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
-import javax.validation.constraints.Size;
+import jakarta.validation.constraints.Size;
 import java.math.BigDecimal;
 import java.util.List;
 
@@ -16,9 +17,11 @@ import java.util.List;
 public class EstimateSearchCriteria {
 
     @JsonProperty("ids")
+    @Size(max = 10)
     private List<String> ids;
 
     @JsonProperty("tenantId")
+    @NotNull
     private String tenantId = null;//mand
 
     @JsonProperty("estimateNumber")
