454f694e5f19ebb0e8fcab098b08aae18d235675
ansh-egov <ansh.goyal@egovernments.org>
2024-10-01 11:55:22 +0530

    added redis template

--- backend/wms/src/main/java/org/egov/wms/config/RedisConfig.java
+++ backend/wms/src/main/java/org/egov/wms/config/RedisConfig.java
@@ -7,6 +7,7 @@ import org.springframework.data.redis.connection.RedisConfiguration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
 @Configuration
@@ -22,6 +23,13 @@ public class RedisConfig {
     public RedisConnectionFactory redisConnectionFactory() {
         RedisConfiguration redisConfiguration = new RedisStandaloneConfiguration(redisHost, redisPort);
         return new LettuceConnectionFactory(redisConfiguration);
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        return redisTemplate;
     }
 
     @Bean
