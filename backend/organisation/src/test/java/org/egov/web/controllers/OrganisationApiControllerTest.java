633d244ded1e81a3148fb2d21331138ccf82e58e
shubhang-egov <shubhang.dhanesha@egovernments.org>
2024-10-03 17:47:27 +0530

    adding lazy-initialization

--- backend/organisation/src/test/java/org/egov/web/controllers/OrganisationApiControllerTest.java
+++ backend/organisation/src/test/java/org/egov/web/controllers/OrganisationApiControllerTest.java
@@ -16,14 +16,18 @@ import org.egov.web.models.OrgRequest;
 import org.egov.web.models.OrgResponse;
 import org.junit.Ignore;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
@@ -50,13 +54,8 @@ import org.junit.jupiter.api.Test;
 //import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 //import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-//@RunWith(SpringRunner.class)
-//@WebMvcTest(OrganisationApiController.class)
-//@Import({TestConfiguration.class})
-//@Slf4j
-@ContextConfiguration(classes = OrganizationMain.class)
-@WebMvcTest(OrganisationApiController.class)
-@Import(TestConfiguration.class)
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = OrganizationMain.class, properties = "spring.main.lazy-initialization=true")
 @AutoConfigureMockMvc
 class OrganisationApiControllerTest {
 
