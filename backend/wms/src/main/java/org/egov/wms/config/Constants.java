9ff263b578149ab992017540b91f662d45dde71b
Shailesh Pandey <110380977+shailesh-egov@users.noreply.github.com>
2024-10-22 16:51:36 +0530

    Added failed payment details in sheet

--- backend/wms/src/main/java/org/egov/wms/config/Constants.java
+++ backend/wms/src/main/java/org/egov/wms/config/Constants.java
@@ -4,7 +4,8 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Constants {
-    public static final List<String> PAYMENT_REPORT_HEADERS = Arrays.asList("Project Id", "Estimated amount","Wage amount paid","Purchase amount paid", "Supervision amount paid", "Total amount paid");
+
+    public static final List<String> PAYMENT_REPORT_HEADERS = Arrays.asList("Project ID", "Estimated amount (₹)","Wage payment successful (₹)", "Wage payment failed (₹)", "Purchase payment successful (₹)", "Purchase payment failed (₹)", "Supervision payment successful (₹)", "Supervision payment failed (₹)");
     public static final String EXPENSE_PURCHASE = "EXPENSE.PURCHASE";
     public static final String EXPENSE_WAGE = "EXPENSE.WAGE";
     public static final String EXPENSE_SUPERVISION = "EXPENSE.SUPERVISION";
