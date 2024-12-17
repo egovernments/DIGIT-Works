ee3c702e2fe7103deb65a450ef6eae22bdded38f
yashita-egov <yashita.bansal@egovernments.org>
2024-12-17 14:20:22 +0530

    update character size locality code

--- backend/attendance/src/main/resources/db/migration/main/V20241217103530__alter_attendance_table.sql
+++ backend/attendance/src/main/resources/db/migration/main/V20241217103530__alter_attendance_table.sql
@@ -1,1 +1,1 @@
-ALTER TABLE eg_wms_attendance_register ADD COLUMN IF NOT EXISTS localityCode character varying(64);
+ALTER TABLE eg_wms_attendance_register ADD COLUMN IF NOT EXISTS localityCode character varying(256);
