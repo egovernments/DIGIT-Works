cc2ab2ec6643c3f925c9dfbcffe12e67259317ba
Holash Chand <holash.c@beehyv.com>
2025-01-06 10:48:25 +0530

    Fixed musterroll tests

--- backend/muster-roll/src/test/java/org/egov/validator/MusterRollValidatorTest.java
+++ backend/muster-roll/src/test/java/org/egov/validator/MusterRollValidatorTest.java
@@ -132,6 +132,7 @@ class MusterRollValidatorTest {
     void shouldThrowException_IfWorkflowIsNull() {
         MusterRollRequest musterRollRequest = MusterRollRequestBuilderTest.builder().withMusterForCreateValidationSuccess();
         musterRollRequest.setWorkflow(null);
+        lenient().when(serviceConfiguration.isMusterRollWorkflowEnabled()).thenReturn(true);
         CustomException exception = assertThrows(CustomException.class, ()-> musterRollValidator.validateCreateMusterRoll(musterRollRequest));
         assertTrue(exception.getCode().contentEquals("WORK_FLOW"));
     }
@@ -141,6 +142,7 @@ class MusterRollValidatorTest {
         MusterRollRequest musterRollRequest = MusterRollRequestBuilderTest.builder().withMusterForCreateValidationSuccess();
         getMockAttendanceRegisterSuccess();
         musterRollRequest.getWorkflow().setAction("");
+        lenient().when(serviceConfiguration.isMusterRollWorkflowEnabled()).thenReturn(true);
         CustomException exception = assertThrows(CustomException.class, ()-> musterRollValidator.validateCreateMusterRoll(musterRollRequest));
         assertNotNull(exception.getErrors().get("WORK_FLOW.ACTION"));
     }
