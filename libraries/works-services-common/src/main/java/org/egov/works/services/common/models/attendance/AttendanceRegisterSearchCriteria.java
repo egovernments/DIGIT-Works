cc964160490017d45ae5e9a9b2945486c95ab66a
yashita-egov <yashita.bansal@egovernments.org>
2025-01-07 11:32:08 +0530

    added isChildrenRequired in register search criteria

--- libraries/works-services-common/src/main/java/org/egov/works/services/common/models/attendance/AttendanceRegisterSearchCriteria.java
+++ libraries/works-services-common/src/main/java/org/egov/works/services/common/models/attendance/AttendanceRegisterSearchCriteria.java
@@ -69,6 +69,9 @@ public class AttendanceRegisterSearchCriteria {
     @JsonProperty("reviewStatus")
     private String reviewStatus;
 
+    @JsonProperty("isChildrenRequired")
+    private boolean isChildrenRequired = false;
+
     public enum SortOrder {
         ASC,
         DESC
