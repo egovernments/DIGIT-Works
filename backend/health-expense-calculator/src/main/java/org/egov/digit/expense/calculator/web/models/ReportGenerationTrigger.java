b8e2a88bc16519b491c67f83185641f04785f5ea
shubhang-egov <shubhang.dhanesha@egovernments.org>
2025-02-21 10:20:07 +0530

    added import, removed max suggestion

--- backend/health-expense-calculator/src/main/java/org/egov/digit/expense/calculator/web/models/ReportGenerationTrigger.java
+++ backend/health-expense-calculator/src/main/java/org/egov/digit/expense/calculator/web/models/ReportGenerationTrigger.java
@@ -1,6 +1,7 @@
 package org.egov.digit.expense.calculator.web.models;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
@@ -38,7 +39,6 @@ public class ReportGenerationTrigger {
     private String billId;
 
     @JsonProperty("createdTime")
-    @Max(value = System.currentTimeMillis(), message = "Created time cannot be in the future")
     /** Timestamp when the trigger was created */
     private Long createdTime;
 
