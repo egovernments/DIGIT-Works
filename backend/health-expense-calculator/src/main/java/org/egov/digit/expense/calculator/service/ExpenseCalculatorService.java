8b5aaa8727e1108dcc193d1b163427deb8689d27
himanshukeshari-eGov <himanshu.keshari@egovernments.org>
2025-03-09 13:07:49 +0530

    Fix for adding the total amount

--- backend/health-expense-calculator/src/main/java/org/egov/digit/expense/calculator/service/ExpenseCalculatorService.java
+++ backend/health-expense-calculator/src/main/java/org/egov/digit/expense/calculator/service/ExpenseCalculatorService.java
@@ -366,6 +366,9 @@ public class ExpenseCalculatorService {
                 if (SUCCESSFUL_CONSTANT.equalsIgnoreCase(billResponse.getResponseInfo().getStatus())) {
                     log.info("Bill successfully posted to expense service. Reference ID " + bill.getReferenceId());
                     List<Bill> respBills = billResponse.getBills();
+                    respBills.get(0).setTotalTransportAmount(bill.getTotalTransportAmount());
+                    respBills.get(0).setTotalFoodAmount(bill.getTotalFoodAmount());
+                    respBills.get(0).setTotalWageAmount(bill.getTotalWageAmount());
                     submittedBills.addAll(respBills);
 //                if(respBills != null && !respBills.isEmpty()) {
 //                    log.info("Persisting meta for bill reference ID: " + bill.getReferenceId());
