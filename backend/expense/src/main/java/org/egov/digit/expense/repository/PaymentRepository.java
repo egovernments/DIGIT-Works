4ad7af6405e60129bcfeae119ba3bbf34f1e3986
Holash Chand <holash.c@beehyv.com>
2025-05-07 11:01:14 +0530

    central instance changes for expense service

--- backend/expense/src/main/java/org/egov/digit/expense/repository/PaymentRepository.java
+++ backend/expense/src/main/java/org/egov/digit/expense/repository/PaymentRepository.java
@@ -5,13 +5,18 @@ import java.util.List;
 
 import jakarta.validation.Valid;
 
+import org.egov.common.exception.InvalidTenantIdException;
+import org.egov.common.utils.MultiStateInstanceUtil;
 import org.egov.digit.expense.repository.querybuilder.PaymentQueryBuilder;
 import org.egov.digit.expense.repository.rowmapper.PaymentRowMapper;
 import org.egov.digit.expense.web.models.Payment;
 import org.egov.digit.expense.web.models.PaymentSearchRequest;
+import org.egov.tracer.model.CustomException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+
+import static org.egov.digit.expense.config.Constants.INVALID_TENANT_ID_ERR_CODE;
 
 @Repository
 public class PaymentRepository {
@@ -22,23 +27,36 @@ public class PaymentRepository {
 	
 	private final PaymentRowMapper paymentBillRowMapper;
 
+	private final MultiStateInstanceUtil multiStateInstanceUtil;
+
 	@Autowired
-	public PaymentRepository(JdbcTemplate jdbcTemplate, PaymentQueryBuilder queryBuilder, PaymentRowMapper paymentBillRowMapper) {
+	public PaymentRepository(JdbcTemplate jdbcTemplate, PaymentQueryBuilder queryBuilder, PaymentRowMapper paymentBillRowMapper, MultiStateInstanceUtil multiStateInstanceUtil) {
 		this.jdbcTemplate = jdbcTemplate;
 		this.queryBuilder = queryBuilder;
 		this.paymentBillRowMapper = paymentBillRowMapper;
-	}
+        this.multiStateInstanceUtil = multiStateInstanceUtil;
+    }
 
 	public List<Payment> search(@Valid PaymentSearchRequest paymentSearchRequest) {
 
 		List<Object> preparedStatementValues = new ArrayList<>();
 		String queryStr = queryBuilder.getPaymentQuery(paymentSearchRequest, preparedStatementValues, false);
-		return jdbcTemplate.query(queryStr, preparedStatementValues.toArray(), paymentBillRowMapper);
+        try {
+            queryStr = multiStateInstanceUtil.replaceSchemaPlaceholder(queryStr, paymentSearchRequest.getPaymentCriteria().getTenantId());
+        } catch (InvalidTenantIdException e) {
+			throw new CustomException(INVALID_TENANT_ID_ERR_CODE, e.getMessage());
+        }
+        return jdbcTemplate.query(queryStr, preparedStatementValues.toArray(), paymentBillRowMapper);
 	}
 
 	public Integer count(@Valid PaymentSearchRequest paymentSearchRequest) {
 		List<Object> preparedStatementValues = new ArrayList<>();
 		String queryStr = queryBuilder.getSearchCountQueryString(paymentSearchRequest, preparedStatementValues);
+		try {
+			queryStr = multiStateInstanceUtil.replaceSchemaPlaceholder(queryStr, paymentSearchRequest.getPaymentCriteria().getTenantId());
+		} catch (InvalidTenantIdException e) {
+			throw new CustomException(INVALID_TENANT_ID_ERR_CODE, e.getMessage());
+		}
 		return jdbcTemplate.queryForObject(queryStr, preparedStatementValues.toArray(), Integer.class);
 	}
 }
