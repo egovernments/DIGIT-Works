591d8b595189ed2bdb7c2aa4e0e4ef9259a80bea
Holash Chand <holash.c@beehyv.com>
2025-05-14 15:22:31 +0530

    Fixed multistate instance bean not found

--- backend/expense-calculator/src/main/java/org/egov/digit/expense/calculator/ExpenseCalculatorMain.java
+++ backend/expense-calculator/src/main/java/org/egov/digit/expense/calculator/ExpenseCalculatorMain.java
@@ -1,15 +1,14 @@
 package org.egov.digit.expense.calculator;
 
+import org.egov.common.utils.MultiStateInstanceUtil;
 import org.egov.tracer.config.TracerConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
 
-@Import({ TracerConfiguration.class })
+@Import({ MultiStateInstanceUtil.class, TracerConfiguration.class })
 @SpringBootApplication
-@ComponentScan(basePackages = { "org.egov.digit.expense.calculator",
-		"org.egov.digit.expense.calculator.web.controllers", "org.egov.digit.expense.calculator.config" })
 public class ExpenseCalculatorMain {
 	public static void main(String[] args) {
 		SpringApplication.run(ExpenseCalculatorMain.class, args);
