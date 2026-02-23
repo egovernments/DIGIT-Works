3f678535b498ceec7dabc51d03b866581606fda6
Holash Chand <holash.c@beehyv.com>
2026-02-23 19:04:04 +0530

    Fixed muster roll consumer not working

--- backend/muster-roll/src/main/java/org/egov/MusterRollMain.java
+++ backend/muster-roll/src/main/java/org/egov/MusterRollMain.java
@@ -2,12 +2,13 @@ package org.egov;
 
 
 import org.egov.tracer.config.TracerConfiguration;
+import org.egov.common.utils.MultiStateInstanceUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
 
-@Import({TracerConfiguration.class})
+@Import({ TracerConfiguration.class, MultiStateInstanceUtil.class})
 @SpringBootApplication
 @ComponentScan(basePackages = {"org.egov", "org.egov.web.controllers", "org.egov.config"})
 public class MusterRollMain {
